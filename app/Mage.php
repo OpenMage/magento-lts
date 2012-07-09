6ea22c9abff24da3f3f2aaed59c527ff803c02eb
Lee Saferite <lee.saferite@lokeycoding.com>
2012-07-09 08:13:54 -0400

    Import Magento Release 1.7.0.2

--- app/Mage.php
+++ app/Mage.php
@@ -170,7 +170,7 @@ final class Mage
             'major'     => '1',
             'minor'     => '7',
             'revision'  => '0',
-            'patch'     => '1',
+            'patch'     => '2',
             'stability' => '',
             'number'    => '',
         );
