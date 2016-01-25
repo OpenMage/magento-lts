28a031f7378f3b92a8f4528f90511cbf5a2b22ee
David Robinson <david.robinson@aoe.com>
2016-01-25 18:07:23 +0000

    Imported Magento Release 1.9.2.3 - license updates

--- get.php
+++ get.php
@@ -20,7 +20,7 @@
  *
  * @category    Mage
  * @package     Mage
- * @copyright  Copyright (c) 2006-2015 X.commerce, Inc. (http://www.magento.com)
+ * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 if (version_compare(phpversion(), '5.2.0', '<')===true) {
