db69295f8238e1fe909b5e43c71abed17817bcc4
Sven Reichel <sven.reichel@hotmail.com>
2017-03-10 12:58:47 +0100

    Fixed joinLeft / joinInner

--- app/code/core/Mage/Reports/Model/Resource/Quote/Collection.php
+++ app/code/core/Mage/Reports/Model/Resource/Quote/Collection.php
@@ -206,7 +206,7 @@ class Mage_Reports_Model_Resource_Quote_Collection extends Mage_Sales_Model_Reso
                 )),
                 array('firstname' => 'cust_fname.value')
             )
-            ->joinLeft(
+            ->joinInner(
                 array('cust_mname' => $attrMiddlenameTableName),
                 implode(' AND ', array(
                     'cust_mname.entity_id = main_table.customer_id',
