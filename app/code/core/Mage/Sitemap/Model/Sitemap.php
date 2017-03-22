8d43547c489315cfd7d87b0f6e19e243f848769b
Bastien LAMAMY <bastien.lamamy@ph2m.com>
2017-03-22 11:11:37 +0100

    Sitemap dispatch event for add custom data

--- app/code/core/Mage/Sitemap/Model/Sitemap.php
+++ app/code/core/Mage/Sitemap/Model/Sitemap.php
@@ -221,6 +221,13 @@ class Mage_Sitemap_Model_Sitemap extends Mage_Core_Model_Abstract
         }
         unset($collection);
 
+        Mage::dispatchEvent('sitemap_generate_before', array(
+            'file'      => $io ,
+            'base_url'  => $baseUrl ,
+            'date'      => $date,
+            'store_id'  => $storeId
+        ));
+
         $io->streamWrite('</urlset>');
         $io->streamClose();
 
