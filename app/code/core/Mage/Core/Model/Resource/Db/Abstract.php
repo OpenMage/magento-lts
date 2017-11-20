e60cd033a78a2b44c93837f524d9be50036ec1d7
Pepijn Blom <pepijn@graciousstudios.nl>
2017-11-20 23:35:15 +0100

    Use describeTable and then use prepareColumnValue to actually prepare the value being used in the where section of select statements

--- app/code/core/Mage/Core/Model/Resource/Db/Abstract.php
+++ app/code/core/Mage/Core/Model/Resource/Db/Abstract.php
@@ -400,6 +400,10 @@ abstract class Mage_Core_Model_Resource_Db_Abstract extends Mage_Core_Model_Reso
      */
     protected function _getLoadSelect($field, $value, $object)
     {
+        $fields = $this->_getReadAdapter()->describeTable($this->getMainTable());
+        if(isset($fields[$field]) && isset($fields[$field]['DATA_TYPE']))   {
+            $value  = $this->_getReadAdapter()->prepareColumnValue($fields[$field], $value);
+        }
         $field  = $this->_getReadAdapter()->quoteIdentifier(sprintf('%s.%s', $this->getMainTable(), $field));
         $select = $this->_getReadAdapter()->select()
             ->from($this->getMainTable())
@@ -773,4 +777,5 @@ abstract class Mage_Core_Model_Resource_Db_Abstract extends Mage_Core_Model_Reso
         }
         return $checksum;
     }
+
 }
