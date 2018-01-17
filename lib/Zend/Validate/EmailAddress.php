6ff4e9ad1095039141159d813dd376f085c308a0
Colin Mollenhour <colin@mollenhour.com>
2018-01-17 13:54:29 -0500

    Add back error flags.

--- lib/Zend/Validate/EmailAddress.php
+++ lib/Zend/Validate/EmailAddress.php
@@ -423,6 +423,9 @@ class Zend_Validate_EmailAddress extends Zend_Validate_Abstract
         $atext = 'a-zA-Z0-9\x21\x23\x24\x25\x26\x27\x2a\x2b\x2d\x2f\x3d\x3f\x5e\x5f\x60\x7b\x7c\x7d\x7e';
         if (preg_match('/^[' . $atext . ']+(\x2e+[' . $atext . ']+)*$/', $this->_localPart)) {
             $result = true;
+        } else {
+            $this->_error(self::DOT_ATOM);
+            $this->_error(self::INVALID_LOCAL_PART);
         }
 
         return $result;
