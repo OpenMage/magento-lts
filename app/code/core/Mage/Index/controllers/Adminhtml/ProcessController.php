26b2e5813a5ebcba3fcffa7b25f3b16694dd82a4
Colin Mollenhour <colin@mollenhour.com>
2018-02-28 10:08:54 -0500

    Update Copyright notices for 1.9.3.8.

--- app/code/core/Mage/Index/controllers/Adminhtml/ProcessController.php
+++ app/code/core/Mage/Index/controllers/Adminhtml/ProcessController.php
@@ -20,7 +20,7 @@
  *
  * @category    Mage
  * @package     Mage_Index
- * @copyright  Copyright (c) 2006-2017 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @copyright  Copyright (c) 2006-2018 Magento, Inc. (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Mage_Index_Adminhtml_ProcessController extends Mage_Adminhtml_Controller_Action
