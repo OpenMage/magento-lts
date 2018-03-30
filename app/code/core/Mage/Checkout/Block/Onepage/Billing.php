f5184061d91ec48865cc1bf8b1409df908917614
seansan <seansan@users.noreply.github.com>
2018-03-30 10:48:52 +0200

    Fix 2 - Always return already filled out address

--- app/code/core/Mage/Checkout/Block/Onepage/Billing.php
+++ app/code/core/Mage/Checkout/Block/Onepage/Billing.php
@@ -102,20 +102,7 @@ class Mage_Checkout_Block_Onepage_Billing extends Mage_Checkout_Block_Onepage_Ab
     public function getAddress()
     {
         if (is_null($this->_address)) {
-            if ($this->isCustomerLoggedIn()) {
-                $this->_address = $this->getQuote()->getBillingAddress();
-                if (!$this->_address->getFirstname()) {
-                    $this->_address->setFirstname($this->getQuote()->getCustomer()->getFirstname());
-                }
-                if (!$this->_address->getMiddlename()) {
-                    $this->_address->setMiddlename($this->getQuote()->getCustomer()->getMiddlename());
-                }
-                if (!$this->_address->getLastname()) {
-                    $this->_address->setLastname($this->getQuote()->getCustomer()->getLastname());
-                }
-            } else {
-                $this->_address = Mage::getModel('sales/quote_address');
-            }
+            $this->_address = $this->getQuote()->getBillingAddress();
         }
 
         return $this->_address;
