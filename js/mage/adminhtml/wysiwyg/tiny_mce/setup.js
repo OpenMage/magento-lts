ca67557834ea2a6daba9b97d0cdffc0a741022c0
Tobias Schifftner <ts@ambimax.de>
2018-04-05 18:14:11 +0200

    Added srcset support (wysiwyg editor)
    Added support for srcset in wysiwyg editor by allowing the tag attributes

--- js/mage/adminhtml/wysiwyg/tiny_mce/setup.js
+++ js/mage/adminhtml/wysiwyg/tiny_mce/setup.js
@@ -215,7 +215,7 @@ tinyMceWysiwygSetup.prototype =
                 +"img[align<bottom?left?middle?right?top|alt|border|class|dir<ltr?rtl|height"
                 +"|hspace|id|ismap<ismap|lang|longdesc|name|onclick|ondblclick|onkeydown"
                 +"|onkeypress|onkeyup|onmousedown|onmousemove|onmouseout|onmouseover"
-                +"|onmouseup|src|style|title|usemap|vspace|width],"
+                +"|onmouseup|sizes|src|srcset|style|title|usemap|vspace|width],"
                 +"input[accept|accesskey|align<bottom?left?middle?right?top|alt"
                 +"|checked<checked|class|dir<ltr?rtl|disabled<disabled|id|ismap<ismap|lang"
                 +"|maxlength|name|onblur|onclick|ondblclick|onfocus|onkeydown|onkeypress"
