9e50ad5da64bf697aa21860268724f0ddab8ae04
Colin Mollenhour <colin@mollenhour.com>
2018-10-04 18:20:10 -0400

    Fix shouldUrlBeSecure when base_unsecure_url is secure.

--- app/code/core/Mage/Core/Model/Config.php
+++ app/code/core/Mage/Core/Model/Config.php
@@ -1489,6 +1489,11 @@ class Mage_Core_Model_Config extends Mage_Core_Model_Config_Base
             return false;
         }
 
+        // If unsecure base url is https, then all urls should be secure
+        if (strpos(Mage::getStoreConfig(Mage_Core_Model_Store::XML_PATH_UNSECURE_BASE_URL), 'https://') === 0) {
+            return true;
+        }
+
         if (!isset($this->_secureUrlCache[$url])) {
             $this->_secureUrlCache[$url] = false;
             $secureUrls = $this->getNode('frontend/secure_url');
