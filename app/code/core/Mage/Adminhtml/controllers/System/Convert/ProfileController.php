1b175994f6c8dc04e3c77dc8860060526609c68c
Colin Mollenhour <colin@mollenhour.com>
2018-10-12 17:52:31 -0400

    Fixes empty dataflow profile history, closes #285 #428 (#544)
    Thanks @sreichel!

--- app/code/core/Mage/Adminhtml/controllers/System/Convert/ProfileController.php
+++ app/code/core/Mage/Adminhtml/controllers/System/Convert/ProfileController.php
@@ -52,6 +52,7 @@ class Mage_Adminhtml_System_Convert_ProfileController extends Mage_Adminhtml_Con
             }
         }
 
+        $profile->setAdminUserId(Mage::getSingleton('admin/session')->getUser()->getId());
         Mage::register('current_convert_profile', $profile);
 
         return $this;
