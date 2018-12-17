895df67ec2ee73a3fb214c02b5330ee4b2105ea4
Simon Sprankel <sprankhub@users.noreply.github.com>
2018-12-17 09:53:37 +0100

    fixed return value of saveCache method

--- app/code/core/Mage/Core/Model/Layout/Update.php
+++ app/code/core/Mage/Core/Model/Layout/Update.php
@@ -210,13 +210,13 @@ class Mage_Core_Model_Layout_Update
         // Cache key is sha1 hash of actual XML string
         $hash = sha1($str);
         $tags[] = self::LAYOUT_GENERAL_CACHE_TAG;
-        Mage::app()->saveCache($hash, $this->getCacheId(), $tags, null);
+        $returnValue = Mage::app()->saveCache($hash, $this->getCacheId(), $tags, null);
 
         // Only save actual XML to cache if it doesn't already exist
         if ( ! Mage::app()->getCache()->test(self::XML_KEY_PREFIX . $hash)) {
-            Mage::app()->saveCache($str, self::XML_KEY_PREFIX . $hash, $tags, null);
+            $returnValue = Mage::app()->saveCache($str, self::XML_KEY_PREFIX . $hash, $tags, null);
         }
-        return TRUE;
+        return $returnValue;
     }
 
     /**
