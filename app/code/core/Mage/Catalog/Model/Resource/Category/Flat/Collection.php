39224086330557f4de82495c2c825132c631af24
Tymoteusz Motylewski <t.motylewski@gmail.com>
2019-01-26 16:55:07 +0100

    Prevent double firing events in Category_Flat_Collection
    The events catalog_category_collection_load_after and catalog_category_collection_load_before
    are already fired in the parent class, so they can be removed from Mage_Catalog_Model_Resource_Category_Flat_Collection

--- app/code/core/Mage/Catalog/Model/Resource/Category/Flat/Collection.php
+++ app/code/core/Mage/Catalog/Model/Resource/Category/Flat/Collection.php
@@ -127,31 +127,6 @@ class Mage_Catalog_Model_Resource_Category_Flat_Collection extends Mage_Core_Mod
     }
 
     /**
-     * Before collection load
-     *
-     * @return Mage_Catalog_Model_Resource_Category_Flat_Collection
-     */
-    protected function _beforeLoad()
-    {
-        Mage::dispatchEvent($this->_eventPrefix . '_load_before',
-                            array($this->_eventObject => $this));
-        return parent::_beforeLoad();
-    }
-
-    /**
-     * After collection load
-     *
-     * @return Mage_Catalog_Model_Resource_Category_Flat_Collection
-     */
-    protected function _afterLoad()
-    {
-        Mage::dispatchEvent($this->_eventPrefix . '_load_after',
-                            array($this->_eventObject => $this));
-
-        return parent::_afterLoad();
-    }
-
-    /**
      * Set store id
      *
      * @param integer $storeId
