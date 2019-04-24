ed0c573576d5e02ddba382b2ff7daa5d9b6baf3c
k.anev <hodollsoft@gmail.com>
2019-04-24 17:30:18 +0300

    fix spacing

--- app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
+++ app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
@@ -140,7 +140,7 @@ class Mage_Core_Model_Session_Abstract_Varien extends Varien_Object
             // secure cookie check to prevent MITM attack
             $secureCookieName = $sessionName . '_cid';
             if (isset($_SESSION[self::SECURE_COOKIE_CHECK_KEY])) {
-				$cookieValue = $cookie->get($secureCookieName);
+                $cookieValue = $cookie->get($secureCookieName);
 				
                 if (!is_string($cookieValue) || $_SESSION[self::SECURE_COOKIE_CHECK_KEY] !== md5($cookieValue)) {
                     session_regenerate_id(false);
