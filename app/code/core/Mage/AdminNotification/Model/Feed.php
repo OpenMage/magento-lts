e55e0c8bd3b1da08ef35dcfc37d286a56a5bce93
Mark <mark@netalico.com>
2019-08-13 11:50:41 -0400

    Misc revisions

--- app/code/core/Mage/AdminNotification/Model/Feed.php
+++ app/code/core/Mage/AdminNotification/Model/Feed.php
@@ -20,7 +20,7 @@
  *
  * @category    Mage
  * @package     Mage_AdminNotification
- * @copyright  Copyright (c) 2006-2018 Magento, Inc. (http://www.magento.com)
+ * @copyright  Copyright (c) 2006-2019 Magento, Inc. (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -68,7 +68,7 @@ class Mage_AdminNotification_Model_Feed extends Mage_Core_Model_Abstract
     /**
      * Check feed for modification
      *
-     * @return Mage_AdminNotification_Model_Feed
+     * @return $this
      */
     public function checkUpdate()
     {
@@ -136,7 +136,7 @@ class Mage_AdminNotification_Model_Feed extends Mage_Core_Model_Abstract
     /**
      * Set last update time (now)
      *
-     * @return Mage_AdminNotification_Model_Feed
+     * @return $this
      */
     public function setLastUpdate()
     {
