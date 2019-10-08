deb813cf7563d2fe5ff9674b7a5e792ff09bcda0
Lubos Hubacek <lubos.hubacek@ateli.cz>
2019-10-08 11:34:44 +0200

    Fix getUrl - throws better exception

--- app/code/core/Mage/Core/Model/Url.php
+++ app/code/core/Mage/Core/Model/Url.php
@@ -362,6 +362,7 @@ class Mage_Core_Model_Url extends Varien_Object
         catch (Mage_Core_Model_Store_Exception $e) {
             // store not found (e.g. on developer server)
             Mage::logException($e);
+            throw new Mage_Core_Exception("Store with id {$data} was not found!");
         }
 
         return $this;
