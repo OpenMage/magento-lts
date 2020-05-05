af93ae380cd270e04dd55ddec6b8a1633b27ef32
Colin Mollenhour <colin@mollenhour.com>
2020-05-05 19:25:14 -0400

    Remove more Magento Connect files.

--- lib/Mage/HTTP/Client/Socket.php
+++ lib/Mage/HTTP/Client/Socket.php
@@ -28,7 +28,7 @@
  * Class to work with HTTP protocol using sockets
  *
  * @category    Mage
- * @package     Mage_Connect
+ * @package     Mage_HTTP
  * @author      Magento Core Team <core@magentocommerce.com>
  */
 class Mage_HTTP_Client_Socket
