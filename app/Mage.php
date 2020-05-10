ef443a743914fc493ac651865657c3873ef63790
Daniel Fahlke <flyingmana@googlemail.com>
2020-05-10 11:52:38 +0200

    bump version to 19.4.3

--- app/Mage.php
+++ app/Mage.php
@@ -215,7 +215,7 @@ final class Mage
         return array(
             'major'     => '19',
             'minor'     => '4',
-            'patch'     => '2',
+            'patch'     => '3',
             'stability' => '', // beta,alpha,rc
             'number'    => '', // 1,2,3,0.3.7,x.7.z.92 @see https://semver.org/#spec-item-9
         );
