2b785cb6218082197be325af5447524ff7671347
spinsch <romano.marc@gmail.com>
2020-05-13 09:01:13 +0200

    refactoring

--- app/code/core/Mage/Checkout/Block/Onepage/Shipping.php
+++ app/code/core/Mage/Checkout/Block/Onepage/Shipping.php
@@ -69,13 +69,7 @@ class Mage_Checkout_Block_Onepage_Shipping extends Mage_Checkout_Block_Onepage_A
     public function getAddress()
     {
         if (is_null($this->_address)) {
-            if ($this->isCustomerLoggedIn()) {
-                $this->_address = $this->getQuote()->getShippingAddress();
-            } else if ($this->getQuote()->getCustomerIsGuest() || !$this->getQuote()->getCustomerId()) {
-                $this->_address = $this->getQuote()->getShippingAddress();
-            } else {
-                $this->_address = Mage::getModel('sales/quote_address');
-            }
+            $this->_address = $this->getQuote()->getShippingAddress();
         }
 
         return $this->_address;
