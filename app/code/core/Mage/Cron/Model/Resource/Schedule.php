88a53be364cbed870c06d2bf97fdd166a87643d7
sv3n <github-sr@hotmail.com>
2020-05-14 04:09:42 +0200

    Mage_Cron - DOC block update (#697)
    * Mage_Cron - DOC block update
    
    * Reverted phpstan related change
    
    * Mini PSR2 update

--- app/code/core/Mage/Cron/Model/Resource/Schedule.php
+++ app/code/core/Mage/Cron/Model/Resource/Schedule.php
@@ -48,10 +48,10 @@ class Mage_Cron_Model_Resource_Schedule extends Mage_Core_Model_Resource_Db_Abst
      * and return true. Otherwise, return false and do not change the job.
      * This method is used to implement locking for cron jobs.
      *
-     * @param unknown_type $scheduleId
+     * @param int $scheduleId
      * @param String $newStatus
      * @param String $currentStatus
-     * @return unknown
+     * @return bool
      */
     public function trySetJobStatusAtomic($scheduleId, $newStatus, $currentStatus)
     {
