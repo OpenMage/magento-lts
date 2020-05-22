c84ff3e9ef6957373adf3d3e5ac42f921f3e18e4
Colin Mollenhour <colin@mollenhour.com>
2020-05-22 05:15:02 -0400

    Update app/code/core/Mage/Sales/Model/Order/Total/Config/Base.php

--- app/code/core/Mage/Sales/Model/Order/Total/Config/Base.php
+++ app/code/core/Mage/Sales/Model/Order/Total/Config/Base.php
@@ -61,7 +61,7 @@ class Mage_Sales_Model_Order_Total_Config_Base extends Mage_Sales_Model_Config_O
      * @param string $class
      * @param string $totalCode
      * @param array $totalConfig
-     * @return false|Mage_Core_Model_Abstract
+     * @return Mage_Sales_Model_Order_Total_Abstract
      * @throws Mage_Core_Exception
      */
     protected function _initModelInstance($class, $totalCode, $totalConfig)
