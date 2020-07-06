6c8a6998daa24fb2b4d89c007266399cc56427a1
Julien Loizelet <julienloizelet@okaeli.com>
2020-07-06 05:15:48 +0200

    Added dash(-) as an allowed character for store code

--- app/code/core/Mage/Core/Model/Resource/Store.php
+++ app/code/core/Mage/Core/Model/Resource/Store.php
@@ -64,9 +64,9 @@ class Mage_Core_Model_Resource_Store extends Mage_Core_Model_Resource_Db_Abstrac
      */
     protected function _beforeSave(Mage_Core_Model_Abstract $model)
     {
-        if (!preg_match('/^[a-z]+[a-z0-9_]*$/', $model->getCode())) {
+        if (!preg_match('/^[a-z]+[a-z0-9_\-]*$/', $model->getCode())) {
             Mage::throwException(
-                Mage::helper('core')->__('The store code may contain only letters (a-z), numbers (0-9) or underscore(_), the first character must be a letter')
+                Mage::helper('core')->__('The store code may contain only letters (a-z), numbers (0-9), underscore(_) or dash(-), the first character must be a letter')
             );
         }
 
