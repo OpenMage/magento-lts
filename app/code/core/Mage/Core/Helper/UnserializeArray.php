851c9edc0ab2a0e847094002195eb99e6bd341c3
Colin Mollenhour <colin@mollenhour.com>
2020-11-06 18:57:08 -0500

    Re-throw unserialize errors as Exception.

--- app/code/core/Mage/Core/Helper/UnserializeArray.php
+++ app/code/core/Mage/Core/Helper/UnserializeArray.php
@@ -40,6 +40,14 @@ class Mage_Core_Helper_UnserializeArray
      */
     public function unserialize($str)
     {
-        return unserialize($str, ['allowed_classes' => false]);
+        try {
+            $result = unserialize($str, ['allowed_classes' => false]);
+            if ($result === false && $str !== serialize(false)) {
+                throw new Exception('Error unserializing data.');
+            }
+            return $result;
+        } catch (Error $e) {
+            throw new Exception('Error unserializing data: '.$e->getMessage(), 0, $e);
+        }
     }
 }
