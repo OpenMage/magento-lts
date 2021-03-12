9ff6fd19939a206e4776d88614ebde5447352d7c
Lubos Hubacek <lubos.hubacek@ateli.cz>
2021-03-12 12:06:19 +0100

    Removed replacing of variable from parent

--- app/code/core/Mage/Directory/Model/Resource/Country/Collection.php
+++ app/code/core/Mage/Directory/Model/Resource/Country/Collection.php
@@ -34,11 +34,6 @@
 class Mage_Directory_Model_Resource_Country_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
 {
     /**
-     * @var Mage_Directory_Model_Country[]
-     */
-    protected $_items;
-
-    /**
      * Define main table
      */
     protected function _construct()
