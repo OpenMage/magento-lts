d879d605aabc5f1160b49f2e36beae9eb94dc380
Fabrizio Balliano <fabrizio@fabrizioballiano.com>
2021-04-02 19:34:13 +0100

    Deleted all the store switcher hints that linked to old magento.com documentation

--- app/code/core/Mage/Adminhtml/Block/Poll/Edit/Tab/Form.php
+++ app/code/core/Mage/Adminhtml/Block/Poll/Edit/Tab/Form.php
@@ -68,8 +68,7 @@ class Mage_Adminhtml_Block_Poll_Edit_Tab_Form extends Mage_Adminhtml_Block_Widge
                 'required'  => true,
                 'name'      => 'store_ids[]',
                 'values'    => Mage::getSingleton('adminhtml/system_store')->getStoreValuesForForm(),
-                'value'     => Mage::registry('poll_data')->getStoreIds(),
-                'after_element_html' => Mage::getBlockSingleton('adminhtml/store_switcher')->getHintHtml()
+                'value'     => Mage::registry('poll_data')->getStoreIds()
             ));
         }
         else {
