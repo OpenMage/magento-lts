237277ee134f5b655642439d2002c1b96af81d68
Milan Davídek <midlan@users.noreply.github.com>
2021-06-17 18:20:40 +0200

    Fixed phpdoc
    Fixed phpdoc of Mage_Core_Model_Resource_Db_Collection_Abstract::addExpressionFieldToSelect

--- app/code/core/Mage/Core/Model/Resource/Db/Collection/Abstract.php
+++ app/code/core/Mage/Core/Model/Resource/Db/Collection/Abstract.php
@@ -333,13 +333,13 @@ abstract class Mage_Core_Model_Resource_Db_Collection_Abstract extends Varien_Da
 
     /**
      * Add attribute expression (SUM, COUNT, etc)
-     * Example: ('sub_total', 'SUM({{attribute}})', 'revenue')
+     * Example: ('sub_total', 'SUM({{attribute}})', array('attribute' => 'revenue'))
      * Example: ('sub_total', 'SUM({{revenue}})', 'revenue')
      * For some functions like SUM use groupByAttribute.
      *
      * @param string $alias
      * @param string $expression
-     * @param array $fields
+     * @param array|string $fields
      * @return $this
      */
     public function addExpressionFieldToSelect($alias, $expression, $fields)
