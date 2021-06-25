4bd2476b258855ae7acc394a7fc31a607a2b5a23
Daniel Fahlke <flyingmana@googlemail.com>
2021-06-25 22:07:53 +0200

    Bump Version - align version with 20.0 branch

--- app/Mage.php
+++ app/Mage.php
@@ -222,7 +222,7 @@ final class Mage
             return array(
                 'major'     => '20',
                 'minor'     => '0',
-                'patch'     => '10',
+                'patch'     => '12',
                 'stability' => '', // beta,alpha,rc
                 'number'    => '', // 1,2,3,0.3.7,x.7.z.92 @see https://semver.org/#spec-item-9
             );
