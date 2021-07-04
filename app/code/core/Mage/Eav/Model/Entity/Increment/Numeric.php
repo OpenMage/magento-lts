1e2e8947b9d186975d7c614bf4800798c057684e
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2021-07-04 15:56:11 +0100

    Handle empty Order increment prefix
    A call to `strpos` with an empty needle would throw a warning pre PHP 8.0. This change will check if the prefix is not empty first before performing the call to `strpos`.

--- app/code/core/Mage/Eav/Model/Entity/Increment/Numeric.php
+++ app/code/core/Mage/Eav/Model/Entity/Increment/Numeric.php
@@ -45,7 +45,7 @@ class Mage_Eav_Model_Entity_Increment_Numeric extends Mage_Eav_Model_Entity_Incr
 
         if (empty($last)) {
             $last = 0;
-        } else if (strpos($last, (string)$this->getPrefix()) === 0) {
+        } else if (!empty($prefix = (string)$this->getPrefix()) && strpos($last, $prefix) === 0) {
             $last = (int)substr($last, strlen($this->getPrefix()));
         } else {
             $last = (int)$last;
