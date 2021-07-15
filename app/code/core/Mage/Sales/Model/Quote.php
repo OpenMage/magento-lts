5b0bcad5fce56b80e9e4187fb4ae0a0f04d9438e
Wouter Samaey <wouter.samaey@storefront.be>
2021-07-15 15:44:06 +0200

    Fixed origData is empty in a quote
    `_origData` is empty when quotes are loaded though 
    - loadByIdWithoutStore()
    - loadActive()
    - loadByCustomer()
    When loading a quote with load(), then `_origData` is available, but only then.

--- app/code/core/Mage/Sales/Model/Quote.php
+++ app/code/core/Mage/Sales/Model/Quote.php
@@ -416,6 +416,7 @@ class Mage_Sales_Model_Quote extends Mage_Core_Model_Abstract
         }
         $this->_getResource()->loadByCustomerId($this, $customerId);
         $this->_afterLoad();
+        $this->setOrigData();
         return $this;
     }
 
@@ -429,6 +430,7 @@ class Mage_Sales_Model_Quote extends Mage_Core_Model_Abstract
     {
         $this->_getResource()->loadActive($this, $quoteId);
         $this->_afterLoad();
+        $this->setOrigData();
         return $this;
     }
 
@@ -442,6 +444,7 @@ class Mage_Sales_Model_Quote extends Mage_Core_Model_Abstract
     {
         $this->_getResource()->loadByIdWithoutStore($this, $quoteId);
         $this->_afterLoad();
+        $this->setOrigData();
         return $this;
     }
 
