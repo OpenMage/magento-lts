76ae07ca13d2e2b7355302124f23d28717f760c3
Sven Reichel <github-sr@hotmail.com>
2021-10-05 18:03:23 +0200

    Change fulltext search separator (OR/AND) from backend

---
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Source/Catalog/Search/Separator.php
@@ -0,0 +1,20 @@
+<?php
+
+class Mage_Adminhtml_Model_System_Config_Source_Catalog_Search_Separator
+{
+    public function toOptionArray()
+    {
+        $types = [
+            ' OR ' => 'OR (default)',
+            ' AND ' => 'AND'
+        ];
+        $options = [];
+        foreach ($types as $k => $v) {
+            $options[] = [
+                'value' => $k,
+                'label' => $v
+            ];
+        }
+        return $options;
+    }
+}
