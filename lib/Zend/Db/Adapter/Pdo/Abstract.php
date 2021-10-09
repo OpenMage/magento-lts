11fa00b6a778d2c99ba5a8501133b98a6f1f3b04
Fabrice Creuzot <code@luigifab.fr>
2021-10-09 14:22:27 +0200

    Fix deprecated functionality: addcslashes(): passing null to parameter #1 ($string) of type string is deprecated

--- lib/Zend/Db/Adapter/Pdo/Abstract.php
+++ lib/Zend/Db/Adapter/Pdo/Abstract.php
@@ -289,6 +289,9 @@ abstract class Zend_Db_Adapter_Pdo_Abstract extends Zend_Db_Adapter_Abstract
      */
     protected function _quote($value)
     {
+        if (is_null($value)) {
+            return "''";
+        }
         if (is_int($value) || is_float($value)) {
             return $value;
         }
