OpenMage/magento-lts

commit f06d2bf0eb1a699269436663ec0466bd32c2e68f

_exportIterateCollection performance optimization (#1467)
* _exportIterateCollection performance optimization

* Update _exportIterateCollection

app/code/core/Mage/Adminhtml/Block/Widget/Grid.php
@@ -963,36 +963,29 @@ class Mage_Adminhtml_Block_Widget_Grid extends Mage_Adminhtml_Block_Widget
      *
      * @param string $callback
      * @param array $args additional arguments for callback method
-     * @return $this
      */
     public function _exportIterateCollection($callback, array $args)
     {
         $originalCollection = $this->getCollection();
         $count = null;
         $page  = 1;
-        $lPage = null;
-        $break = false;
 
-        while ($break !== true) {
+        do {
             $collection = clone $originalCollection;
             $collection->setPageSize($this->_exportPageSize);
             $collection->setCurPage($page);
             $collection->load();
-            if (is_null($count)) {
-                $count = $collection->getSize();
-                $lPage = $collection->getLastPageNumber();
-            }
-            if ($lPage == $page) {
-                $break = true;
-            }
-            $page ++;
+
+            $count = $collection->count();
+
+            $page++;
 
             foreach ($collection as $item) {
                 call_user_func_array(array($this, $callback), array_merge(array($item), $args));
             }
             $collection->clear();
             unset($collection);
-        }
+        } while($count == $this->_exportPageSize);
     }
 
     /**

commit 9d9a6658669f3fec993b8f761fa176aa165572dd

Fixes infinite loop caused by #1467
The infinite loop happens when you try to export collection that has number of items divisible by 1000(`$this->_exportPageSize`).
In this case the Varien collection will reset the page number to `lastPageNumber` when `$page` is higher.
`$page` is higher than `lastPageNumber` because the current logic assume that there is always one more page if the current page count is the same as page export size which is not true in case when count is divisible by page size.

app/code/core/Mage/Adminhtml/Block/Widget/Grid.php
@@ -969,7 +969,6 @@ class Mage_Adminhtml_Block_Widget_Grid extends Mage_Adminhtml_Block_Widget
     public function _exportIterateCollection($callback, array $args)
     {
         $originalCollection = $this->getCollection();
-        $count = null;
         $page  = 1;
 
         do {
@@ -978,7 +977,7 @@ class Mage_Adminhtml_Block_Widget_Grid extends Mage_Adminhtml_Block_Widget
             $collection->setCurPage($page);
             $collection->load();
 
-            $count = $collection->count();
+            $lastPageNumber = $collection->getLastPageNumber();
 
             $page++;
 
@@ -987,7 +986,7 @@ class Mage_Adminhtml_Block_Widget_Grid extends Mage_Adminhtml_Block_Widget
             }
             $collection->clear();
             unset($collection);
-        } while($count == $this->_exportPageSize);
+        } while ($page <= $lastPageNumber);
     }
 
     /**