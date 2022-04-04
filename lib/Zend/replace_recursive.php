70b1b2e7409bad96f26ba4359293d3d793734174
Fabrice Creuzot <code@luigifab.fr>
2022-04-04 20:00:00 +0000

    Do not comment require_once $path of Application.php

--- lib/Zend/replace_recursive.php
+++ lib/Zend/replace_recursive.php
@@ -1,18 +1,22 @@
 <?php
 
+chdir(__DIR__);
 replace_recursive('.');
 
 function replace_recursive($dir)
 {
     $files = glob($dir.'/*');
     foreach ($files as $file) {
+
         if ($file=='.' || $file=='..') {
             continue;
         }
+
         if (is_dir($file)) {
             replace_recursive($file);
             continue;
         }
+
         if (substr($file, -4)!=='.php') {
             continue;
         }
@@ -22,17 +26,23 @@ function replace_recursive($dir)
         }
 
         $orig = file_get_contents($file);
-
         $replaced = preg_replace("/([^#])require_once/", "\$1#require_once", $orig);
 
-        if (strpos($file, 'Locale/Math.php')!==false) {
+        if (strpos($file, 'Locale/Math.php') !== false) {
             $replaced = str_replace(
                 "#require_once 'Zend/Locale/Math/PhpMath.php';",
                 "require_once 'Zend/Locale/Math/PhpMath.php';",
                 $replaced);
         }
 
-        if (strcmp($orig, $replaced)===0) {
+        if (strpos($file, '/Application.php') !== false) {
+            $replaced = str_replace(
+                '#require_once $path;',
+                'require_once $path;',
+                $replaced);
+        }
+
+        if (strcmp($orig, $replaced) === 0) {
             continue;
         }
 
