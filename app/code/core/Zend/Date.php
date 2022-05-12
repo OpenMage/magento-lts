bcb83908c435a6e8281132c90efa176ab8a50cc5
MarcinNowakMacopedia <61323809+MarcinNowakMacopedia@users.noreply.github.com>
2022-05-12 22:17:01 +0200

    Prevent Zend_Date from checking locale for know date codes (#2050)

--- app/code/core/Zend/Date.php
+++ app/code/core/Zend/Date.php
@@ -544,12 +544,12 @@ class Zend_Date extends Zend_Date_DateObject
         }
 
         if (($part !== null) && !defined($part)
-            && ($part != 'ee') && ($part != 'ss') && ($part != 'GG') && ($part != 'MM') && ($part != 'EE') && ($part != 'TT')
+            && ($part !== 'ee') && ($part !== 'ss') && ($part !== 'GG') && ($part !== 'MM') && ($part !== 'EE') && ($part !== 'TT')
+            && ($part !== 'U') && ($part !== 'X') && ($part !== 'c')
             && Zend_Locale::isLocale($part, null, false)) {
             $locale = $part;
             $part = null;
         }
-
         if ($part === null) {
             $part = self::TIMESTAMP;
         } else if (self::$_options['format_type'] == 'php') {
