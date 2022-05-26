6a5f004ca959ea77a83d547d6b48f1f15ca2a37a
Justin Beaty <51970393+justinbeaty@users.noreply.github.com>
2022-05-26 09:35:40 -0700

    Fix protect code changing on order save (#1422)

--- app/code/core/Mage/Sales/Model/Order.php
+++ app/code/core/Mage/Sales/Model/Order.php
@@ -2349,7 +2349,9 @@ class Mage_Sales_Model_Order extends Mage_Sales_Model_Abstract
             $this->unsShippingAddressId();
         }
 
-        $this->setData('protect_code', substr(md5(uniqid(mt_rand(), true) . ':' . microtime(true)), 5, 6));
+        if (!$this->getId()) {
+            $this->setData('protect_code', substr(md5(uniqid(mt_rand(), true) . ':' . microtime(true)), 5, 6));
+        }
         return $this;
     }
 
