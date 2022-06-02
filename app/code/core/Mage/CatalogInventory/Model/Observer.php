f4d4dcb66d86dd0af1ca0a47ab0797bafd523043
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-06-02 09:23:40 +0100

    Revert "creditmemo bundle inventory calculation error" (#2180)

--- app/code/core/Mage/CatalogInventory/Model/Observer.php
+++ app/code/core/Mage/CatalogInventory/Model/Observer.php
@@ -793,7 +793,7 @@ class Mage_CatalogInventory_Model_Observer
                 $parentOrderId = $item->getOrderItem()->getParentItemId();
                 /* @var $parentItem Mage_Sales_Model_Order_Creditmemo_Item */
                 $parentItem = $parentOrderId ? $creditmemo->getItemByOrderId($parentOrderId) : false;
-                $qty = $item->getQty();
+                $qty = $parentItem ? ($parentItem->getQty() * $item->getQty()) : $item->getQty();
                 if (isset($items[$item->getProductId()])) {
                     $items[$item->getProductId()]['qty'] += $qty;
                 } else {
