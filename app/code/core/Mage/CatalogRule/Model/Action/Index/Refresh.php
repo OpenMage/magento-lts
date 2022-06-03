9808b168eef96d4c931fef0213449b56bd06c8b4
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-06-03 15:54:08 +0200

    Changed timestamp definition to fix issue #1166

--- app/code/core/Mage/CatalogRule/Model/Action/Index/Refresh.php
+++ app/code/core/Mage/CatalogRule/Model/Action/Index/Refresh.php
@@ -115,10 +115,7 @@ class Mage_CatalogRule_Model_Action_Index_Refresh
     public function execute()
     {
         $this->_app->dispatchEvent('catalogrule_before_apply', array('resource' => $this->_resource));
-
-        /** @var Mage_Core_Model_Date $coreDate */
-        $coreDate  = $this->_factory->getModel('core/date');
-        $timestamp = $coreDate->gmtTimestamp('Today');
+        $timestamp = Mage::app()->getLocale()->date(null, null, null,true)->get(Zend_Date::TIMESTAMP);
 
         foreach ($this->_app->getWebsites(false) as $website) {
             /** @var Mage_Core_Model_Website $website */
