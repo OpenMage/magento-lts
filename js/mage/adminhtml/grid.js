5a6f468f9a312b4e3953dccaea8b1cd3c4ce4d33
Justin Beaty <justin.beaty@gmail.com>
2022-08-14 16:43:15 -0700

    remove IE hack in grids

--- js/mage/adminhtml/grid.js
+++ js/mage/adminhtml/grid.js
@@ -180,27 +180,10 @@ varienGrid.prototype = {
                                 setLocation(response.ajaxRedirect);
                             }
                         } else {
-                            /**
-                             * For IE <= 7.
-                             * If there are two elements, and first has name, that equals id of second.
-                             * In this case, IE will choose one that is above
-                             *
-                             * @see https://prototype.lighthouseapp.com/projects/8886/tickets/994-id-selector-finds-elements-by-name-attribute-in-ie7
-                             */
-                            var divId = $(this.containerId);
-                            if (divId.id == this.containerId) {
-                                divId.update(responseText);
-                            } else {
-                                $$('div[id="'+this.containerId+'"]')[0].update(responseText);
-                            }
+                            $(this.containerId).update(responseText);
                         }
                     } catch (e) {
-                        var divId = $(this.containerId);
-                        if (divId.id == this.containerId) {
-                            divId.update(responseText);
-                        } else {
-                            $$('div[id="'+this.containerId+'"]')[0].update(responseText);
-                        }
+                        $(this.containerId).update(responseText);
                     }
                 }.bind(this)
             });
