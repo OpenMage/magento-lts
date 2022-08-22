6f5524dba085ba92a7a783885cc099de48394008
Justin Beaty <justin.beaty@gmail.com>
2022-08-22 15:42:46 -0700

    add note about fix-defer.js

--- js/extjs/fix-defer.js
+++ js/extjs/fix-defer.js
@@ -17,6 +17,15 @@
  * @license     https://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
+/*
+ * Both ExtJS and PrototypeJS write to Function.prototype.defer
+ * However, PrototypeJS has a default delay of 0.01s if no first argument is provided
+ * Ref: https://github.com/prototypejs/prototype/blob/1.7.3/src/prototype/lang/function.js#L292-L295
+ * While ExtJS executes the function immediately. Presumably this causes an error for
+ * PrototypeJS Ajax calls.
+ *
+ */
+
 (function(){
     var eDefer = Function.prototype.defer;
     Function.prototype.defer = function() {
