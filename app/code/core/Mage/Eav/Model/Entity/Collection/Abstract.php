51edd941879cf1057416dc5f80b932774bb062b3
sv3n <github-sr@hotmail.com>
2022-08-25 09:01:00 +0200

    Update app/code/core/Mage/Eav/Model/Entity/Collection/Abstract.php
    Co-authored-by: Ng Kiat Siong <kiatsiong.ng@gmail.com>

--- app/code/core/Mage/Eav/Model/Entity/Collection/Abstract.php
+++ app/code/core/Mage/Eav/Model/Entity/Collection/Abstract.php
@@ -1332,7 +1332,7 @@ abstract class Mage_Eav_Model_Entity_Collection_Abstract extends Varien_Data_Col
         /**
          * process join type
          */
-        $joinMethod = ($joinType === 'left') ? 'joinLeft' : 'join';
+        $joinMethod = $joinType === 'left' ? 'joinLeft' : 'join';
 
         $this->_joinAttributeToSelect($joinMethod, $attribute, $attrTable, $condArr, $attributeCode, $attrFieldName);
 
