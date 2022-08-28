82bebd2199dd6261e6103442f12efdc35361258f
sv3n <github-sr@hotmail.com>
2022-08-28 19:25:35 +0200

    Update app/code/core/Mage/Dataflow/Model/Convert/Parser/Abstract.php
    Co-authored-by: Ng Kiat Siong <kiatsiong.ng@gmail.com>

--- app/code/core/Mage/Dataflow/Model/Convert/Parser/Abstract.php
+++ app/code/core/Mage/Dataflow/Model/Convert/Parser/Abstract.php
@@ -87,7 +87,7 @@ abstract class Mage_Dataflow_Model_Convert_Parser_Abstract
     /**
      * Retrieve Batch import model
      *
-     * @return Mage_Dataflow_Model_Import
+     * @return Mage_Dataflow_Model_Batch_Import
      */
     public function getBatchImportModel()
     {
