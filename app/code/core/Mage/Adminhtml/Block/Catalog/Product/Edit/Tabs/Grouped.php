a936d6294c47d6ae4017a269069c91724dc46a2c
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-29 10:45:10 +0100

    Fixed whitespace in docblock for Mage_Adminhtml (#2514)

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tabs/Grouped.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tabs/Grouped.php
@@ -12,8 +12,8 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Adminhtml
+ * @category   Mage
+ * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -21,9 +21,9 @@
 /**
  * admin edit tabs for grouped product
  *
- * @category    Mage
- * @package     Mage_Adminhtml
- * @author      Magento Core Team <core@magentocommerce.com>
+ * @category   Mage
+ * @package    Mage_Adminhtml
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Adminhtml_Block_Catalog_Product_Edit_Tabs_Grouped extends Mage_Adminhtml_Block_Catalog_Product_Edit_Tabs
 {
