007ca5563d4a40d1e99ea4f2958c0facd4b831b6
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-29 10:15:58 +0100

    whitespace for mage_adminhtml

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Create/Form/Abstract.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Create/Form/Abstract.php
@@ -12,8 +12,8 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Adminhtml
+ * @category   Mage
+ * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -21,9 +21,9 @@
 /**
  * Sales Order Create Form Abstract Block
  *
- * @category    Mage
- * @package     Mage_Adminhtml
- * @author      Magento Core Team <core@magentocommerce.com>
+ * @category   Mage
+ * @package    Mage_Adminhtml
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
 abstract class Mage_Adminhtml_Block_Sales_Order_Create_Form_Abstract
     extends Mage_Adminhtml_Block_Sales_Order_Create_Abstract
