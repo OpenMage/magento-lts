0487e85bddf9eb162e76a6d371f5738a1386da44
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-29 10:53:07 +0100

    Removed useless constructors (#2515)

--- app/code/core/Mage/Catalog/Block/Product/Send.php
+++ app/code/core/Mage/Catalog/Block/Product/Send.php
@@ -27,11 +27,6 @@
  */
 class Mage_Catalog_Block_Product_Send extends Mage_Catalog_Block_Product_Abstract
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     /**
      * Retrieve username for form field
      *
