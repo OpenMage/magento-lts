e6e5ad7e908b37918d700a3cdba2042678bd7c80
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-31 10:14:14 +0100

    Fixed whitespace for docblocks (#2528)

--- app/code/core/Mage/Paypal/Model/Method/Agreement.php
+++ app/code/core/Mage/Paypal/Model/Method/Agreement.php
@@ -12,16 +12,18 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Paypal
+ * @category   Mage
+ * @package    Mage_Paypal
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Paypal Billing Agreement method
  *
- * @author Magento Core Team <core@magentocommerce.com>
+ * @category   Mage
+ * @package    Mage_Paypal
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Paypal_Model_Method_Agreement extends Mage_Sales_Model_Payment_Method_Billing_AgreementAbstract
     implements Mage_Payment_Model_Billing_Agreement_MethodInterface
--- app/code/core/Mage/Paypal/Model/System/Config/Source/AuthorizationAmounts.php
+++ app/code/core/Mage/Paypal/Model/System/Config/Source/AuthorizationAmounts.php
@@ -12,19 +12,19 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Paypal
+ * @category   Mage
+ * @package    Mage_Paypal
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Source model for available Authorization Amounts for Account Verification
  *
+ * @category   Mage
+ * @package    Mage_Paypal
+ * @author     Magento Core Team <core@magentocommerce.com>
  * @deprecated since 1.6.2.0
- * @category    Mage
- * @package     Mage_Paypal
- * @author      Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Paypal_Model_System_Config_Source_AuthorizationAmounts
 {
