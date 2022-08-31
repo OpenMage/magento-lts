ce19bcdf2f4c59ee5146bb521dd99e5fd543d411
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-31 05:55:30 +0100

    Fixed whitespace for docblocks in modules (Mage_L* -> Mage_P*) (#2525)

--- app/code/core/Mage/Log/Model/Visitor/Online.php
+++ app/code/core/Mage/Log/Model/Visitor/Online.php
@@ -12,14 +12,18 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Log
+ * @category   Mage
+ * @package    Mage_Log
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Prepare Log Online Visitors Model
+ *
+ * @category   Mage
+ * @package    Mage_Log
+ * @author     Magento Core Team <core@magentocommerce.com>
  *
  * @method Mage_Log_Model_Resource_Visitor_Online _getResource()
  * @method Mage_Log_Model_Resource_Visitor_Online getResource()
@@ -36,9 +40,6 @@
  * @method string getLastUrl()
  * @method $this setLastUrl(string $value)
  *
- * @category    Mage
- * @package     Mage_Log
- * @author      Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Log_Model_Visitor_Online extends Mage_Core_Model_Abstract
 {
