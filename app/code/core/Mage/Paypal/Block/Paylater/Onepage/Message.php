b0f7b1f35cd54f69e5d27666dd420ddfd40d78b3
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-31 14:16:01 +0100

    cosmetic changes

--- app/code/core/Mage/Paypal/Block/Paylater/Onepage/Message.php
+++ app/code/core/Mage/Paypal/Block/Paylater/Onepage/Message.php
@@ -3,11 +3,9 @@
  * Paypal Pay Later
  *
  * https://www.paypal.com/us/for-you/pay-in-4
- *
  * This empty class exists to facilitate easy 3rd party extending Onepage Messaging functionality
  *
  */
 class Mage_Paypal_Block_Paylater_Onepage_Message extends Mage_Paypal_Block_Paylater_Message
 {
-
 }
