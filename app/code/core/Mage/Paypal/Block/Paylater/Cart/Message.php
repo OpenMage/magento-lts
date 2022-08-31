be4bc6b554c6aba64ffa03f0816bccb646258ef7
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-31 14:15:12 +0100

    cosmetic changes

--- app/code/core/Mage/Paypal/Block/Paylater/Cart/Message.php
+++ app/code/core/Mage/Paypal/Block/Paylater/Cart/Message.php
@@ -3,11 +3,9 @@
  * Paypal Pay Later
  *
  * https://www.paypal.com/us/for-you/pay-in-4
- *
  * This empty class exists to facilitate easy 3rd party extending Cart Messaging functionality
  *
  */
 class Mage_Paypal_Block_Paylater_Cart_Message extends Mage_Paypal_Block_Paylater_Message
 {
-
 }
