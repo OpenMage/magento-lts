b3e230468d8bcd8d68d30a9304e764bacae9c85c
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-31 17:02:51 +0100

    Update Invoice.php

--- app/code/core/Mage/Sales/Model/Order/Invoice.php
+++ app/code/core/Mage/Sales/Model/Order/Invoice.php
@@ -824,7 +824,7 @@ class Mage_Sales_Model_Order_Invoice extends Mage_Sales_Model_Abstract
         }
 
         // Stop store emulation process
-        if (isset($initialEnvironmentInfo)) {
+        if (isset($appEmulation) && isset($initialEnvironmentInfo)) {
             $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
         }
 
