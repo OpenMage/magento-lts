6b2bd49f719fb10e1625b423ac269e270ac8d03a
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-31 10:14:14 +0100

    Fixed whitespace for docblocks (#2528)

--- app/code/core/Mage/Rule/Model/Condition/Product/Abstract.php
+++ app/code/core/Mage/Rule/Model/Condition/Product/Abstract.php
@@ -12,18 +12,18 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Rule
+ * @category   Mage
+ * @package    Mage_Rule
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Abstract Rule product condition data model
  *
- * @category Mage
- * @package Mage_Rule
- * @author Magento Core Team <core@magentocommerce.com>
+ * @category   Mage
+ * @package    Mage_Rule
+ * @author     Magento Core Team <core@magentocommerce.com>
  *
  * @method $this setAttributeOption(array $value)
  * @method string getJsFormObject()
--- app/code/core/Mage/Shipping/Model/Rate/Result/Abstract.php
+++ app/code/core/Mage/Shipping/Model/Rate/Result/Abstract.php
@@ -12,14 +12,19 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Shipping
+ * @category   Mage
+ * @package    Mage_Shipping
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Class Mage_Shipping_Model_Rate_Result_Abstract
+ *
+ * @category   Mage
+ * @package    Mage_Shipping
+ * @author     Magento Core Team <core@magentocommerce.com>
+ *
  * @method string getCarrier()
  * @method string getCarrierTitle()
  * @method string getMethodTitle()
