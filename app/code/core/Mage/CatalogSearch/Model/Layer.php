5ec89e56e87d63c06c538c1f55a96f0d05387081
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-08-31 11:18:58 +0100

    Get catalog search result collection from engine

--- app/code/core/Mage/CatalogSearch/Model/Layer.php
+++ app/code/core/Mage/CatalogSearch/Model/Layer.php
@@ -37,7 +37,7 @@ class Mage_CatalogSearch_Model_Layer extends Mage_Catalog_Model_Layer
         if (isset($this->_productCollections[$this->getCurrentCategory()->getId()])) {
             $collection = $this->_productCollections[$this->getCurrentCategory()->getId()];
         } else {
-            $collection = Mage::getResourceModel('catalogsearch/fulltext_collection');
+            $collection = Mage::helper('catalogsearch')->getEngine()->getResultCollection();
             $this->prepareProductCollection($collection);
             $this->_productCollections[$this->getCurrentCategory()->getId()] = $collection;
         }
