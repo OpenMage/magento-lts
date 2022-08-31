40b120cb3efd8d514795ac8d879eaa2078768aff
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-31 17:02:21 +0100

    Update Creditmemo.php

--- app/code/core/Mage/Sales/Model/Order/Creditmemo.php
+++ app/code/core/Mage/Sales/Model/Order/Creditmemo.php
@@ -789,7 +789,7 @@ class Mage_Sales_Model_Order_Creditmemo extends Mage_Sales_Model_Abstract
         }
 
         // Stop store emulation process
-        if (isset($initialEnvironmentInfo)) {
+        if (isset($appEmulation) && isset($initialEnvironmentInfo)) {
             $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
         }
 
