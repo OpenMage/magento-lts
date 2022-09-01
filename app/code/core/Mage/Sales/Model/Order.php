ddc5c4e39c84dea073f97249e0e9331b3623632e
luigifab <31816829+luigifab@users.noreply.github.com>
2022-09-01 06:56:46 +0200

    Merge isset
    Co-authored-by: sv3n <github-sr@hotmail.com>

--- app/code/core/Mage/Sales/Model/Order.php
+++ app/code/core/Mage/Sales/Model/Order.php
@@ -1404,7 +1404,7 @@ class Mage_Sales_Model_Order extends Mage_Sales_Model_Abstract
         }
 
         // Stop store emulation process
-        if (isset($appEmulation) && isset($initialEnvironmentInfo)) {
+        if (isset($appEmulation, $initialEnvironmentInfo)) {
             $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
         }
 
