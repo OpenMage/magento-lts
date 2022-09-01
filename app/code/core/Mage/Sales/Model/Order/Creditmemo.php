238f1f78adcca4c28c4cb8b693fd7f00b0776be3
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-09-01 18:01:18 +0100

    Fixed whitespace for docblocks (#2535)

--- app/code/core/Mage/Sales/Model/Order/Creditmemo.php
+++ app/code/core/Mage/Sales/Model/Order/Creditmemo.php
@@ -12,14 +12,18 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Sales
+ * @category   Mage
+ * @package    Mage_Sales
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Order creditmemo model
+ *
+ * @category   Mage
+ * @package    Mage_Sales
+ * @author     Magento Core Team <core@magentocommerce.com>
  *
  * @method Mage_Sales_Model_Resource_Order_Creditmemo _getResource()
  * @method Mage_Sales_Model_Resource_Order_Creditmemo getResource()
@@ -144,10 +148,6 @@
  * @method bool getOfflineRequested()
  * @method bool getPaymentRefundDisallowed()
  * @method $this setTotalQty(float $value)
- *
- * @category    Mage
- * @package     Mage_Sales
- * @author      Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Sales_Model_Order_Creditmemo extends Mage_Sales_Model_Abstract
 {
