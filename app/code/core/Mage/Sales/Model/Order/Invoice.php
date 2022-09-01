659b319e6b11675f040b00118a92c682fe6a4fc9
luigifab <31816829+luigifab@users.noreply.github.com>
2022-09-01 06:56:30 +0200

    Merge isset
    Co-authored-by: sv3n <github-sr@hotmail.com>

--- app/code/core/Mage/Sales/Model/Order/Invoice.php
+++ app/code/core/Mage/Sales/Model/Order/Invoice.php
@@ -824,7 +824,7 @@ class Mage_Sales_Model_Order_Invoice extends Mage_Sales_Model_Abstract
         }
 
         // Stop store emulation process
-        if (isset($appEmulation) && isset($initialEnvironmentInfo)) {
+        if (isset($appEmulation, $initialEnvironmentInfo)) {
             $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
         }
 
