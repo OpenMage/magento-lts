abbee575ec766e2211e0935c0d82beb82da63ce7
sv3n <github-sr@hotmail.com>
2022-09-02 03:36:39 +0200

    Update app/code/core/Zend/Validate/EmailAddress.php
    Co-authored-by: Ng Kiat Siong <kiatsiong.ng@gmail.com>

--- app/code/core/Zend/Validate/EmailAddress.php
+++ app/code/core/Zend/Validate/EmailAddress.php
@@ -324,7 +324,7 @@ class Zend_Validate_EmailAddress extends Zend_Validate_Abstract
     /**
      * Returns the set domainCheck option
      *
-     * @return string
+     * @return bool
      */
     public function getDomainCheck()
     {
