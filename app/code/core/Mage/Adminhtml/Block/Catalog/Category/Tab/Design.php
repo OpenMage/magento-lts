7f584c5717cbc721cba296e69068750638fb2a5f
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-09-02 01:34:27 +0100

    Fixed whitespace for docblocks

--- app/code/core/Mage/Adminhtml/Block/Catalog/Category/Tab/Design.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Category/Tab/Design.php
@@ -15,7 +15,13 @@
  * @category   Mage
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * @category   Mage
+ * @package    Mage_Adminhtml
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Adminhtml_Block_Catalog_Category_Tab_Design extends Mage_Adminhtml_Block_Catalog_Form
 {
