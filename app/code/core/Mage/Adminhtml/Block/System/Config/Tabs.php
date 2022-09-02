a0949d316e53f6e6a517043f585ef309a672f4d5
sv3n <github-sr@hotmail.com>
2022-09-02 03:36:47 +0200

    Update app/code/core/Mage/Adminhtml/Block/System/Config/Tabs.php
    Co-authored-by: Ng Kiat Siong <kiatsiong.ng@gmail.com>

--- app/code/core/Mage/Adminhtml/Block/System/Config/Tabs.php
+++ app/code/core/Mage/Adminhtml/Block/System/Config/Tabs.php
@@ -42,8 +42,8 @@ class Mage_Adminhtml_Block_System_Config_Tabs extends Mage_Adminhtml_Block_Widge
     }
 
     /**
-     * @param object $a
-     * @param object $b
+     * @param Mage_Core_Model_Config_Element $a
+     * @param Mage_Core_Model_Config_Element $b
      * @return int
      */
     protected function _sort($a, $b)
