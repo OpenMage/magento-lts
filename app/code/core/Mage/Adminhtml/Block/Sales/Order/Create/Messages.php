d525d9a89872c26ad46d9c374f8897528ab9a64a
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-09-03 11:45:46 +0100

    Fixed whitespace for docblocks

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Create/Messages.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Create/Messages.php
@@ -15,7 +15,7 @@
  * @category   Mage
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
@@ -27,11 +27,9 @@
  */
 class Mage_Adminhtml_Block_Sales_Order_Create_Messages extends Mage_Adminhtml_Block_Messages
 {
-
     public function _prepareLayout()
     {
         $this->addMessages(Mage::getSingleton('adminhtml/session_quote')->getMessages(true));
         return parent::_prepareLayout();
     }
-
 }
