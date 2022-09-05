a2d7f20902ebaf6cacd1e9d6d6899c56cc72860c
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-09-05 12:00:52 +0100

    Fixed whitespace for docblocks

--- app/code/core/Mage/Downloadable/sql/downloadable_setup/mysql4-upgrade-1.4.0.0-1.4.0.1.php
+++ app/code/core/Mage/Downloadable/sql/downloadable_setup/mysql4-upgrade-1.4.0.0-1.4.0.1.php
@@ -12,16 +12,16 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Downloadable
+ * @category   Mage
+ * @package    Mage_Downloadable
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /** @var Mage_Catalog_Model_Resource_Eav_Mysql4_Setup $installer */
 $installer = $this;
-
 $installer->startSetup();
+
 $installer->run("
 DROP TABLE IF EXISTS `{$installer->getTable('catalog/product_index_price')}_downloadable_idx`;
 
@@ -44,4 +44,5 @@ CREATE TABLE `{$installer->getTable('downloadable/product_price_indexer_tmp')}` 
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8;
 
 ");
+
 $installer->endSetup();
