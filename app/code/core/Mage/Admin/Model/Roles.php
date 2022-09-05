32c595c1e3e8641f5fede992362cf5840e10a604
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-09-05 16:12:27 +0100

    Fixed whitespace for docblocks

--- app/code/core/Mage/Admin/Model/Roles.php
+++ app/code/core/Mage/Admin/Model/Roles.php
@@ -15,7 +15,7 @@
  * @category   Mage
  * @package    Mage_Admin
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
@@ -51,9 +51,6 @@ class Mage_Admin_Model_Roles extends Mage_Core_Model_Abstract
      */
     protected $_eventPrefix = 'admin_roles';
 
-    /**
-     * Constructor
-     */
     protected function _construct()
     {
         $this->_init('admin/roles');
--- app/code/core/Mage/Core/Model/Url/Rewrite.php
+++ app/code/core/Mage/Core/Model/Url/Rewrite.php
@@ -12,14 +12,18 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Core
+ * @category   Mage
+ * @package    Mage_Core
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Url rewrite model class
+ *
+ * @category   Mage
+ * @package    Mage_Core
+ * @author     Magento Core Team <core@magentocommerce.com>
  *
  * @method Mage_Core_Model_Resource_Url_Rewrite _getResource()
  * @method Mage_Core_Model_Resource_Url_Rewrite getResource()
@@ -45,10 +49,6 @@
  * @method string|array getTags()
  * @method $this setTags(string|array $value)
  * @method bool hasCategoryId()
- *
- * @category    Mage
- * @package     Mage_Core
- * @author      Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Core_Model_Url_Rewrite extends Mage_Core_Model_Abstract implements Mage_Core_Model_Url_Rewrite_Interface
 {
