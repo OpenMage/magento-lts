43df76a5a6105a28d19322f4edf295fd249d8316
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-09-05 12:18:59 +0100

    Fixed whitespace for docblocks

--- app/code/core/Mage/Catalog/Block/Seo/Sitemap/Tree/Pager.php
+++ app/code/core/Mage/Catalog/Block/Seo/Sitemap/Tree/Pager.php
@@ -12,10 +12,10 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Catalog
+ * @category   Mage
+ * @package    Mage_Catalog
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
@@ -23,9 +23,8 @@
  *
  * @category   Mage
  * @package    Mage_Page
- * @author      Magento Core Team <core@magentocommerce.com>
- *
- * @todo        separate order, mode and pager
+ * @author     Magento Core Team <core@magentocommerce.com>
+ * @todo       Separate order, mode and pager
  */
 class Mage_Catalog_Block_Seo_Sitemap_Tree_Pager extends Mage_Page_Block_Html_Pager
 {
--- app/code/core/Mage/Sales/Model/Order/Payment.php
+++ app/code/core/Mage/Sales/Model/Order/Payment.php
@@ -12,14 +12,18 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Sales
+ * @category   Mage
+ * @package    Mage_Sales
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Order payment information
+ *
+ * @category   Mage
+ * @package    Mage_Sales
+ * @author     Magento Core Team <core@magentocommerce.com>
  *
  * @method Mage_Sales_Model_Resource_Order_Payment _getResource()
  * @method Mage_Sales_Model_Resource_Order_Payment getResource()
@@ -193,10 +197,6 @@
  * @method $this unsTransactionId()
  *
  * @method $this setVoidTransactionId(string $value)
- *
- * @category    Mage
- * @package     Mage_Sales
- * @author      Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Sales_Model_Order_Payment extends Mage_Payment_Model_Info
 {
