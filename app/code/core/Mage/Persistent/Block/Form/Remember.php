4b274b222c53ea7ceb232528bb5b97665ac1704a
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-09-05 15:58:48 +0100

    Fixed whitespace for docblocks

--- app/code/core/Mage/Persistent/Block/Form/Remember.php
+++ app/code/core/Mage/Persistent/Block/Form/Remember.php
@@ -12,20 +12,19 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magento.com so we can send you a copy immediately.
  *
- * @category    Mage
- * @package     Mage_Persistent
+ * @category   Mage
+ * @package    Mage_Persistent
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Remember Me block
  *
- * @category    Mage
- * @package     Mage_Persistent
- * @author      Magento Core Team <core@magentocommerce.com>
+ * @category   Mage
+ * @package    Mage_Persistent
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
-
 class Mage_Persistent_Block_Form_Remember extends Mage_Core_Block_Template
 {
     /**
