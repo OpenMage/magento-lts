153054c1fd430daaae554603e5b142b6b3252e83
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-09-05 11:16:16 +0100

    Fixed whitespace for docblocks

--- app/code/core/Mage/Eav/sql/eav_setup/mysql4-upgrade-0.7.8-0.7.9.php
+++ app/code/core/Mage/Eav/sql/eav_setup/mysql4-upgrade-0.7.8-0.7.9.php
@@ -15,15 +15,16 @@
  * @category   Mage
  * @package    Mage_Eav
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-$installer = $this;
 /** @var Mage_Eav_Model_Entity_Setup $installer */
-
+$installer  = $this;
 $installer->startSetup();
+
 $installer->run("
 ALTER TABLE {$installer->getTable('eav_attribute_group')}
     CHANGE `attribute_group_name` `attribute_group_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL default '';
 ");
+
 $installer->endSetup();
