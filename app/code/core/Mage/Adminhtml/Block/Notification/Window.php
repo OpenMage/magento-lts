bd4499859fdb7b7f6f65bd74a4610cc643b3a598
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-09-10 14:59:23 +0100

    Fix passing null to sprintf

--- app/code/core/Mage/Adminhtml/Block/Notification/Window.php
+++ app/code/core/Mage/Adminhtml/Block/Notification/Window.php
@@ -144,7 +144,7 @@ class Mage_Adminhtml_Block_Notification_Window extends Mage_Adminhtml_Block_Noti
         if (is_null($this->_severityIconsUrl)) {
             $this->_severityIconsUrl =
                 (Mage::app()->getFrontController()->getRequest()->isSecure() ? 'https://' : 'http://')
-                . sprintf(Mage::getStoreConfig(self::XML_SEVERITY_ICONS_URL_PATH), Mage::getVersion(),
+                . sprintf((string)Mage::getStoreConfig(self::XML_SEVERITY_ICONS_URL_PATH), Mage::getVersion(),
                     $this->getNoticeSeverity())
             ;
         }
