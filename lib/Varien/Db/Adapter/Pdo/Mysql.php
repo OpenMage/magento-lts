9a49aa121c9f5f2bb34c2e403dd15862916caf5a
Colin Mollenhour <colin@mollenhour.com>
2022-09-12 13:38:10 -0700

    Stripped null bytes from strings and filter conditions (#1430)

--- lib/Varien/Db/Adapter/Pdo/Mysql.php
+++ lib/Varien/Db/Adapter/Pdo/Mysql.php
@@ -3008,8 +3008,8 @@ class Varien_Db_Adapter_Pdo_Mysql extends Zend_Db_Adapter_Pdo_Mysql implements V
      */
     protected function _prepareQuotedSqlCondition($text, $value, $fieldName)
     {
-      $text = str_replace('{{fieldName}}', $fieldName, $text);
-      return $this->quoteInto($text, $value);
+        $sql = $this->quoteInto($text, str_replace("\0", '', $value));
+        return str_replace('{{fieldName}}', $fieldName, $sql);
     }
 
     /**
@@ -3023,7 +3023,7 @@ class Varien_Db_Adapter_Pdo_Mysql extends Zend_Db_Adapter_Pdo_Mysql implements V
      */
     protected function _transformStringSqlCondition($conditionKey, $value)
     {
-        $value = (string) $value;
+        $value = str_replace("\0", '', (string) $value);
         if ($value == '') {
             return ($conditionKey == 'seq') ? 'null' : 'notnull';
         } else {
@@ -3098,7 +3098,7 @@ class Varien_Db_Adapter_Pdo_Mysql extends Zend_Db_Adapter_Pdo_Mysql implements V
             case 'mediumtext':
             case 'text':
             case 'longtext':
-                $value  = (string)$value;
+                $value  = str_replace("\0", '', (string)$value);
                 if ($column['NULLABLE'] && $value == '') {
                     $value = null;
                 }
