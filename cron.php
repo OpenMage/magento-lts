1409fa92e4a0aae9906e4eecec5f19816aaa8ab0
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-09-29 07:54:47 +0100

    Improvements to cron scripts (#2380)

--- cron.php
+++ cron.php
@@ -18,9 +18,7 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-// Change current directory to the directory of current script
 chdir(__DIR__);
-
 require 'app/bootstrap.php';
 require 'app/Mage.php';
 
@@ -29,8 +27,7 @@ if (!Mage::isInstalled()) {
     exit;
 }
 
-// Only for urls
-// Don't remove this
+// Only for urls, don't remove this
 $_SERVER['SCRIPT_NAME'] = str_replace(basename(__FILE__), 'index.php', $_SERVER['SCRIPT_NAME']);
 $_SERVER['SCRIPT_FILENAME'] = str_replace(basename(__FILE__), 'index.php', $_SERVER['SCRIPT_FILENAME']);
 
@@ -43,9 +40,12 @@ try {
 
 umask(0);
 
-$disabledFuncs = array_map('trim', explode(',', strtolower(ini_get('disable_functions'))));
-$isShellDisabled = is_array($disabledFuncs) ? in_array('shell_exec', $disabledFuncs) : true;
-$isShellDisabled = (stripos(PHP_OS, 'win') === false) ? $isShellDisabled : true;
+$disabledFuncs = array_map('trim', preg_split("/,|\s+/", strtolower(ini_get('disable_functions'))));
+$isShellDisabled = in_array('shell_exec', $disabledFuncs)
+    || !str_contains(strtolower(PHP_OS), 'win')
+    || !shell_exec('which expr 2>/dev/null')
+    || !shell_exec('which ps 2>/dev/null')
+    || !shell_exec('which sed 2>/dev/null');
 
 try {
     if (stripos(PHP_OS, 'win') === false) {
@@ -62,8 +62,8 @@ try {
             $fileName = escapeshellarg(basename(__FILE__));
             $cronPath = escapeshellarg(__DIR__ . '/cron.sh');
 
-            shell_exec(escapeshellcmd("/bin/sh $cronPath $fileName -mdefault 1") . " > /dev/null 2>&1 &");
-            shell_exec(escapeshellcmd("/bin/sh $cronPath $fileName -malways 1") . " > /dev/null 2>&1 &");
+            shell_exec(escapeshellcmd("/bin/sh $cronPath $fileName -mdefault 1") . " &");
+            shell_exec(escapeshellcmd("/bin/sh $cronPath $fileName -malways 1") . " &");
             exit;
         }
     }
