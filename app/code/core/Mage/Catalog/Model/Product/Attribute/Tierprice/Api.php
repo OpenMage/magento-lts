e7ad47ef3543558519463c8c3baef5b7b9d6e9cb
sv3n <github-sr@hotmail.com>
2022-10-05 08:16:09 +0200

    Update app/code/core/Mage/Catalog/Model/Product/Attribute/Tierprice/Api.php
    Co-authored-by: Ng Kiat Siong <kiatsiong.ng@gmail.com>

--- app/code/core/Mage/Catalog/Model/Product/Attribute/Tierprice/Api.php
+++ app/code/core/Mage/Catalog/Model/Product/Attribute/Tierprice/Api.php
@@ -55,9 +55,9 @@ class Mage_Catalog_Model_Product_Attribute_Tierprice_Api extends Mage_Catalog_Mo
             $row = [];
             $row['customer_group_id'] = (empty($tierPrice['all_groups']) ? $tierPrice['cust_group'] : 'all');
             $row['website']           = (
-                $tierPrice['website_id'] ?
-                    Mage::app()->getWebsite($tierPrice['website_id'])->getCode() :
-                    'all'
+                $tierPrice['website_id']
+                    ? Mage::app()->getWebsite($tierPrice['website_id'])->getCode()
+                    : 'all'
             );
             $row['qty']               = $tierPrice['price_qty'];
             $row['price']             = $tierPrice['price'];
