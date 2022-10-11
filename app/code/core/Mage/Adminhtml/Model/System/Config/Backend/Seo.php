91d071674b7ffb9119398f807df141862261ce0b
MarcinNowakMacopedia <61323809+MarcinNowakMacopedia@users.noreply.github.com>
2022-10-11 14:25:29 +0200

    Update app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Seo.php
    Fix class description
    
    Co-authored-by: sv3n <github-sr@hotmail.com>

--- app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Seo.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Backend/Seo.php
@@ -18,10 +18,10 @@
  * versions in the future. If you wish to customize Magento for your
  * needs please refer to http://www.magento.com for more information.
  *
- * @category    Mage
- * @package     Mage_Adminhtml
+ * @category   Mage
+ * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 
