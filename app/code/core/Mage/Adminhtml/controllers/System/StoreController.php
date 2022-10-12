c1170fa0ccf7f00f46e096cd3531afdb72953816
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-10-12 11:17:00 +0100

    Require reindex when enabling a store view

--- app/code/core/Mage/Adminhtml/controllers/System/StoreController.php
+++ app/code/core/Mage/Adminhtml/controllers/System/StoreController.php
@@ -222,11 +222,6 @@ class Mage_Adminhtml_System_StoreController extends Mage_Adminhtml_Controller_Ac
                         if ($postData['store']['store_id']) {
                             $storeModel->load($postData['store']['store_id']);
                         }
-                        if ($postData['store']['is_active'] == 1 && $storeModel->getIsActive() == 0) {
-                            Mage::getModel('index/process')
-                                ->load('catalog_product_attribute', 'indexer_code')
-                                ->changeStatus(Mage_Index_Model_Process::STATUS_REQUIRE_REINDEX);
-                        }
                         $storeModel->setData($postData['store']);
                         if ($postData['store']['store_id'] == '') {
                             $storeModel->setId(null);
