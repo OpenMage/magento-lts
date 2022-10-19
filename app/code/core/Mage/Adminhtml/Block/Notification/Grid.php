5caab82aa38041468340f8b8a1707849af389219
sv3n <github-sr@hotmail.com>
2022-10-19 11:31:05 +0200

    Cast getLoadingTimeout() to int in template (#2661)

--- app/code/core/Mage/Adminhtml/Block/Notification/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Notification/Grid.php
@@ -110,7 +110,7 @@ class Mage_Adminhtml_Block_Notification_Grid extends Mage_Adminhtml_Block_Widget
     }
 
     /**
-     * @param Varien_Object $row
+     * @param Mage_AdminNotification_Model_Inbox $row
      * @return string
      */
     public function getRowClass(Varien_Object $row)
