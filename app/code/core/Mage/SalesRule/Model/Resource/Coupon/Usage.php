5e86f183fb9f45289cfcdc6f0866a893e830ea1c
Sven Reichel <github-sr@hotmail.com>
2022-11-08 04:45:19 +0100

    Fix: Mage_SalesRule_Model_Resource_Coupon_Usage::updateCustomerCouponTimesUsed()

--- app/code/core/Mage/SalesRule/Model/Resource/Coupon/Usage.php
+++ app/code/core/Mage/SalesRule/Model/Resource/Coupon/Usage.php
@@ -52,8 +52,8 @@ class Mage_SalesRule_Model_Resource_Coupon_Usage extends Mage_Core_Model_Resourc
         $timesUsed = $read->fetchOne($select, [':coupon_id' => $couponId, ':customer_id' => $customerId]);
 
         if ($timesUsed !== false) {
-            $timesUsed += ($decrement ? -1 : 1);
-            if($timesUsed >= 0) {
+            $timesUsed = (int)$timesUsed + ($decrement ? -1 : 1);
+            if ($timesUsed >= 0) {
                 $this->_getWriteAdapter()->update(
                     $this->getMainTable(),
                     [
