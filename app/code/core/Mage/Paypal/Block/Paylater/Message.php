27611c565453342dba28b0cc3e9af2797f7f3fd7
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-11-09 18:34:59 +0000

    Update docblock

--- app/code/core/Mage/Paypal/Block/Paylater/Message.php
+++ app/code/core/Mage/Paypal/Block/Paylater/Message.php
@@ -1,10 +1,31 @@
 <?php
+/**
+ * OpenMage
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * @category   Mage
+ * @package    Mage_Paypal
+ * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 /**
  * Paypal Pay Later
  *
  * Available to specific list of merchant countries only
  *
+ * @category   Mage
+ * @package    Mage_Paypal
+ * @author     Magento Core Team <core@magentocommerce.com>
  */
 class Mage_Paypal_Block_Paylater_Message extends Mage_Core_Block_Template
 {
