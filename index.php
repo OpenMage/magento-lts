7973e5a70c2f74c8878ea953bc97d310a3b42297
Sven Reichel <github-sr@hotmail.com>
2022-11-23 06:15:38 +0100

    Combined workflows

--- index.php
+++ index.php
@@ -55,10 +55,10 @@ require_once $mageFilename;
 umask(0);
 
 /* Store or website code */
-$mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : '';
+$mageRunCode = $_SERVER['MAGE_RUN_CODE'] ?? '';
 
 /* Run store or run website */
-$mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
+$mageRunType = $_SERVER['MAGE_RUN_TYPE'] ?? 'store';
 
 if (file_exists($maintenanceFile)) {
     $maintenanceBypass = false;
