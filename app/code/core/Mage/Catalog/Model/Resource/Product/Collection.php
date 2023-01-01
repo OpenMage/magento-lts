e647860f813cd64fe330f5684202ed651bcaeb55
Sven Reichel <github-sr@hotmail.com>
2023-01-02 00:21:27 +0100

    Replace array_keys() and in_array() to array_key_exists()
    - see https://github.com/rectorphp/rector/blob/main/docs/rector_rules_overview.md#arraykeysandinarraytoarraykeyexistsrector

--- app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
+++ app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
@@ -243,8 +243,7 @@ class Mage_Catalog_Model_Resource_Product_Collection extends Mage_Catalog_Model_
         // prepare response object for event
         $response = new Varien_Object();
         $response->setAdditionalCalculations([]);
-        $tableAliases = array_keys($select->getPart(Zend_Db_Select::FROM));
-        if (in_array(self::INDEX_TABLE_ALIAS, $tableAliases)) {
+        if (array_key_exists(self::INDEX_TABLE_ALIAS, $select->getPart(Zend_Db_Select::FROM))) {
             $table = self::INDEX_TABLE_ALIAS;
         } else {
             $table = reset($tableAliases);
