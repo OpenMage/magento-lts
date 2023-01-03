1fd35a1ee8993749f4d59fc85fa8579b96315a9b
sv3n <github-sr@hotmail.com>
2023-01-03 23:10:28 +0100

    Removed Mage_Backup (#2811)

--- app/code/core/Mage/Adminhtml/controllers/System/StoreController.php
+++ app/code/core/Mage/Adminhtml/controllers/System/StoreController.php
@@ -453,6 +453,13 @@ class Mage_Adminhtml_System_StoreController extends Mage_Adminhtml_Controller_Ac
      */
     protected function _backupDatabase($failPath, $arguments = [])
     {
+        if (!class_exists('Mage_Backup_Model_Backup')
+            || !class_exists('Mage_Backup_Model_Db')
+            || !class_exists('Mage_Backup_Helper_Data')
+        ) {
+            return $this;
+        }
+
         if (!$this->getRequest()->getParam('create_backup')) {
             return $this;
         }
