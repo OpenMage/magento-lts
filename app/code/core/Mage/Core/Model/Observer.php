56774e4421a445cd45a4be6342fb7c9864cc2bc8
Ikko Eltociear Ashimine <eltociear@gmail.com>
2023-01-05 14:22:36 +0900

    Fix typo in Observer.php
    occured -> occurred

--- app/code/core/Mage/Core/Model/Observer.php
+++ app/code/core/Mage/Core/Model/Observer.php
@@ -60,7 +60,7 @@ class Mage_Core_Model_Observer
                 $flagData = $flag->getFlagData();
                 if (isset($flagData['has_errors']) && $flagData['has_errors']) {
                     $severity       = Mage_AdminNotification_Model_Inbox::SEVERITY_MAJOR;
-                    $title          = Mage::helper('adminhtml')->__('An error has occured while syncronizing media storages.');
+                    $title          = Mage::helper('adminhtml')->__('An error has occurred while syncronizing media storages.');
                     $description    = Mage::helper('adminhtml')->__('One or more media files failed to be synchronized during the media storages syncronization process. Refer to the log file for details.');
                 } else {
                     $severity       = Mage_AdminNotification_Model_Inbox::SEVERITY_NOTICE;
