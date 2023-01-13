2d960913aee7788df2c4a4832a08245d37343707
luigifab <31816829+luigifab@users.noreply.github.com>
2023-01-13 23:51:30 +0100

    Fixed error on unlink when no such file or directory (#2675)

--- lib/Varien/Io/File.php
+++ lib/Varien/Io/File.php
@@ -399,6 +399,7 @@ class Varien_Io_File extends Varien_Io_Abstract
      */
     public static function rmdirRecursive($dir, $recursive = true)
     {
+        $result = true;
         if ($recursive) {
             if (is_dir($dir)) {
                 foreach (scandir($dir) as $item) {
@@ -408,7 +409,7 @@ class Varien_Io_File extends Varien_Io_Abstract
                     self::rmdirRecursive($dir . "/" . $item, $recursive);
                 }
                 $result = @rmdir($dir);
-            } else {
+            } elseif (file_exists($dir)) {
                 $result = @unlink($dir);
             }
         } else {
