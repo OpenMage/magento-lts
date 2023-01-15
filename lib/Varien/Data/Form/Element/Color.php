a6d9dfafd79603e8ce63fe8233ce0555edc47bfa
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-01-15 18:07:05 +0000

    Added frontend_type color

---
+++ lib/Varien/Data/Form/Element/Color.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * OpenMage
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * @category   Varien
+ * @package    Varien_Data
+ * @copyright  Copyright (c) 2023 The OpenMage Contributors (https://www.openmage.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Form text element
+ *
+ * @category   Varien
+ * @package    Varien_Data
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Varien_Data_Form_Element_Color extends Varien_Data_Form_Element_Abstract
+{
+    /**
+     * Varien_Data_Form_Element_Text constructor.
+     * @param array $attributes
+     */
+    public function __construct($attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->setType('color');
+        $this->setExtType('textfield');
+    }
+
+    /**
+     * @return string
+     */
+    public function getHtml()
+    {
+        $this->addClass('input-text');
+        return parent::getHtml();
+    }
+
+    /**
+     * @return array
+     */
+    public function getHtmlAttributes()
+    {
+        return ['type', 'title', 'class', 'style', 'onclick', 'onchange', 'onkeyup', 'disabled', 'readonly', 'maxlength', 'tabindex'];
+    }
+}
