0aae70c94c312e1201f9c532c1f93f85e89ef10c
Ng Kiat Siong <kiatsiong.ng@gmail.com>
2023-02-22 08:57:53 +0800

    Update Newpass.php
    Fixed typo, format warning.

--- app/code/core/Mage/Adminhtml/Block/Customer/Edit/Renderer/Newpass.php
+++ app/code/core/Mage/Adminhtml/Block/Customer/Edit/Renderer/Newpass.php
@@ -35,7 +35,7 @@ class Mage_Adminhtml_Block_Customer_Edit_Renderer_Newpass extends Mage_Adminhtml
         if ($element->getNote()) {
             $html .= '<p class="note"><span>' . $element->getNote() . '</span></p>';
         }
-        $html .= '<p id="email-passowrd-warning" style="display:none;" class="note"><span>' . Mage::helper('customer')->__('Warning: an email containing the plaintext password will be sent.') . '</span></p>';
+        $html .= '<p id="email-password-warning" style="display:none;" class="note"><span>' . Mage::helper('customer')->__('<strong style=""color:red"">Warning!</strong> An email containing the plaintext password will be sent.') . '</span></p>';
         $html .= '</td>';
         $html .= '</tr>' . "\n";
         $html .= '<tr>';
