4f44eb11131abf8df9ae8a46e14f458c8429a381
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-03-03 10:50:49 +0000

    just removed an empty line

--- app/code/core/Mage/Sales/Model/Quote.php
+++ app/code/core/Mage/Sales/Model/Quote.php
@@ -1111,7 +1111,6 @@ class Mage_Sales_Model_Quote extends Mage_Core_Model_Abstract
         Mage::dispatchEvent('sales_quote_product_add_after', ['items' => $items]);
 
         reset($items);
-
         return current($items);
     }
 
