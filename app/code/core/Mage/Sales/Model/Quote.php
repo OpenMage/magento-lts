19c9cebfa19670a4a18ff993ebde0651f1030a06
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-03-06 10:40:53 -0300

    [ UPDATED ] bugfix: return first cart item when adding bundle produto to cart

--- app/code/core/Mage/Sales/Model/Quote.php
+++ app/code/core/Mage/Sales/Model/Quote.php
@@ -1110,7 +1110,7 @@ class Mage_Sales_Model_Quote extends Mage_Core_Model_Abstract
 
         Mage::dispatchEvent('sales_quote_product_add_after', ['items' => $items]);
 
-        return reset($items);
+        return $parentItem;
     }
 
     /**
