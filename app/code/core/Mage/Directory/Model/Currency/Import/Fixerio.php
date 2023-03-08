b999a2c3e17e833d000f0a08252ea280d3fce115
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-03-08 13:59:25 +0000

    Changed FixerIO API

--- app/code/core/Mage/Directory/Model/Currency/Import/Fixerio.php
+++ app/code/core/Mage/Directory/Model/Currency/Import/Fixerio.php
@@ -43,7 +43,7 @@ class Mage_Directory_Model_Currency_Import_Fixerio extends Mage_Directory_Model_
      *
      * @var string
      */
-    protected $_url = 'https://api.apilayer.com/fixer/latest?apikey={{ACCESS_KEY}}&base={{CURRENCY_FROM}}&symbols={{CURRENCY_TO}}';
+    protected $_url = 'https://api.apilayer.com/currency_data/live?source={{CURRENCY_FROM}}&currencies={{CURRENCY_TO}}';
 
     /**
      * Information messages stack
@@ -106,7 +106,7 @@ class Mage_Directory_Model_Currency_Import_Fixerio extends Mage_Directory_Model_
      * @param array $currenciesTo
      * @return array
      */
-    protected function _convertBatch(array $data, $currencyFrom, array $currenciesTo)
+    protected function _convertBatch(array $data, string $currencyFrom, array $currenciesTo): array
     {
         $accessKey = Mage::getStoreConfig(self::XML_PATH_FIXERIO_API_KEY);
         if (empty($accessKey)) {
@@ -115,43 +115,37 @@ class Mage_Directory_Model_Currency_Import_Fixerio extends Mage_Directory_Model_
             $data[$currencyFrom] = $this->_makeEmptyResponse($currenciesTo);
             return $data;
         }
-
         $currenciesImploded = implode(',', $currenciesTo);
         $url = str_replace(
-            ['{{ACCESS_KEY}}', '{{CURRENCY_FROM}}', '{{CURRENCY_TO}}'],
-            [$accessKey, $currencyFrom, $currenciesImploded],
+            ['{{CURRENCY_FROM}}', '{{CURRENCY_TO}}'],
+            [$currencyFrom, $currenciesImploded],
             $this->_url
         );
-
         $timeLimitCalculated = 2 * (int) Mage::getStoreConfig(self::XML_PATH_FIXERIO_TIMEOUT)
             + (int) ini_get('max_execution_time');
-
         @set_time_limit($timeLimitCalculated);
         try {
-            $response = $this->_getServiceResponse($url);
+            $response = $this->_getServiceResponse($url, $accessKey);
         } catch (Exception $e) {
             ini_restore('max_execution_time');
         }
-
         if (!$this->_validateResponse($response, $currencyFrom)) {
             $data[$currencyFrom] = $this->_makeEmptyResponse($currenciesTo);
             return $data;
         }
-
         foreach ($currenciesTo as $currencyTo) {
             if ($currencyFrom == $currencyTo) {
                 $data[$currencyFrom][$currencyTo] = $this->_numberFormat(1);
             } else {
-                if (empty($response['rates'][$currencyTo])) {
+                if (empty($response['quotes'])) {
                     $this->_messages[] = Mage::helper('directory')
                         ->__('We can\'t retrieve a rate from %s for %s.', $url, $currencyTo);
                     $data[$currencyFrom][$currencyTo] = null;
                 } else {
-                    $data[$currencyFrom][$currencyTo] = $this->_numberFormat((float) $response['rates'][$currencyTo]);
+                    $data[$currencyFrom][$currencyTo] = $this->_numberFormat((float) $response['quotes'][$currencyFrom.$currencyTo]);
                 }
             }
         }
-
         return $data;
     }
 
@@ -162,16 +156,16 @@ class Mage_Directory_Model_Currency_Import_Fixerio extends Mage_Directory_Model_
      * @param int $retry
      * @return array
      */
-    protected function _getServiceResponse($url, $retry = 0)
+    protected function _getServiceResponse(string $url, $accessKey, int $retry = 0): array
     {
         $response = [];
         try {
             $jsonResponse = $this->_httpClient
                 ->setUri($url)
+                ->setHeaders(['Content-Type' => 'text/plain', 'apikey' => $accessKey])
                 ->setConfig(['timeout' => Mage::getStoreConfig(self::XML_PATH_FIXERIO_TIMEOUT)])
                 ->request('GET')
                 ->getBody();
-
             $response = json_decode($jsonResponse, true);
         } catch (Exception $e) {
             if ($retry === 0) {
