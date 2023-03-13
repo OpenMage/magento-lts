5fe73d9c16a7b434479d5b951b7c46c8cc68d813
Loek van Gool <loek@deheerhoreca.nl>
2023-03-13 22:46:41 +0100

    Log PHP session errors and prevent PHP errors

--- app/code/core/Mage/Core/Model/Resource/Session.php
+++ app/code/core/Mage/Core/Model/Resource/Session.php
@@ -139,14 +139,18 @@ class Mage_Core_Model_Resource_Session implements Zend_Session_SaveHandler_Inter
     public function setSaveHandler()
     {
         if ($this->hasConnection()) {
-            session_set_save_handler(
-                [$this, 'open'],
-                [$this, 'close'],
-                [$this, 'read'],
-                [$this, 'write'],
-                [$this, 'destroy'],
-                [$this, 'gc']
-            );
+            if (!headers_sent($file, $line)) {
+                session_set_save_handler(
+                    [$this, 'open'],
+                    [$this, 'close'],
+                    [$this, 'read'],
+                    [$this, 'write'],
+                    [$this, 'destroy'],
+                    [$this, 'gc']
+                );
+            } else {
+                Mage::log('Failed to set session save handler, headers already sent. Output started at '.$file.':'.$line.'. URL: '.Mage::helper('core/url')->getCurrentUrl(), null, 'exception.log', true);
+            }
         } else {
             session_save_path(Mage::getBaseDir('session'));
         }
