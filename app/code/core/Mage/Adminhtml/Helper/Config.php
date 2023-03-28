970c038748cb1cda9b1f6845a559901e3ec08b8b
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-03-28 14:23:14 +0100

    Removed "notice of license" line in docblock since it is redundant

--- app/code/core/Mage/Adminhtml/Helper/Config.php
+++ app/code/core/Mage/Adminhtml/Helper/Config.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 /**
  * OpenMage
  *
- * NOTICE OF LICENSE
- *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
