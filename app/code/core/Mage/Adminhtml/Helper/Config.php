de1e4485116ca7c43c0553c44a3fbcc0943759d4
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-04-03 21:25:49 +0100

    removed "if you did not receive" section

--- app/code/core/Mage/Adminhtml/Helper/Config.php
+++ app/code/core/Mage/Adminhtml/Helper/Config.php
@@ -9,9 +9,6 @@ declare(strict_types=1);
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
  * https://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magento.com so we can send you a copy immediately.
  *
  * @category   Mage
  * @package    Mage_Adminhtml
