8b71e0fa1e8c70926946bbd2bfe0ad5d6e637cb4
luigifab <31816829+luigifab@users.noreply.github.com>
2023-04-18 10:20:20 +0200

    Set sort order and disable click-to-top for coupons grid

--- app/code/core/Mage/Adminhtml/Block/Promo/Quote/Edit/Tab/Coupons/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Promo/Quote/Edit/Tab/Coupons/Grid.php
@@ -27,6 +27,7 @@ class Mage_Adminhtml_Block_Promo_Quote_Edit_Tab_Coupons_Grid extends Mage_Adminh
     {
         parent::__construct();
         $this->setId('couponCodesGrid');
+        $this->setDefaultSort('created_at');
         $this->setUseAjax(true);
     }
 
@@ -127,5 +128,14 @@ class Mage_Adminhtml_Block_Promo_Quote_Edit_Tab_Coupons_Grid extends Mage_Adminh
     public function getGridUrl()
     {
         return $this->getUrl('*/*/couponsGrid', ['_current' => true]);
+    }
+
+    /**
+     * @param Varien_Object $row
+     * @return string
+     */
+    public function getRowUrl($row)
+    {
+        return '';
     }
 }
