d1d7050c829a3683cb56bbc4a6072c9dfd1fc79a
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-04-24 18:42:25 +0100

    Rewrote the patch based on @colinmollenhour suggestions

--- app/Mage.php
+++ app/Mage.php
@@ -43,9 +43,7 @@ include_once "Varien/Autoload.php";
 Varien_Autoload::register();
 
 /** AUTOLOADER PATCH **/
-if (file_exists($autoloaderPath = BP . DS . 'vendor/autoload.php') ||
-    file_exists($autoloaderPath = BP . DS . '../vendor/autoload.php')
-) {
+if (file_exists($autoloaderPath = (getenv('COMPOSER_VENDOR_PATH') ?: BP . DS .  'vendor') . DS . 'autoload.php')) {
     require $autoloaderPath;
 }
 /** AUTOLOADER PATCH **/
