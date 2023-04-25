c8fe0b5968fbc2861814ce9096c86968188fffd2
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-04-25 17:58:26 +0100

    First test converting the plugins

--- js/mage/adminhtml/wysiwyg/tiny_mce/plugins/magentovariable/editor_plugin.js
+++ js/mage/adminhtml/wysiwyg/tiny_mce/plugins/magentovariable/editor_plugin.js
@@ -12,45 +12,22 @@
  * @license     https://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
-tinyMCE.addI18n({en:{
-    magentovariable:
-    {
-        insert_variable : "Insert Variable"
-    }
-}});
-
-(function() {
-    tinymce.create('tinymce.plugins.MagentovariablePlugin', {
-        /**
-         * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
-         * @param {string} url Absolute URL to where the plugin is located.
-         */
-        init : function(ed, url) {
-            ed.addCommand('mceMagentovariable', function() {
-                var pluginSettings = ed.settings.magentoPluginsOptions.get('magentovariable');
-                MagentovariablePlugin.setEditor(ed);
-                MagentovariablePlugin.loadChooser(pluginSettings.url, null);
-            });
-
-            // Register Widget plugin button
-            ed.addButton('magentovariable', {
-                title : 'magentovariable.insert_variable',
-                cmd : 'mceMagentovariable',
-                image : url + '/img/icon.gif'
-            });
-        },
-
-        getInfo : function() {
-            return {
-                longname : 'Magento Variable Manager Plugin for TinyMCE 3.x',
-                author : 'Magento Core Team',
-                authorurl : 'http://magentocommerce.com',
-                infourl : 'https://www.openmage.org',
-                version : "1.0"
-            };
-        }
+tinymce.PluginManager.add('OpenMageVariablePlugin', (ed, url) => {
+    ed.addCommand('mceMagentovariable', function() {
+        var pluginSettings = ed.settings.magentoPluginsOptions.get('magentovariable');
+        MagentovariablePlugin.setEditor(ed);
+        MagentovariablePlugin.loadChooser(pluginSettings.url, null);
     });
 
-    // Register plugin
-    tinymce.PluginManager.add('magentovariable', tinymce.plugins.MagentovariablePlugin);
-})();
+    // Register Widget plugin button
+    ed.addButton('magentovariable', {
+        title : 'magentovariable.insert_variable',
+        cmd : 'mceMagentovariable',
+        image : url + '/img/icon.gif'
+    });
+
+    return {
+        name: 'OpenMage Variable Manager Plugin for TinyMCE',
+        url: 'https://www.openmage.org'
+    };
+});
