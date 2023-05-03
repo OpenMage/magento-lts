da9ba28646adb913f59871e3684455c3646830c1
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-05-03 11:46:35 +0200

    Converted msrp_rwd.js to vanilla javascript (part 2) (#3230)
    Co-authored-by: Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>

--- skin/frontend/rwd/default/js/msrp_rwd.js
+++ skin/frontend/rwd/default/js/msrp_rwd.js
@@ -14,19 +14,15 @@
 Catalog.Map.showHelp = Catalog.Map.showHelp.wrap(function (parent, event) {
     var helpBox = document.getElementById('map-popup');
     var bodyNode = document.getElementsByTagName('body')[0];
-
-    // Resolve calculation bug in parent so we can actually use these classes...
+    parent(event);
+    
     if (helpBox && this != Catalog.Map && Catalog.Map.active != this.link) {
-        parent(event);
-
-        helpBox.removeClassName('map-popup-right');
-        helpBox.removeClassName('map-popup-left');
+        helpBox.classList.remove('map-popup-right');
+        helpBox.classList.remove('map-popup-left');
         if (Element.getWidth(bodyNode) < event.pageX + (Element.getWidth(helpBox) / 2)) {
-            helpBox.addClassName('map-popup-left');
+            helpBox.classList.add('map-popup-left');
         } else if (event.pageX - (Element.getWidth(helpBox) / 2) < 0) {
-            helpBox.addClassName('map-popup-right');
+            helpBox.classList.add('map-popup-right');
         }
-    } else {
-        parent(event);
     }
 });
