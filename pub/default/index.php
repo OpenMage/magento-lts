2d953babc18b0208d4dc84dbfe03346d2185f4d2
Colin Mollenhour <colin@mollenhour.com>
2023-05-23 22:57:34 -0400

    Use symlink for .htaccess. Use require for index.php. Update dev/openmage environment to use pub/default directory.

--- pub/default/index.php
+++ pub/default/index.php
@@ -1,66 +1,20 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category    Mage
- * @package     Mage
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
+//
+// This file is the entrypoint for the default OpenMage store.
+// Create a separate subdirectory of pub/ to add more virtual hosts for different stores
+// with different root directories and assign the run code and run type variables below as appropriate.
+//
 
-if (version_compare(phpversion(), '5.3.0', '<')===true) {
-    echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;">
-<div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
-<h3 style="margin:0; font-size:1.7em; font-weight:normal; text-transform:none; text-align:left; color:#2f2f2f;">
-Whoops, it looks like you have an invalid PHP version.</h3></div><p>Magento supports PHP 5.3.0 or newer.
-<a href="http://www.magentocommerce.com/install" target="">Find out</a> how to install</a>
- Magento using PHP-CGI as a work-around.</p></div>';
-    exit;
-}
-
-/**
- * Compilation includes configuration file
- */
-define('MAGENTO_ROOT', getcwd());
-
-$mageFilename = MAGENTO_ROOT . '../../app/Mage.php';
-$maintenanceFile = 'maintenance.flag';
-
-if (!file_exists($mageFilename)) {
-    if (is_dir('downloader')) {
-        header("Location: downloader");
-    } else {
-        echo $mageFilename." was not found";
-    }
-    exit;
-}
-
-if (file_exists($maintenanceFile)) {
-    include_once dirname(__FILE__) . '/errors/503.php';
-    exit;
-}
-
-require MAGENTO_ROOT . '/app/bootstrap.php';
-require_once $mageFilename;
-
-#Varien_Profiler::enable();
-
-if (isset($_SERVER['MAGE_IS_DEVELOPER_MODE'])) {
-    Mage::setIsDeveloperMode(true);
-}
-
-#ini_set('display_errors', 1);
-
-umask(0);
+chdir(dirname(dirname(__DIR__)));
 
 /* Store or website code */
-$mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : '';
+if (empty($_SERVER['MAGE_RUN_CODE'])) {
+    $_SERVER['MAGE_RUN_CODE'] = '';
+}
 
 /* Run store or run website */
-$mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
+if (empty($_SERVER['MAGE_RUN_TYPE'])) {
+    $_SERVER['MAGE_RUN_TYPE'] = 'store';
+}
 
-Mage::run($mageRunCode, $mageRunType);
+require 'index.php';
