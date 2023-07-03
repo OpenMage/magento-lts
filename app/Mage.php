2764d0c4611bd6328bf70e9a217eea5084d95f91
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-07-03 20:09:28 +0100

    Version bump

--- app/Mage.php
+++ app/Mage.php
@@ -217,7 +217,7 @@ final class Mage
                 'major'     => '20',
                 'minor'     => '1',
                 'patch'     => '0',
-                'stability' => 'rc6', // beta,alpha,rc
+                'stability' => 'rc7', // beta,alpha,rc
                 'number'    => '', // 1,2,3,0.3.7,x.7.z.92 @see https://semver.org/#spec-item-9
             ];
         }
@@ -226,7 +226,7 @@ final class Mage
             'major'     => '19',
             'minor'     => '5',
             'patch'     => '0',
-            'stability' => 'rc6', // beta,alpha,rc
+            'stability' => 'rc7', // beta,alpha,rc
             'number'    => '', // 1,2,3,0.3.7,x.7.z.92 @see https://semver.org/#spec-item-9
         ];
     }
