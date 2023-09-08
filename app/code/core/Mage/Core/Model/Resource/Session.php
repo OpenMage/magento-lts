27099f373870dfd11e128cff22e58f6f520af534
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-09-08 10:34:51 +0100

    estetic

--- app/code/core/Mage/Core/Model/Resource/Session.php
+++ app/code/core/Mage/Core/Model/Resource/Session.php
@@ -71,10 +71,6 @@ class Mage_Core_Model_Resource_Session implements SessionHandlerInterface
         $this->_write        = $resource->getConnection('core_write');
     }
 
-    /**
-     * Destrucor
-     *
-     */
     public function __destruct()
     {
         session_write_close();
@@ -255,9 +251,7 @@ class Mage_Core_Model_Resource_Session implements SessionHandlerInterface
     public function gc($sessMaxLifeTime): int|false
     {
         if ($this->_automaticCleaningFactor > 0) {
-            if ($this->_automaticCleaningFactor == 1 ||
-                rand(1, $this->_automaticCleaningFactor) == 1
-            ) {
+            if ($this->_automaticCleaningFactor == 1 || rand(1, $this->_automaticCleaningFactor) == 1) {
                 $where = ['session_expires < ?' => Varien_Date::toTimestamp(true)];
                 return $this->_write->delete($this->_sessionTable, $where);
             }
