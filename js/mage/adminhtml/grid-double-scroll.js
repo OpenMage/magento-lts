7f2904eb28ce7692d9cde3cb56b1082ffb5703ab
Tony <tonyweboss@gmail.com>
2023-09-17 10:38:20 +0200

    refactor better double-scroll

--- js/mage/adminhtml/grid-double-scroll.js
+++ js/mage/adminhtml/grid-double-scroll.js
@@ -1,53 +1,66 @@
-window.addEventListener('DOMContentLoaded', function() {
-    function DoubleScroll(element) {
-        if(!element.getAttribute('data-doublescroll')){
-            var scrollbar= document.createElement('div');
-            scrollbar.appendChild(document.createElement('div'));
-            scrollbar.style.overflow= 'auto';
-            scrollbar.style.overflowY= 'hidden';
-            scrollbar.firstChild.style.width= element.scrollWidth+'px';
-            scrollbar.firstChild.style.height= '0';
-            scrollbar.firstChild.style.paddingTop= '1px';
-            scrollbar.firstChild.appendChild(document.createTextNode('\xA0'));
-            var running = false;
-            scrollbar.onscroll= function() {
-                if(running) {
-                    running = false;
-                    return;
-                }
-                running = true;
-                element.scrollLeft= scrollbar.scrollLeft;
-            };
-            element.onscroll= function() {
-                if(running) {
-                    running = false;
-                    return;
-                }
-                running = true;
-                scrollbar.scrollLeft= element.scrollLeft;
-            };
-            element.parentNode.insertBefore(scrollbar, element);
-            element.setAttribute('data-doublescroll', 1);
-        }
+class gridDoubleScroll {
+
+    _scrolling = false;
+
+    constructor(wrapperScrollBar) {
+        this.init(wrapperScrollBar);   
     }
-    var horscrolls = document.querySelectorAll('.hor-scroll');
-    for (horscroll of horscrolls) {    
-        DoubleScroll(horscroll);
-    }
-    const config = { childList: true };
-    const callback = function(mutationsList, observer) {
-        var horscrolls = document.querySelectorAll('.hor-scroll');
-        for (horscroll of horscrolls) {      
-            DoubleScroll(horscroll);
+
+    init(wrapperScrollBar) {
+        this.wrapperScrollBar = wrapperScrollBar;
+        let scrollbarTop = this.wrapperScrollBar.parentNode.querySelector('.hor-scroll-top');
+        if (!scrollbarTop){
+            this.createDoubleScroll(this.wrapperScrollBar);
+            this.scrollbarTop.addEventListener('scroll', this.syncWrapperScrollBar.bind(this), false);
+            this.wrapperScrollBar.addEventListener('scroll', this.syncScrollBarTop.bind(this), false);
+            const observer = new MutationObserver( this.updateDoubleScrollWidth.bind(this) );
+            observer.observe(this.wrapperScrollBar, { childList: true, subtree: true });
         }
+        this.updateDoubleScrollWidth();
+    }
+
+    createDoubleScroll() {
+        let scrollbarTop = document.createElement('div');
+        scrollbarTop.classList.add('hor-scroll-top');
+        scrollbarTop.appendChild(document.createElement('div'));
+        scrollbarTop.style.overflow = 'auto';
+        scrollbarTop.style.overflowY = 'hidden';
+        //scrollbarTop.firstChild.style.height = '0';
+        scrollbarTop.firstChild.style.paddingTop = '1px';
+        scrollbarTop.firstChild.appendChild(document.createTextNode('\xA0'));
+        this.wrapperScrollBar.parentNode.insertBefore(scrollbarTop, this.wrapperScrollBar);
+
+        this.scrollbarTop = scrollbarTop;
+    }
+
+    syncWrapperScrollBar() {
+        if(this._scrolling) {
+            this._scrolling = false;
+            return;
+        }
+        this._scrolling = true;
+        this.wrapperScrollBar.scrollLeft = this.scrollbarTop.scrollLeft;
+    }
+
+    syncScrollBarTop() {
+        if(this._scrolling) {
+            this._scrolling = false;
+            return;
+        }
+        this._scrolling = true;
+        this.scrollbarTop.scrollLeft = this.wrapperScrollBar.scrollLeft;
     };
-    const observer = new MutationObserver(callback);
-    const productGridNode = document.getElementById('productGrid');
-    if(productGridNode){
-        observer.observe(productGridNode, config);
+
+    updateDoubleScrollWidth() {
+        if (this.scrollbarTop.firstChild.style.width != this.wrapperScrollBar.scrollWidth) {
+            this.scrollbarTop.firstChild.style.width = this.wrapperScrollBar.scrollWidth + 'px';
+        }
     }
-    const salesOrderGridNode = document.getElementById('sales_order_grid');
-    if(salesOrderGridNode){
-        observer.observe(salesOrderGridNode, config);
-    }
+};
+
+window.addEventListener('DOMContentLoaded', function() {
+    let horScrolls = document.querySelectorAll('.hor-scroll');
+    horScrolls.forEach( (horScroll) => {
+        new gridDoubleScroll(horScroll);
+    });
 });
