eaecab540c1fefa4b844f213f0853ad2630d1b1a
Michael Davidsen <m-overlund@users.noreply.github.com>
2023-09-29 10:42:03 +0200

    Update assign_order2customer.php
    camelCase variables

--- shell/actions/assign_order2customer.php
+++ shell/actions/assign_order2customer.php
@@ -46,35 +46,35 @@ echo "Please input Website ID: ";
 $websiteID = getInput();
 
 echo "Please input customer email: ";
-$customeremailid = getInput('5');
+$customerEmailID = getInput('5');
 
 
 /****************************************/
 
-if(isset($customeremailid) && isset($websiteID))
+if(isset($customerEmailID) && isset($websiteID))
 {
 	$toCustomer = Mage::getModel('customer/customer')
 		->setWebsiteId($websiteID)
-		->loadByEmail($customeremailid);
+		->loadByEmail($customerEmailID);
 
 
 	$orders = Mage::getModel('sales/order')->getCollection()
 		->addAttributeToFilter('store_id',$websiteID)
 		->addAttributeToFilter('customer_is_guest', array('nin' => array(0)))
-		->addAttributeToFilter('customer_email', $customeremailid);
+		->addAttributeToFilter('customer_email', $customerEmailID);
 
 		
 	foreach($orders as $order)
 	{
 		try {
-			$orderbyid = Mage::getModel('sales/order')->loadByIncrementId($order['increment_id']);
+			$orderByID = Mage::getModel('sales/order')->loadByIncrementId($order['increment_id']);
 
-			$orderbyid->setCustomerId($toCustomer->getId());
-			$orderbyid->setCustomerFirstname($toCustomer->getFirstname());
-			$orderbyid->setCustomerLastname($toCustomer->getLastname());
-			$orderbyid->setCustomerEmail($toCustomer->getEmail());
-			$orderbyid->setCustomerIsGuest(0);
-			$orderbyid->save();
+			$orderByID->setCustomerId($toCustomer->getId());
+			$orderByID->setCustomerFirstname($toCustomer->getFirstname());
+			$orderByID->setCustomerLastname($toCustomer->getLastname());
+			$orderByID->setCustomerEmail($toCustomer->getEmail());
+			$orderByID->setCustomerIsGuest(0);
+			$orderByID->save();
 			
 			echo $order['increment_id'] . " saved!\n";
 			
