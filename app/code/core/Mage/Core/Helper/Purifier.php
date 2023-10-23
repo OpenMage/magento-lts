af55463fd06df3959ff32d16396e14cd2adcde9a
Judx <juho.holsa@gmail.com>
2023-10-23 11:30:11 +0300

    Add HtmlPurifier for improved MaliciousCode filtering

---
+++ app/code/core/Mage/Core/Helper/Purifier.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * OpenMage
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available at https://opensource.org/license/osl-3-0-php
+ *
+ * @category   Mage
+ * @package    Mage_Core
+ * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
+ * @copyright  Copyright (c) 2019-2023 The OpenMage Contributors (https://www.openmage.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * HTMLPurifier helper
+ *
+ * @category   Mage
+ * @package    Mage_Core
+ */
+class Mage_Core_Helper_Purifier extends Mage_Core_Helper_Abstract
+{
+    public const CACHE_DEFINITION = 'Cache.DefinitionImpl';
+
+    /**
+     * @var HTMLPurifier|null
+     */
+    protected ?HTMLPurifier $purifier;
+
+    /**
+     * Purifier Constructor Call
+     * @param HTMLPurifier|null $purifier
+     */
+    public function __construct(
+        ?HTMLPurifier $purifier = null
+    ) {
+        $config = HTMLPurifier_Config::createDefault();
+        $config->set(self::CACHE_DEFINITION, null);
+        $this->purifier = $purifier ?? new HTMLPurifier($config);
+    }
+
+    /**
+     * Purify Html Content
+     *
+     * @param array|string $content
+     * @return mixed
+     */
+    public function purify($content): mixed
+    {
+        return is_array($content) ? $this->purifier->purifyArray($content) : $this->purifier->purify($content);
+    }
+
+}
