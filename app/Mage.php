c7d6d554841a6fad29e99b25f2989b2704cb457c
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-10-27 11:18:52 +0100

    Version bump in preparation for release

--- app/Mage.php
+++ app/Mage.php
@@ -215,8 +215,8 @@ final class Mage
         if (self::getOpenMageMajorVersion() === 20) {
             return [
                 'major'     => '20',
-                'minor'     => '1',
-                'patch'     => '1',
+                'minor'     => '2',
+                'patch'     => '0',
                 'stability' => '', // beta,alpha,rc
                 'number'    => '', // 1,2,3,0.3.7,x.7.z.92 @see https://semver.org/#spec-item-9
             ];
@@ -225,7 +225,7 @@ final class Mage
         return [
             'major'     => '19',
             'minor'     => '5',
-            'patch'     => '1',
+            'patch'     => '2',
             'stability' => '', // beta,alpha,rc
             'number'    => '', // 1,2,3,0.3.7,x.7.z.92 @see https://semver.org/#spec-item-9
         ];
