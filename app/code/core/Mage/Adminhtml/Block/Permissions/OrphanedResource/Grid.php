bf96a9f8c1045b7f7d3d3c3789d8822c02a3042e
Ng Kiat Siong <kiatsiong.ng@gmail.com>
2023-11-12 14:19:09 +0800

    Update app/code/core/Mage/Adminhtml/Block/Permissions/OrphanedResource/Grid.php
    Co-authored-by: Sven Reichel <github-sr@hotmail.com>

--- app/code/core/Mage/Adminhtml/Block/Permissions/OrphanedResource/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Permissions/OrphanedResource/Grid.php
@@ -82,7 +82,7 @@ class Mage_Adminhtml_Block_Permissions_OrphanedResource_Grid extends Mage_Adminh
         return $this;
     }
 
-    public function getRowUrl($row)
+    public function getRowUrl($row): string
     {
         return '';
     }
