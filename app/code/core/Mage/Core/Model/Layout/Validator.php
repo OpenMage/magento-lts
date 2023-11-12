c504166780ec8bf857ccc6403da84cf75e7558cb
Fabrice Creuzot <code@luigifab.fr>
2023-11-12 18:19:53 +0100

    Allow to save product

--- app/code/core/Mage/Core/Model/Layout/Validator.php
+++ app/code/core/Mage/Core/Model/Layout/Validator.php
@@ -143,6 +143,9 @@ class Mage_Core_Model_Layout_Validator extends Zend_Validate_Abstract
             try {
                 $value = new Varien_Simplexml_Element('<config>' . $value . '</config>');
             } catch (Exception $e) {
+                if (Mage::getIsDeveloperMode()) {
+                    throw new Exception($e->getMessage() . '  in ' . $e->getFile() . ' on line ' . $e->getLine());
+                }
                 $this->_error(self::XML_INVALID);
                 return false;
             }
@@ -158,6 +161,9 @@ class Mage_Core_Model_Layout_Validator extends Zend_Validate_Abstract
             try {
                 $this->validateTemplatePath($templatePaths);
             } catch (Exception $e) {
+                if (Mage::getIsDeveloperMode()) {
+                    throw new Exception($e->getMessage() . '  in ' . $e->getFile() . ' on line ' . $e->getLine());
+                }
                 $this->_error(self::INVALID_TEMPLATE_PATH);
                 return false;
             }
@@ -233,9 +239,9 @@ class Mage_Core_Model_Layout_Validator extends Zend_Validate_Abstract
         /** @var Varien_Simplexml_Element $path */
         foreach ($templatePaths as $path) {
             if ($path->hasChildren()) {
-                $path = stripcslashes(trim((string) $path->children(), '"'));
+                $path = stripcslashes(trim((string)$path->children(), '"'));
             }
-            if (strpos($path, '..' . DS) !== false) {
+            if (strpos((string)$path, '..' . DS) !== false) {
                 throw new Exception();
             }
         }
