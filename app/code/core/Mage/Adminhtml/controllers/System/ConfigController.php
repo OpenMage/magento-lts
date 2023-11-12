067a2ee4364aaf79d8cac822c01975198550ec3a
Fabrice Creuzot <code@luigifab.fr>
2023-11-12 18:34:44 +0100

    Show file on error

--- app/code/core/Mage/Adminhtml/controllers/System/ConfigController.php
+++ app/code/core/Mage/Adminhtml/controllers/System/ConfigController.php
@@ -191,8 +191,7 @@ class Mage_Adminhtml_System_ConfigController extends Mage_Adminhtml_Controller_A
         } catch (Exception $e) {
             $session->addException(
                 $e,
-                Mage::helper('adminhtml')->__('An error occurred while saving this configuration:') . ' '
-                . $e->getMessage()
+                Mage::helper('adminhtml')->__('An error occurred while saving this configuration:') . ' ' . (Mage::getIsDeveloperMode() ? $e->getMessage() . '  in ' . $e->getFile() . ' on line ' . $e->getLine() : $e->getMessage())
             );
         }
 
