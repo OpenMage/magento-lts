8f4b79861cc7d8f53c14deaffa5743df096d8650
Sven Reichel <github-sr@hotmail.com>
2024-02-03 02:27:05 +0100

    Mage_Adminhtml_Block_Report_Grid_Abstract

--- app/code/core/Mage/Adminhtml/Block/Report/Grid/Abstract.php
+++ app/code/core/Mage/Adminhtml/Block/Report/Grid/Abstract.php
@@ -27,7 +27,7 @@ class Mage_Adminhtml_Block_Report_Grid_Abstract extends Mage_Adminhtml_Block_Wid
     /**
      * Column for grid to be grouped by
      *
-     * @var string
+     * @var string|null
      */
     protected $_columnGroupBy;
 
@@ -85,7 +85,7 @@ class Mage_Adminhtml_Block_Report_Grid_Abstract extends Mage_Adminhtml_Block_Wid
 
     /**
      * Add column to grid
-     * Overriden to add support for visibility_filter column option
+     * Override to add support for visibility_filter column option
      * It stands for conditional visibility of the column depending on filter field values
      * Value of visibility_filter supports (filter_field_name => filter_field_value) pairs
      *
@@ -122,7 +122,7 @@ class Mage_Adminhtml_Block_Report_Grid_Abstract extends Mage_Adminhtml_Block_Wid
     /**
      * Get allowed store ids array intersected with selected scope in store switcher
      *
-     * @return  array
+     * @return array
      */
     protected function _getStoreIds()
     {
@@ -165,7 +165,9 @@ class Mage_Adminhtml_Block_Report_Grid_Abstract extends Mage_Adminhtml_Block_Wid
             }
         }
 
-        $resourceCollection = Mage::getResourceModel($this->getResourceCollectionName())
+        /** @var Mage_Sales_Model_Resource_Report_Collection_Abstract $resourceCollection */
+        $resourceCollection = Mage::getResourceModel($this->getResourceCollectionName());
+        $resourceCollection
             ->setPeriod($filterData->getData('period_type'))
             ->setDateRange($filterData->getData('from', null), $filterData->getData('to', null))
             ->addStoreFilter($storeIds)
@@ -193,7 +195,9 @@ class Mage_Adminhtml_Block_Report_Grid_Abstract extends Mage_Adminhtml_Block_Wid
         }
 
         if ($this->getCountTotals()) {
-            $totalsCollection = Mage::getResourceModel($this->getResourceCollectionName())
+            /** @var Mage_Sales_Model_Resource_Report_Collection_Abstract $totalsCollection */
+            $totalsCollection = Mage::getResourceModel($this->getResourceCollectionName());
+            $totalsCollection
                 ->setPeriod($filterData->getData('period_type'))
                 ->setDateRange($filterData->getData('from', null), $filterData->getData('to', null))
                 ->addStoreFilter($storeIds)
@@ -219,7 +223,9 @@ class Mage_Adminhtml_Block_Report_Grid_Abstract extends Mage_Adminhtml_Block_Wid
     {
         if (!$this->getTotals()) {
             $filterData = $this->getFilterData();
-            $totalsCollection = Mage::getResourceModel($this->getResourceCollectionName())
+            /** @var Mage_Sales_Model_Resource_Report_Collection_Abstract $totalsCollection */
+            $totalsCollection = Mage::getResourceModel($this->getResourceCollectionName());
+            $totalsCollection
                 ->setPeriod($filterData->getData('period_type'))
                 ->setDateRange($filterData->getData('from', null), $filterData->getData('to', null))
                 ->addStoreFilter($this->_getStoreIds())
@@ -244,7 +250,9 @@ class Mage_Adminhtml_Block_Report_Grid_Abstract extends Mage_Adminhtml_Block_Wid
     public function getSubTotals()
     {
         $filterData = $this->getFilterData();
-        $subTotalsCollection = Mage::getResourceModel($this->getResourceCollectionName())
+        /** @var Mage_Sales_Model_Resource_Report_Collection_Abstract $subTotalsCollection */
+        $subTotalsCollection = Mage::getResourceModel($this->getResourceCollectionName());
+        $subTotalsCollection
             ->setPeriod($filterData->getData('period_type'))
             ->setDateRange($filterData->getData('from', null), $filterData->getData('to', null))
             ->addStoreFilter($this->_getStoreIds())
@@ -288,7 +296,7 @@ class Mage_Adminhtml_Block_Report_Grid_Abstract extends Mage_Adminhtml_Block_Wid
     /**
      * Add order status filter
      *
-     * @param Mage_Reports_Model_Resource_Report_Collection_Abstract $collection
+     * @param Mage_Sales_Model_Resource_Report_Collection_Abstract $collection
      * @param Varien_Object $filterData
      * @return $this
      */
@@ -302,7 +310,7 @@ class Mage_Adminhtml_Block_Report_Grid_Abstract extends Mage_Adminhtml_Block_Wid
      * Adds custom filter to resource collection
      * Can be overridden in child classes if custom filter needed
      *
-     * @param Mage_Reports_Model_Resource_Report_Collection_Abstract $collection
+     * @param Mage_Sales_Model_Resource_Report_Collection_Abstract $collection
      * @param Varien_Object $filterData
      * @return $this
      */
