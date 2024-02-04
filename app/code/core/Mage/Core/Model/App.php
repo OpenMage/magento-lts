50bdaf8221b6749ae4433361c3de8ff269bec968
Pascal Querner <p.querner@ottomatik.de>
2024-02-04 13:43:40 +0100

    wip: override config from env vars

--- app/code/core/Mage/Core/Model/App.php
+++ app/code/core/Mage/Core/Model/App.php
@@ -442,6 +442,7 @@ class Mage_Core_Model_App
                         Varien_Profiler::stop('mage::app::init::apply_db_schema_updates');
                     }
                     $this->_config->loadDb();
+                    $this->_config->loadEnv();
                     $this->_config->saveCache();
                 }
             } finally {
