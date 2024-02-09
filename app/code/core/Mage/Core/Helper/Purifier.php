74bfb326c7e01fa6682addba4f7fc32a824e14b7
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-02-09 00:39:58 +0000

    copyright update

--- app/code/core/Mage/Core/Helper/Purifier.php
+++ app/code/core/Mage/Core/Helper/Purifier.php
@@ -8,13 +8,11 @@
  *
  * @category   Mage
  * @package    Mage_Core
- * @copyright  Copyright (c) 2023 The OpenMage Contributors (https://www.openmage.org)
+ * @copyright  Copyright (c) 2024 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
- * HTMLPurifier helper
- *
  * @category   Mage
  * @package    Mage_Core
  */
