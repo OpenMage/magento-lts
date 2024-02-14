16167e3e2a0d3f686fe70664a35e45df2797fee2
Sven Reichel <github-sr@hotmail.com>
2024-02-14 14:31:52 +0100

    Updated PhpUnit test (#10)
    * Test: removed Testfield b/c it does not work locally
    
    * Test: updated workflow
    
    * Test: updated workflow ... removed composer validation (for now)
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Test: updated workflow ... will fail
    
    * Test: updated workflow ... added DB/install
    
    * Test: updated workflow ...
    
    * Test: updated workflow ...
    
    * Remove gitpod running php73
    
    * Remove docker install running php82
    
    * Test: moved tests to root (as common practice)
    
    * Test: updated workflow ... bootstrap.php
    
    * Test: updated workflow ... test
    
    * Test: updated workflow ... test
    
    * Test: updated workflow ... test
    
    * Test: updated workflow ... test
    
    * Test: updated workflow ... test
    
    * Test: updated workflow ... sonar config
    
    * Test: updated workflow ... phpunit.xml.dist
    
    * Test: updated workflow ... sonar workflow
    
    * Test: updated workflow ... phpunit.yml
    
    * Test: updated workflow ... gitignore [skip ci]
    
    * Test: ddev shortcut [skip ci]
    
    * Test: updated workflow ... gitignore [skip ci]
    
    * Test: updated workflow ... gitignore [skip ci]

---
+++ tests/unit/Base/ClassLoadingTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace OpenMage\Tests\Unit\Base;
+
+use PHPUnit\Framework\TestCase;
+
+class ClassLoadingTest extends TestCase
+{
+    /**
+     * @dataProvider provideClassExistsData
+     * @param bool $expectedResult
+     * @param string $class
+     * @return void
+     */
+    public function testClassExists(bool $expectedResult, string $class): void
+    {
+        self::assertSame($expectedResult, class_exists($class));
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function provideClassExistsData(): array
+    {
+        return [
+            'class exists #1' => [
+                true,
+                'Mage'
+            ],
+            'class exists #2' => [
+                true,
+                'Mage_Eav_Model_Entity_Increment_Numeric'
+            ],
+            'class not exists' => [
+                false,
+                'Mage_Non_Existent'
+            ],
+        ];
+    }
+}
