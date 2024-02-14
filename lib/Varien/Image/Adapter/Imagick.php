966932ad4d855b559fdceff5c80c98afa25a6b16
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-02-14 14:36:51 +0000

    copyright and phpcs

--- lib/Varien/Image/Adapter/Imagick.php
+++ lib/Varien/Image/Adapter/Imagick.php
@@ -6,21 +6,16 @@
  *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magento.com so we can send you a copy immediately.
+ * It is also available at https://opensource.org/license/osl-3-0-php
  *
  * @category   Varien
  * @package    Varien_Image
- * @copyright  Copyright (c) 2023 The OpenMage Contributors (https://www.openmage.org)
+ * @copyright  Copyright (c) 2024 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 class Varien_Image_Adapter_Imagick extends Varien_Image_Adapter_Abstract
 {
-
     /**
      * @var Imagick
      */
