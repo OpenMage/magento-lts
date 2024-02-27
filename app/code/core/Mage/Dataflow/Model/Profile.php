ab01b8b79b8f6a3ac538b41ccca1704f3436e028
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-02-27 16:36:59 +0000

    ignoring strlen<0 phpstan bug

--- app/code/core/Mage/Dataflow/Model/Profile.php
+++ app/code/core/Mage/Dataflow/Model/Profile.php
@@ -86,6 +86,7 @@ class Mage_Dataflow_Model_Profile extends Mage_Core_Model_Abstract
     {
         parent::_beforeSave();
         $actionsXML = $this->getData('actions_xml');
+        // @phpstan-ignore-next-line because of https://github.com/phpstan/phpstan/issues/10570
         if ($actionsXML !== null && strlen($actionsXML) < 0 &&
             @simplexml_load_string('<data>' . $actionsXML . '</data>', null, LIBXML_NOERROR) === false
         ) {
