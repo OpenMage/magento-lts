0d6bbd658a9107142b5c6c2568c99b328030eaeb
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-04-19 10:39:57 +0100

    typo

--- app/code/core/Mage/Cms/sql/cms_setup/mysql4-upgrade-1.6.0.0-1.6.0.3.php
+++ app/code/core/Mage/Cms/sql/cms_setup/mysql4-upgrade-1.6.0.0-1.6.0.3.php
@@ -7,7 +7,7 @@
  * It is also available at https://opensource.org/license/osl-3-0-php
  *
  * @category   Mage
- * @package    Mage_Catalog
+ * @package    Mage_Cms
  * @copyright  Copyright (c) 2023 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
