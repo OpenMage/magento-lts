935671591f871c02fc41ebc38717c37df4465f96
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-04-23 13:31:58 +0100

    Added support for Laravel Ignition

--- app/code/core/Mage/Catalog/controllers/CategoryController.php
+++ app/code/core/Mage/Catalog/controllers/CategoryController.php
@@ -29,6 +29,7 @@ class Mage_Catalog_CategoryController extends Mage_Core_Controller_Front_Action
      */
     protected function _initCategory()
     {
+        Mage::throwException("pipo");
         Mage::dispatchEvent('catalog_controller_category_init_before', ['controller_action' => $this]);
         $categoryId = (int) $this->getRequest()->getParam('id', false);
         if (!$categoryId) {
