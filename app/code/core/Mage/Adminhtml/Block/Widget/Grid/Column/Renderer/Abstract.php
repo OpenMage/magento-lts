b98ed7ab182f76facb99f4895f0a988a9f8adc7a
luigifab <31816829+luigifab@users.noreply.github.com>
2024-05-06 10:42:23 +0200

    Type casting and minor fixes in preparation for `declare strict_types=1` (#3648)
    Co-authored-by: Fabrizio Balliano <fabrizio.balliano@gmail.com>
    Co-authored-by: Ng Kiat Siong <kiatsiong.ng@gmail.com>

--- app/code/core/Mage/Adminhtml/Block/Widget/Grid/Column/Renderer/Abstract.php
+++ app/code/core/Mage/Adminhtml/Block/Widget/Grid/Column/Renderer/Abstract.php
@@ -142,7 +142,7 @@ abstract class Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract extends
 
         if ($this->getColumn()->hasData('width')) {
             $customWidth = $this->getColumn()->getData('width');
-            if (($customWidth === null) || (preg_match('/^[0-9]+%?$/', $customWidth))) {
+            if (($customWidth === null) || (preg_match('/^[0-9]+%?$/', (string)$customWidth))) {
                 $width = $customWidth;
             } elseif (preg_match('/^([0-9]+)px$/', $customWidth, $matches)) {
                 $width = (int)$matches[1];
--- app/code/core/Mage/Core/Model/Resource/Helper/Mysql4.php
+++ app/code/core/Mage/Core/Model/Resource/Helper/Mysql4.php
@@ -260,7 +260,7 @@ class Mage_Core_Model_Resource_Helper_Mysql4 extends Mage_Core_Model_Resource_He
             list($correlationName, $column, $alias) = $columnEntry;
             if ($column instanceof Zend_Db_Expr) {
                 if ($alias !== null) {
-                    if (preg_match('/(^|[^a-zA-Z_])^(SELECT)?(SUM|MIN|MAX|AVG|COUNT)\s*\(/i', $column, $matches)) {
+                    if (preg_match('/(^|[^a-zA-Z_])^(SELECT)?(SUM|MIN|MAX|AVG|COUNT)\s*\(/i', (string)$column, $matches)) {
                         $column = $this->prepareColumn($column, $groupByCondition);
                     }
                     $preparedColumns[strtoupper($alias)] = [null, $column, $alias];
