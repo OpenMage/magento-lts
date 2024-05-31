cdf436e19ea1ee102d214d419299a68ad17bca39
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-05-31 15:38:59 +0100

    Fixed undefined array index label_use_default in Mage_Catalog_Model_Product_Attribute_Backend_Media

--- app/code/core/Mage/Catalog/Model/Product/Attribute/Backend/Media.php
+++ app/code/core/Mage/Catalog/Model/Product/Attribute/Backend/Media.php
@@ -242,6 +242,13 @@ class Mage_Catalog_Model_Product_Attribute_Backend_Media extends Mage_Eav_Model_
                 $image['value_id']      = $this->_getResource()->insertGallery($data);
             }
 
+            if (!isset($image["label_use_default"])) {
+                $image["label_use_default"] = null;
+            }
+            if (!isset($image["position_use_default"])) {
+                $image["position_use_default"] = null;
+            }
+
             $this->_getResource()->deleteGalleryValueInStore($image['value_id'], $object->getStoreId());
 
             // Add per store labels, position, disabled
