OpenMage/magento-lts

commit fc97382ffcf9bb865346de078aafb7996564c7d1

Added EAV Config Cache (#2993)

app/code/core/Mage/Catalog/Model/Resource/Product/Compare/Item/Collection.php
@@ -45,7 +45,7 @@ class Mage_Catalog_Model_Resource_Product_Compare_Item_Collection extends Mage_C
     /**
      * Comparable attributes cache
      *
-     * @var array|null
+     * @var Mage_Eav_Model_Entity_Attribute_Abstract[]|null
      */
     protected $_comparableAttributes;
 
@@ -211,7 +211,7 @@ class Mage_Catalog_Model_Resource_Product_Compare_Item_Collection extends Mage_C
     /**
      * Retrieve Merged comparable attributes for compared product items
      *
-     * @return array
+     * @return Mage_Eav_Model_Entity_Attribute_Abstract[]
      */
     public function getComparableAttributes()
     {
@@ -219,36 +219,19 @@ class Mage_Catalog_Model_Resource_Product_Compare_Item_Collection extends Mage_C
             $this->_comparableAttributes = [];
             $setIds = $this->_getAttributeSetIds();
             if ($setIds) {
-                $select = $this->getConnection()->select()
-                    ->from(['main_table' => $this->getTable('eav/attribute')])
-                    ->join(
-                        ['additional_table' => $this->getTable('catalog/eav_attribute')],
-                        'additional_table.attribute_id=main_table.attribute_id'
-                    )
-                    ->joinLeft(
-                        ['al' => $this->getTable('eav/attribute_label')],
-                        'al.attribute_id = main_table.attribute_id AND al.store_id = ' . (int) $this->getStoreId(),
-                        ['store_label' => new Zend_Db_Expr('IFNULL(al.value, main_table.frontend_label)')]
-                    )
-                    ->joinLeft(
-                        ['ai' => $this->getTable('eav/entity_attribute')],
-                        'ai.attribute_id = main_table.attribute_id'
-                    )
-                    ->where('additional_table.is_comparable=?', 1)
-                    ->where('ai.attribute_set_id IN(?)', $setIds)
-                    ->order(['ai.attribute_group_id ASC', 'ai.sort_order ASC']);
-                $attributesData = $this->getConnection()->fetchAll($select);
-                if ($attributesData) {
-                    $entityType = Mage_Catalog_Model_Product::ENTITY;
-                    Mage::getSingleton('eav/config')
-                        ->importAttributesData($entityType, $attributesData);
-                    foreach ($attributesData as $data) {
-                        $attribute = Mage::getSingleton('eav/config')
-                            ->getAttribute($entityType, $data['attribute_code']);
+                $eavConfig = Mage::getSingleton('eav/config');
+                $attributeIds = $eavConfig->getAttributeSetAttributeIds($setIds);
+                $this->_comparableAttributes = [];
+                foreach ($attributeIds as $attributeId) {
+                    $attribute = $eavConfig->getAttribute(Mage_Catalog_Model_Product::ENTITY, $attributeId);
+                    if ($attribute->getData('is_comparable')) {
                         $this->_comparableAttributes[$attribute->getAttributeCode()] = $attribute;
                     }
-                    unset($attributesData);
                 }
+
+                usort($this->_comparableAttributes, function ($a, $b) {
+                    return $a->getPosition() - $b->getPosition();
+                });
             }
         }
         return $this->_comparableAttributes;

app/code/core/Mage/Catalog/Model/Config.php
@@ -330,17 +330,13 @@ class Mage_Catalog_Model_Config extends Mage_Eav_Model_Config
     public function getAttributesUsedInProductListing()
     {
         if (is_null($this->_usedInProductListing)) {
+            $allAttributes = Mage::getSingleton('eav/config')
+                ->getAttributes(Mage_Catalog_Model_Product::ENTITY);
             $this->_usedInProductListing = [];
-            $entityType = Mage_Catalog_Model_Product::ENTITY;
-            $attributesData = $this->_getResource()
-                ->setStoreId($this->getStoreId())
-                ->getAttributesUsedInListing();
-            Mage::getSingleton('eav/config')
-                ->importAttributesData($entityType, $attributesData);
-            foreach ($attributesData as $attributeData) {
-                $attributeCode = $attributeData['attribute_code'];
-                $this->_usedInProductListing[$attributeCode] = Mage::getSingleton('eav/config')
-                    ->getAttribute($entityType, $attributeCode);
+            foreach ($allAttributes as $attribute) {
+                if ($attribute->getData('used_in_product_listing')) {
+                    $this->_usedInProductListing[$attribute->getAttributeCode()] = $attribute;
+                }
             }
         }
         return $this->_usedInProductListing;
@@ -354,16 +350,13 @@ class Mage_Catalog_Model_Config extends Mage_Eav_Model_Config
     public function getAttributesUsedForSortBy()
     {
         if (is_null($this->_usedForSortBy)) {
+            $allAttributes = Mage::getSingleton('eav/config')
+                ->getAttributes(Mage_Catalog_Model_Product::ENTITY);
             $this->_usedForSortBy = [];
-            $entityType     = Mage_Catalog_Model_Product::ENTITY;
-            $attributesData = $this->_getResource()
-                ->getAttributesUsedForSortBy();
-            Mage::getSingleton('eav/config')
-                ->importAttributesData($entityType, $attributesData);
-            foreach ($attributesData as $attributeData) {
-                $attributeCode = $attributeData['attribute_code'];
-                $this->_usedForSortBy[$attributeCode] = Mage::getSingleton('eav/config')
-                    ->getAttribute($entityType, $attributeCode);
+            foreach ($allAttributes as $attribute) {
+                if ($attribute->getData('used_for_sort_by')) {
+                    $this->_usedForSortBy[$attribute->getAttributeCode()] = $attribute;
+                }
             }
         }
         return $this->_usedForSortBy;

app/code/core/Mage/Eav/Model/Resource/Entity/Attribute/Collection.php
@@ -42,6 +42,13 @@ class Mage_Eav_Model_Resource_Entity_Attribute_Collection extends Mage_Core_Mode
     protected $_addSetInfoFlag   = false;
 
     /**
+     * Tracks if addStoreLabel has been called to avoid conflicts on duplicate calls
+     *
+     * @var bool|int
+     */
+    protected $_addedStoreLabelsFlag = false;
+
+    /**
      * Resource model initialization
      *
      */
@@ -418,14 +425,21 @@ class Mage_Eav_Model_Resource_Entity_Attribute_Collection extends Mage_Core_Mode
      */
     public function addStoreLabel($storeId)
     {
-        $adapter        = $this->getConnection();
-        $joinExpression = $adapter
-            ->quoteInto('al.attribute_id = main_table.attribute_id AND al.store_id = ?', (int) $storeId);
-        $this->getSelect()->joinLeft(
-            ['al' => $this->getTable('eav/attribute_label')],
-            $joinExpression,
-            ['store_label' => $adapter->getIfNullSql('al.value', 'main_table.frontend_label')]
-        );
+        // if not called previously
+        if ($this->_addedStoreLabelsFlag === false) {
+            $adapter = $this->getConnection();
+            $joinExpression = $adapter
+                ->quoteInto('al.attribute_id = main_table.attribute_id AND al.store_id = ?', (int)$storeId);
+            $this->getSelect()->joinLeft(
+                ['al' => $this->getTable('eav/attribute_label')],
+                $joinExpression,
+                ['store_label' => $adapter->getIfNullSql('al.value', 'main_table.frontend_label')]
+            );
+            $this->_addedStoreLabelsFlag = $storeId;
+        } // check that previous call $storeId matches current call
+        elseif ($this->_addedStoreLabelsFlag !== $storeId) {
+            throw new Exception('Cannot call addStoreLabel for different store views on the same collection');
+        }
 
         return $this;
     }

.all-contributorsrc
@@ -1373,6 +1373,15 @@
       "contributions": [
         "code"
       ]
+    },
+    {
+      "login": "davidhiendl",
+      "name": "David Hiendl",
+      "avatar_url": "https://avatars.githubusercontent.com/u/11006964?v=4",
+      "profile": "https://github.com/davidhiendl",
+      "contributions": [
+        "code"
+      ]
     }
   ],
   "contributorsPerLine": 7

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Attribute/Set/Main.php
@@ -390,8 +390,8 @@ class Mage_Adminhtml_Block_Catalog_Product_Attribute_Set_Main extends Mage_Admin
     {
         $isDefault = $this->getData('is_current_set_default');
         if (is_null($isDefault)) {
-            $defaultSetId = Mage::getModel('eav/entity_type')
-                ->load(Mage::registry('entityType'))
+            $defaultSetId = Mage::getSingleton('eav/config')
+                ->getEntityType(Mage::registry('entityType'))
                 ->getDefaultAttributeSetId();
             $isDefault = $this->_getSetId() == $defaultSetId;
             $this->setData('is_current_set_default', $isDefault);

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Attribute/Set/Main/Formgroup.php
@@ -76,8 +76,7 @@ class Mage_Adminhtml_Block_Catalog_Product_Attribute_Set_Main_Formgroup extends 
     {
         return ((int) $this->getRequest()->getParam('id') > 0)
                     ? (int) $this->getRequest()->getParam('id')
-                    : Mage::getModel('eav/entity_type')
-                        ->load(Mage::registry('entityType'))
+                    : Mage::getSingleton('eav/config')->getEntityType(Mage::registry('entityType'))
                         ->getDefaultAttributeSetId();
     }
 }

app/code/core/Mage/Api2/Model/Resource.php
@@ -1030,7 +1030,7 @@ abstract class Mage_Api2_Model_Resource
         $model = $this->getConfig()->getResourceWorkingModel($this->getResourceType());
 
         /** @var Mage_Eav_Model_Entity_Type $entityType */
-        $entityType = Mage::getModel('eav/entity_type')->load($model, 'entity_model');
+        $entityType = Mage::getSingleton('eav/config')->getEntityType($model, 'entity_model');
 
         /** @var Mage_Eav_Model_Entity_Attribute $attribute */
         foreach ($entityType->getAttributeCollection() as $attribute) {

commit f0e8993e06158bf632faf4b5f90ebf660ecd4f95

fix regression introduced in #2993 where attributes are no longer sorted correctly by attribute group order in attribute comparison

app/code/core/Mage/Catalog/Model/Resource/Product/Compare/Item/Collection.php
@@ -215,15 +215,23 @@ class Mage_Catalog_Model_Resource_Product_Compare_Item_Collection extends Mage_C
                 $eavConfig = Mage::getSingleton('eav/config');
                 $attributeIds = $eavConfig->getAttributeSetAttributeIds($setIds);
                 $this->_comparableAttributes = [];
+                $attributeSortInfo = [];
                 foreach ($attributeIds as $attributeId) {
                     $attribute = $eavConfig->getAttribute(Mage_Catalog_Model_Product::ENTITY, $attributeId);
                     if ($attribute->getData('is_comparable')) {
                         $this->_comparableAttributes[$attribute->getAttributeCode()] = $attribute;
+                        $attributeSortInfo[$attribute->getAttributeCode()] = $eavConfig->getAttributeSetGroupInfo($attributeId, $setIds);
                     }
                 }
 
-                usort($this->_comparableAttributes, function ($a, $b) {
-                    return $a->getPosition() - $b->getPosition();
+                uasort($this->_comparableAttributes, function ($a, $b) use ($attributeSortInfo) {
+                    /** @var Mage_Eav_Model_Entity_Attribute_Abstract $a */
+                    /** @var Mage_Eav_Model_Entity_Attribute_Abstract $b */
+
+                    $aSort = $attributeSortInfo[$a->getAttributeCode()]; // contains group_id, group_sort, sort
+                    $bSort = $attributeSortInfo[$b->getAttributeCode()]; // contains group_id, group_sort, sort
+
+                    return $aSort['group_sort'] <=> $bSort['group_sort'] ?: $aSort['sort'] <=> $bSort['sort'];
                 });
             }
         }

app/code/core/Mage/Eav/Model/Config.php
@@ -599,6 +599,27 @@ class Mage_Eav_Model_Config
     }
 
     /**
+     * Return first attribute sorting information found for a given list of attribute sets
+     * @param int $attributeId
+     * @param int|int[] $attributeSetIds
+     * @return false|array
+     */
+    public function getAttributeSetGroupInfo($attributeId, $attributeSetIds)
+    {
+        if (!is_array($attributeSetIds)) {
+            $attributeSetIds = [$attributeSetIds];
+        }
+
+        foreach ($attributeSetIds as $attributeSetId) {
+            if (isset($this->_attributeSetInfo[$attributeId][$attributeSetId])) {
+                return $this->_attributeSetInfo[$attributeId][$attributeSetId];
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * @param mixed $entityType
      * @param string $attribute
      * @return  Mage_Eav_Model_Entity_Attribute_Abstract|null