OpenMage/magento-lts

commit fc97382ffcf9bb865346de078aafb7996564c7d1

Added EAV Config Cache (#2993)

app/code/core/Mage/Catalog/Model/Layer.php
@@ -202,7 +202,7 @@ class Mage_Catalog_Model_Layer extends Varien_Object
     /**
      * Get collection of all filterable attributes for layer products set
      *
-     * @return Mage_Catalog_Model_Resource_Product_Attribute_Collection|array
+     * @return Mage_Catalog_Model_Resource_Eav_Attribute[]
      */
     public function getFilterableAttributes()
     {
@@ -210,17 +210,26 @@ class Mage_Catalog_Model_Layer extends Varien_Object
         if (!$setIds) {
             return [];
         }
-        /** @var Mage_Catalog_Model_Resource_Product_Attribute_Collection $collection */
-        $collection = Mage::getResourceModel('catalog/product_attribute_collection');
-        $collection
-            ->setItemObjectClass('catalog/resource_eav_attribute')
-            ->setAttributeSetFilter($setIds)
-            ->addStoreLabel(Mage::app()->getStore()->getId())
-            ->setOrder('position', 'ASC');
-        $collection = $this->_prepareAttributeCollection($collection);
-        $collection->load();
 
-        return $collection;
+        $eavConfig = Mage::getSingleton('eav/config');
+        /** @var Mage_Catalog_Model_Resource_Eav_Attribute[] $attributes */
+        $attributes = [];
+        foreach ($setIds as $setId) {
+            $setAttributeIds = $eavConfig->getAttributeSetAttributeIds($setId);
+            foreach ($setAttributeIds as $attributeId) {
+                if (!isset($attributes[$attributeId])) {
+                    $attribute = $eavConfig->getAttribute(Mage_Catalog_Model_Product::ENTITY, $attributeId);
+                    if ($attribute instanceof Mage_Catalog_Model_Resource_Eav_Attribute && $attribute->getIsFilterable()) {
+                        $attributes[$attributeId] = $attribute;
+                    }
+                }
+            }
+        }
+        usort($attributes, function ($a, $b) {
+            return $a->getPosition() - $b->getPosition();
+        });
+
+        return $attributes;
     }
 
     /**

.all-contributorsrc
@@ -1373,6 +1373,15 @@
       "contributions": [
         "code"
       ]
+    },
+    {
+      "login": "davidhiendl",
+      "name": "David Hiendl",
+      "avatar_url": "https://avatars.githubusercontent.com/u/11006964?v=4",
+      "profile": "https://github.com/davidhiendl",
+      "contributions": [
+        "code"
+      ]
     }
   ],
   "contributorsPerLine": 7

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Attribute/Set/Main.php
@@ -390,8 +390,8 @@ class Mage_Adminhtml_Block_Catalog_Product_Attribute_Set_Main extends Mage_Admin
     {
         $isDefault = $this->getData('is_current_set_default');
         if (is_null($isDefault)) {
-            $defaultSetId = Mage::getModel('eav/entity_type')
-                ->load(Mage::registry('entityType'))
+            $defaultSetId = Mage::getSingleton('eav/config')
+                ->getEntityType(Mage::registry('entityType'))
                 ->getDefaultAttributeSetId();
             $isDefault = $this->_getSetId() == $defaultSetId;
             $this->setData('is_current_set_default', $isDefault);

app/code/core/Mage/Adminhtml/Block/Catalog/Product/Attribute/Set/Main/Formgroup.php
@@ -76,8 +76,7 @@ class Mage_Adminhtml_Block_Catalog_Product_Attribute_Set_Main_Formgroup extends 
     {
         return ((int) $this->getRequest()->getParam('id') > 0)
                     ? (int) $this->getRequest()->getParam('id')
-                    : Mage::getModel('eav/entity_type')
-                        ->load(Mage::registry('entityType'))
+                    : Mage::getSingleton('eav/config')->getEntityType(Mage::registry('entityType'))
                         ->getDefaultAttributeSetId();
     }
 }

app/code/core/Mage/Api2/Model/Resource.php
@@ -1030,7 +1030,7 @@ abstract class Mage_Api2_Model_Resource
         $model = $this->getConfig()->getResourceWorkingModel($this->getResourceType());
 
         /** @var Mage_Eav_Model_Entity_Type $entityType */
-        $entityType = Mage::getModel('eav/entity_type')->load($model, 'entity_model');
+        $entityType = Mage::getSingleton('eav/config')->getEntityType($model, 'entity_model');
 
         /** @var Mage_Eav_Model_Entity_Attribute $attribute */
         foreach ($entityType->getAttributeCollection() as $attribute) {

app/code/core/Mage/Catalog/Block/Product/Compare/List.php
@@ -126,7 +126,7 @@ class Mage_Catalog_Block_Product_Compare_List extends Mage_Catalog_Block_Product
     /**
      * Retrieve Product Compare Attributes
      *
-     * @return array
+     * @return Mage_Eav_Model_Entity_Attribute_Abstract[]
      */
     public function getAttributes()
     {

app/code/core/Mage/Catalog/Helper/Data.php
@@ -416,8 +416,8 @@ class Mage_Catalog_Helper_Data extends Mage_Core_Helper_Abstract
     {
         if ($this->_mapApplyToProductType === null) {
             /** @var Mage_Catalog_Model_Resource_Eav_Attribute $attribute */
-            $attribute = Mage::getModel('catalog/resource_eav_attribute')
-                ->loadByCode(Mage_Catalog_Model_Product::ENTITY, 'msrp_enabled');
+            $attribute = Mage::getSingleton('eav/config')
+                ->getAttribute(Mage_Catalog_Model_Product::ENTITY, 'msrp_enabled');
             $this->_mapApplyToProductType = $attribute->getApplyTo();
         }
         return empty($this->_mapApplyToProductType) || in_array($product->getTypeId(), $this->_mapApplyToProductType);

app/code/core/Mage/Catalog/Model/Api2/Product.php
@@ -39,7 +39,7 @@ class Mage_Catalog_Model_Api2_Product extends Mage_Api2_Model_Resource
     {
         $attributes = $this->getAvailableAttributesFromConfig();
         /** @var Mage_Eav_Model_Entity_Type $entityType */
-        $entityType = Mage::getModel('eav/entity_type')->loadByCode('catalog_product');
+        $entityType = Mage::getSingleton('eav/config')->getEntityType(Mage_Catalog_Model_Product::ENTITY);
         $entityOnlyAttrs = $this->getEntityOnlyAttributes($userType, $operation);
         /** @var Mage_Catalog_Model_Resource_Eav_Attribute $attribute */
         foreach ($entityType->getAttributeCollection() as $attribute) {

commit 3da8c6e906f23dc6b017d191ca5b7b5294577fc9

Fixed regression introduced in #2993 where attributes are not correctly pre-filtered for the layered navigation (#4063)

app/code/core/Mage/Catalog/Model/Layer.php
@@ -212,6 +212,9 @@ class Mage_Catalog_Model_Layer extends Varien_Object
             foreach ($setAttributeIds as $attributeId) {
                 if (!isset($attributes[$attributeId])) {
                     $attribute = $eavConfig->getAttribute(Mage_Catalog_Model_Product::ENTITY, $attributeId);
+                    if (!$this->_filterFilterableAttributes($attribute)) {
+                        continue;
+                    }
                     if ($attribute instanceof Mage_Catalog_Model_Resource_Eav_Attribute && $attribute->getIsFilterable()) {
                         $attributes[$attributeId] = $attribute;
                     }
@@ -247,6 +250,15 @@ class Mage_Catalog_Model_Layer extends Varien_Object
     {
         $collection->addIsFilterableFilter();
         return $collection;
+    }
+
+    /**
+     * Filter which attributes are included in getFilterableAttributes
+     *
+     */
+    protected function _filterFilterableAttributes(Mage_Catalog_Model_Resource_Eav_Attribute $attribute): bool
+    {
+        return $attribute->getIsFilterable() > 0;
     }
 
     /**

app/code/core/Mage/CatalogSearch/Model/Layer.php
@@ -100,6 +100,15 @@ class Mage_CatalogSearch_Model_Layer extends Mage_Catalog_Model_Layer
     }
 
     /**
+     * Filter which attributes are included in getFilterableAttributes
+     *
+     */
+    protected function _filterFilterableAttributes(Mage_Catalog_Model_Resource_Eav_Attribute  $attribute): bool
+    {
+        return $attribute->getIsVisible() && $attribute->getIsFilterableInSearch() > 0;
+    }
+
+    /**
      * Prepare attribute for use in layered navigation
      *
      * @param   Mage_Eav_Model_Entity_Attribute $attribute