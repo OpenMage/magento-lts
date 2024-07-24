aae923229112b80e7cf1e0b1d713e858efcd97f1
Daniel Fahlke <flyingmana@googlemail.com>
2024-07-24 15:48:33 +0200

    fix codestyle - empty line (#4112)

--- app/code/core/Mage/Core/Model/Security/HtmlEscapedString.php
+++ app/code/core/Mage/Core/Model/Security/HtmlEscapedString.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 /**
@@ -6,7 +7,6 @@ declare(strict_types=1);
  */
 class Mage_Core_Model_Security_HtmlEscapedString implements Stringable
 {
-
     protected $originalValue;
     protected $allowedTags;
 
