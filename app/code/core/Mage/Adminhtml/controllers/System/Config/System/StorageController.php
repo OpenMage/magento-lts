a93a26e2f78f5766b869abf96b5fab160e2d7ba8
Jozef Kotuľák <kotulak.jozef@gmail.com>
2024-08-01 23:01:04 +0200

    Revert "Added check for isModuleOutputEnabled('Mage_AdminNotification')"
    This reverts commit 8685a124b4d4d314919f38d4de72716b19e079d4.

--- app/code/core/Mage/Adminhtml/controllers/System/Config/System/StorageController.php
+++ app/code/core/Mage/Adminhtml/controllers/System/Config/System/StorageController.php
@@ -147,13 +147,10 @@ class Mage_Adminhtml_System_Config_System_StorageController extends Mage_Adminht
                     $state = Mage_Core_Model_File_Storage_Flag::STATE_NOTIFIED;
                     // no break
                 case Mage_Core_Model_File_Storage_Flag::STATE_NOTIFIED:
-                    $result['html'] = '';
-                    if (Mage::helper('core')->isModuleOutputEnabled('Mage_AdminNotification')) {
-                        $block = Mage::getSingleton('core/layout')
-                            ->createBlock('adminhtml/notification_toolbar')
-                            ->setTemplate('notification/toolbar.phtml');
-                        $result['html'] = $block->toHtml();
-                    }
+                    $block = Mage::getSingleton('core/layout')
+                        ->createBlock('adminhtml/notification_toolbar')
+                        ->setTemplate('notification/toolbar.phtml');
+                    $result['html'] = $block->toHtml();
 
                     $flagData = $flag->getFlagData();
                     if (is_array($flagData)) {
