75e548a54c92aca854af0d593fc8124ce2148c87
Sven Reichel <github-sr@hotmail.com>
2024-09-04 21:40:15 +0200

    Added tests

---
+++ tests/unit/Mage/Adminhtml/Block/Customer/Edit/Tab/NewsletterTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * OpenMage
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available at https://opensource.org/license/osl-3-0-php
+ *
+ * @category   OpenMage
+ * @package    OpenMage_Tests
+ * @copyright  Copyright (c) 2024 The OpenMage Contributors (https://www.openmage.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace OpenMage\Tests\Unit\Mage\Adminhtml\Block\Customer\Edit\Tab;
+
+use Mage;
+use Mage_Adminhtml_Block_Customer_Edit_Tab_Newsletter;
+use Mage_Customer_Model_Customer;
+use PHPUnit\Framework\TestCase;
+
+class NewsletterTest extends TestCase
+{
+    /**
+     * @var Mage_Adminhtml_Block_Customer_Edit_Tab_Newsletter
+     */
+    public Mage_Adminhtml_Block_Customer_Edit_Tab_Newsletter $subject;
+
+    public function setUp(): void
+    {
+        Mage::app();
+        // phpcs:ignore Ecg.Classes.ObjectInstantiation.DirectInstantiation
+        $this->subject = new Mage_Adminhtml_Block_Customer_Edit_Tab_Newsletter();
+    }
+
+    /**
+     * @return void
+     *
+     * @group Mage_Adminhtml
+     */
+    public function testInitForm(): void
+    {
+        $mock = $this->getMockBuilder(Mage_Adminhtml_Block_Customer_Edit_Tab_Newsletter::class)
+            ->setMethods(['getRegistryCurrentCustomer'])
+            ->getMock();
+
+        $mock->expects($this->any())
+            ->method('getRegistryCurrentCustomer')
+            // phpcs:ignore Ecg.Classes.ObjectInstantiation.DirectInstantiation
+            ->willReturn(new Mage_Customer_Model_Customer);
+
+        $this->assertInstanceOf(Mage_Adminhtml_Block_Customer_Edit_Tab_Newsletter::class, $mock->initForm());
+    }
+}
