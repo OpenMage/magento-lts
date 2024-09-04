e0e3cbaa481bc69b476fbccbeefd7d9cf481afea
Sven Reichel <github-sr@hotmail.com>
2024-09-04 20:26:48 +0200

    Added tests

---
+++ tests/unit/Mage/Log/Model/LogTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * OpenMage
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available at https://opensource.org/license/osl-3-0-php
+ *
+ * @category   OpenMage
+ * @package    OpenMage_Tests
+ * @copyright  Copyright (c) 2024 The OpenMage Contributors (https://www.openmage.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace OpenMage\Tests\Unit\Mage\Log\Model;
+
+use Mage;
+use Mage_Log_Model_Log;
+use PHPUnit\Framework\TestCase;
+
+class LogTest extends TestCase
+{
+    /**
+     * @var Mage_Log_Model_Log
+     */
+    public Mage_Log_Model_Log $subject;
+
+    public function setUp(): void
+    {
+        Mage::app();
+        $this->subject = Mage::getModel('log/log');
+    }
+
+    /**
+     * @return void
+     *
+     * @group Mage_Log
+     * @group Mage_Log_Model
+     */
+    public function testClean(): void
+    {
+        $this->assertInstanceOf(Mage_Log_Model_Log::class, $this->subject->clean());
+    }
+}
