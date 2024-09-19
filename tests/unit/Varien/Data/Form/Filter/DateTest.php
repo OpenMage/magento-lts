41afedae7eff707a22fb2722867591715da60ae2
Sven Reichel <github-sr@hotmail.com>
2024-09-19 03:43:00 +0200

    Backport PR 3833

---
+++ tests/unit/Varien/Data/Form/Filter/DateTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * OpenMage
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available at https://opensource.org/license/osl-3-0-php
+ *
+ * @category   OpenMage
+ * @package    OpenMage_Tests
+ * @copyright  Copyright (c) 2024 The OpenMage Contributors (https://www.openmage.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace OpenMage\Tests\Unit\Varien\Data\Form\Filter;
+
+use PHPUnit\Framework\TestCase;
+use ValueError;
+use Varien_Data_Form_Filter_Date;
+
+class DateTest extends TestCase
+{
+    public Varien_Data_Form_Filter_Date $subject;
+
+    public function setUp(): void
+    {
+        $this->subject = new Varien_Data_Form_Filter_Date(null, 'en_US');
+    }
+
+    /**
+     * @group Varien_Data
+     */
+    public function testInputFilter(): void
+    {
+        $this->assertEquals('', $this->subject->inputFilter(''));
+        $this->assertEquals(null, $this->subject->inputFilter(null));
+        $this->assertEquals('1990-05-18', $this->subject->inputFilter('1990-05-18'));
+        $this->assertEquals('0090-05-18', $this->subject->inputFilter('90-05-18'));
+        $this->assertEquals('1990-05-08', $this->subject->inputFilter('1990-5-8'));
+        $this->assertEquals('1970-01-01', $this->subject->inputFilter('1970-01-01'));
+
+        try {
+            $this->subject->inputFilter('1990-18-18');
+            $this->fail('expected a ValueError'); // ValueError: bcsub(): Argument #1 ($num1) is not well-formed
+        } catch (ValueError $e) {
+            $this->assertStringContainsString('bcsub', $e->getMessage());
+        }
+    }
+}
