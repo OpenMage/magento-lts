702c0f6b69e8ae2e9bef12a945c2ddf34c243518
Sven Reichel <github-sr@hotmail.com>
2024-10-06 02:58:22 +0200

    updated copyright dates

--- app/code/core/Mage/Adminhtml/Block/Customer/Edit/Tab/View/Sales.php
+++ app/code/core/Mage/Adminhtml/Block/Customer/Edit/Tab/View/Sales.php
@@ -9,7 +9,7 @@
  * @category   Mage
  * @package    Mage_Adminhtml
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2022-2023 The OpenMage Contributors (https://www.openmage.org)
+ * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -47,14 +47,12 @@ class Mage_Adminhtml_Block_Customer_Edit_Tab_View_Sales extends Mage_Adminhtml_B
     public function _beforeToHtml()
     {
         $this->_currency = Mage::getModel('directory/currency')
-            ->load(Mage_Directory_Helper_Data::getConfigCurrencyBase(),)
-        ;
+            ->load(Mage_Directory_Helper_Data::getConfigCurrencyBase());
 
         $this->_collection = Mage::getResourceModel('sales/sale_collection')
             ->setCustomerFilter(Mage::registry('current_customer'))
             ->setOrderStateFilter(Mage_Sales_Model_Order::STATE_CANCELED, true)
-            ->load()
-        ;
+            ->load();
 
         $this->_groupedCollection = [];
 
