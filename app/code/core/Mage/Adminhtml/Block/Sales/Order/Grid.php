d3584bcc5ae98b993644c6fcb20933887eb315fa
Sven Reichel <github-sr@hotmail.com>
2024-10-17 00:17:10 +0200

    refactor

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Grid.php
@@ -142,9 +142,13 @@ class Mage_Adminhtml_Block_Sales_Order_Grid extends Mage_Adminhtml_Block_Widget_
         }
 
         if (Mage::helper('sales')->isModuleOutputEnabled('Mage_Rss')) {
+            $storeId = null;
+
             $filterString = $this->getParam($this->getVarNameFilter(), '');
-            $filter = Mage::helper('adminhtml')->prepareFilterString($filterString);
-            $storeId = array_key_exists('store_id', $filter) ? $filter['store_id'] : null;
+            if ($filterString) {
+                $filter = Mage::helper('adminhtml')->prepareFilterString($filterString);
+                $storeId = array_key_exists('store_id', $filter) ? $filter['store_id'] : null;
+            }
 
             if (Mage::helper('rss')->isRssAdminOrderNewEnabled($storeId)) {
                 $slug = $storeId ? '/store/' . $storeId : '';
