4e2127ac8e50c36b07fa9950930d8ffc0aced397
Hans Mackowiak <h.mackowiak@b3-it.de>
2024-10-18 14:45:49 +0200

    Update Mage_SalesRule_Model_Quote_Discount
    * DiscountDescription should only be string or null (not array)
    * strlen needs a type check for string before
    * sales_quote_address_discount_item needs to be done after calculator->process

--- app/code/core/Mage/SalesRule/Model/Quote/Discount.php
+++ app/code/core/Mage/SalesRule/Model/Quote/Discount.php
@@ -63,7 +63,7 @@ class Mage_SalesRule_Model_Quote_Discount extends Mage_Sales_Model_Quote_Address
         $this->_calculator->init($store->getWebsiteId(), $quote->getCustomerGroupId(), $quote->getCouponCode());
         $this->_calculator->initTotals($items, $address);
 
-        $address->setDiscountDescription([]);
+        $address->setDiscountDescription('');
         /** @var Mage_Sales_Model_Quote_Item[] $items */
         $items = $this->_calculator->sortItemsByPriority($items);
         foreach ($items as $item) {
@@ -78,9 +78,6 @@ class Mage_SalesRule_Model_Quote_Discount extends Mage_Sales_Model_Quote_Address
                     continue;
                 }
 
-                $eventArgs['item'] = $item;
-                Mage::dispatchEvent('sales_quote_address_discount_item', $eventArgs);
-
                 if ($item->getHasChildren() && $item->isChildrenCalculated()) {
                     foreach ($item->getChildren() as $child) {
                         $this->_calculator->process($child);
@@ -91,6 +88,8 @@ class Mage_SalesRule_Model_Quote_Discount extends Mage_Sales_Model_Quote_Address
                     }
                 } else {
                     $this->_calculator->process($item);
+                    $eventArgs['item'] = $item;
+                    Mage::dispatchEvent('sales_quote_address_discount_item', $eventArgs);
                     $this->_aggregateItemDiscount($item);
                 }
             }
@@ -142,7 +141,7 @@ class Mage_SalesRule_Model_Quote_Discount extends Mage_Sales_Model_Quote_Address
 
         if ($amount != 0) {
             $description = $address->getDiscountDescription();
-            if (strlen($description)) {
+            if (is_string($description) && strlen($description)) {
                 $title = Mage::helper('sales')->__('Discount (%s)', $description);
             } else {
                 $title = Mage::helper('sales')->__('Discount');
