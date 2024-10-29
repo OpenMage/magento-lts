48187b69f9ed14426ffe2716e4ec6c6037ceda09
Sven Reichel <github-sr@hotmail.com>
2024-10-29 18:00:50 +0100

    fixes

--- tests/unit/Mage/Adminhtml/Block/CacheTest.php
+++ tests/unit/Mage/Adminhtml/Block/CacheTest.php
@@ -40,7 +40,7 @@ class CacheTest extends TestCase
      */
     public function testGetFlushStorageUrl(): void
     {
-        $this->assertStringStartsWith('https', $this->subject->getFlushStorageUrl());
+        $this->assertStringStartsWith('http', $this->subject->getFlushStorageUrl());
     }
 
     /**
@@ -51,6 +51,6 @@ class CacheTest extends TestCase
      */
     public function testGetFlushSystemUrl(): void
     {
-        $this->assertStringStartsWith('https', $this->subject->getFlushSystemUrl());
+        $this->assertStringStartsWith('http', $this->subject->getFlushSystemUrl());
     }
 }
