78e95c06b96416343926e28006c0d2ffa9e019c1
Sven Reichel <github-sr@hotmail.com>
2024-11-03 15:18:17 +0100

    rector

--- app/code/core/Mage/Catalog/Block/Product/Abstract.php
+++ app/code/core/Mage/Catalog/Block/Product/Abstract.php
@@ -271,7 +271,6 @@ abstract class Mage_Catalog_Block_Product_Abstract extends Mage_Core_Block_Templ
     /**
      * Get product reviews summary
      *
-     * @param Mage_Catalog_Model_Product $product
      * @param string|false $templateType
      * @param bool $displayIfNoReviews
      * @return string
