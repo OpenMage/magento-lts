844641426a384cae4e1f5ecd9a0bccc3f2c8e022
Sven Reichel <github-sr@hotmail.com>
2024-11-12 10:21:22 +0100

    Update app/code/core/Mage/Core/Model/Layout.php
    Co-authored-by: Ng Kiat Siong <kiatsiong.ng@gmail.com>

--- app/code/core/Mage/Core/Model/Layout.php
+++ app/code/core/Mage/Core/Model/Layout.php
@@ -462,7 +462,7 @@ class Mage_Core_Model_Layout extends Varien_Simplexml_Config
             }
             $name = 'ANONYMOUS_' . count($this->_blocks);
         } elseif (isset($this->_blocks[$name]) && Mage::getIsDeveloperMode()) {
-            #Mage::throwException(Mage::helper('core')->__('Block with name "%s" already exists', $name));
+            //Mage::throwException(Mage::helper('core')->__('Block with name "%s" already exists', $name));
         }
 
         $block->setType($type);
