7e7532276c082889994da9880eb57648d63649cc
Sven Reichel <github-sr@hotmail.com>
2024-11-24 04:33:40 +0100

    Bugfix: Mage_Adminhtml_Helper_Dashboard_Data->countStores() (#4370)

--- app/code/core/Mage/Adminhtml/Helper/Dashboard/Data.php
+++ app/code/core/Mage/Adminhtml/Helper/Dashboard/Data.php
@@ -29,7 +29,7 @@ class Mage_Adminhtml_Helper_Dashboard_Data extends Mage_Core_Helper_Data
     /**
      * Retrieve stores configured in system.
      *
-     * @return array
+     * @return Mage_Core_Model_Resource_Store_Collection
      */
     public function getStores()
     {
@@ -47,7 +47,7 @@ class Mage_Adminhtml_Helper_Dashboard_Data extends Mage_Core_Helper_Data
      */
     public function countStores()
     {
-        return count($this->_stores->getItems());
+        return count($this->getStores()->getItems());
     }
 
     /**
