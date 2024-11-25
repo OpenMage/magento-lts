af8dad9cec3934af9d5c5d5ad999b94a0341af18
Sven Reichel <github-sr@hotmail.com>
2024-11-25 12:08:26 +0100

    PHPUnit: added tests (#4340)
    * added tests
    
    * phpstan
    
    * added tests
    
    * added tests
    
    * fix .... sample data change?
    
    * DOCs update (replaced some mixed typtes)
    
    * added tests
    
    * cs
    
    * Updated test
    
    * Added test
    
    * belongs not to that PR
    
    * belongs not to that PR

--- tests/unit/Mage/AdminNotification/Model/FeedTest.php
+++ tests/unit/Mage/AdminNotification/Model/FeedTest.php
@@ -20,6 +20,7 @@ namespace OpenMage\Tests\Unit\Mage\AdminNotification\Model;
 use Mage;
 use Mage_AdminNotification_Model_Feed;
 use PHPUnit\Framework\TestCase;
+use SimpleXMLElement;
 
 class FeedTest extends TestCase
 {
@@ -47,5 +48,23 @@ class FeedTest extends TestCase
     public function testCheckUpdate(): void
     {
         $this->assertInstanceOf(Mage_AdminNotification_Model_Feed::class, $this->subject->checkUpdate());
+    }
+
+    /**
+     * @group Mage_AdminNotification
+     * @group Mage_AdminNotification_Model
+     */
+    public function testGetFeedData(): void
+    {
+        $this->assertInstanceOf(SimpleXMLElement::class, $this->subject->getFeedData());
+    }
+
+    /**
+     * @group Mage_AdminNotification
+     * @group Mage_AdminNotification_Model
+     */
+    public function testGetFeedXml(): void
+    {
+        $this->assertInstanceOf(SimpleXMLElement::class, $this->subject->getFeedXml());
     }
 }
