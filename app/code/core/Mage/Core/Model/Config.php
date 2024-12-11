e9556401de1296d3c86a7f760a95285132db68b9
Justin Beaty <justin.beaty@gmail.com>
2024-12-10 16:42:55 -0800

    fix

--- app/code/core/Mage/Core/Model/Config.php
+++ app/code/core/Mage/Core/Model/Config.php
@@ -1517,8 +1517,10 @@ class Mage_Core_Model_Config extends Mage_Core_Model_Config_Base
 
     /**
      * Retrieve resource helper class name
+     *
+     * @return string|false
      */
-    public function getResourceHelperClassName(string $moduleAlias): string|false
+    public function getResourceHelperClassName(string $moduleAlias)
     {
         $connectionModel = $this->_getResourceConnectionModel($moduleAlias);
         $modelClass = sprintf('%s/helper_%s', $moduleAlias, $connectionModel);
