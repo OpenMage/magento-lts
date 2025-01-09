18063d29e80fdfb5b0309494833cf178bffb4278
Sven Reichel <github-sr@hotmail.com>
2025-01-09 11:57:57 +0100

    shorter copyright

--- app/code/core/Mage/Core/Model/Security/HtmlEscapedString.php
+++ app/code/core/Mage/Core/Model/Security/HtmlEscapedString.php
@@ -3,12 +3,6 @@
 declare(strict_types=1);
 
 /**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
  * @category   Mage
  * @package    Mage_Core
  * @copyright  Copyright (c) 2024 The OpenMage Contributors (https://www.openmage.org)
--- app/code/core/Mage/Cron/Model/Schedule.php
+++ app/code/core/Mage/Cron/Model/Schedule.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
  * @category   Mage
  * @package    Mage_Cron
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
