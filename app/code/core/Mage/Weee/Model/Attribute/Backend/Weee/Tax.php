1a366187f39fd7e6c6e930c0d9848ce78a9f0015
Sven Reichel <github-sr@hotmail.com>
2025-01-09 13:10:30 +0100

    updated dates

--- app/code/core/Mage/Weee/Model/Attribute/Backend/Weee/Tax.php
+++ app/code/core/Mage/Weee/Model/Attribute/Backend/Weee/Tax.php
@@ -4,7 +4,7 @@
  * @category   Mage
  * @package    Mage_Weee
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2019-2024 The OpenMage Contributors (https://www.openmage.org)
+ * @copyright  Copyright (c) 2019-2025 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Mage_Weee_Model_Attribute_Backend_Weee_Tax extends Mage_Catalog_Model_Product_Attribute_Backend_Price
--- lib/Unserialize/Reader/ArrKey.php
+++ lib/Unserialize/Reader/ArrKey.php
@@ -3,7 +3,7 @@
  * @category   Unserialize
  * @package    Unserialize_Reader
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2022 The OpenMage Contributors (https://www.openmage.org)
+ * @copyright  Copyright (c) 2022-2025 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
