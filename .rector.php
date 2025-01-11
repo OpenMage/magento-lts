dab90182662c741f65f19d699c7c34ae77ba43e3
Sven Reichel <github-sr@hotmail.com>
2025-01-11 18:40:49 +0100

    Rector: updated config for php7
    - "withPreparedSets()" works, but it made for php8 and give warnings

--- .rector.php
+++ .rector.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector as CodeQuality;
 use Rector\DeadCode\Rector as DeadCode;
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector as TypeDeclaration;
 
 return RectorConfig::configure()
@@ -22,7 +24,11 @@ return RectorConfig::configure()
         CodeQuality\BooleanNot\SimplifyDeMorganBinaryRector::class,
         CodeQuality\If_\SimplifyIfReturnBoolRector::class,
         __DIR__ . '/shell/translations.php',
-        __DIR__ . '/shell/update-copyright.php.php'
+        __DIR__ . '/shell/update-copyright.php.php',
+    ])
+    ->withSets([
+        PHPUnitSetList::PHPUNIT_90,
+        SetList::PRIVATIZATION,
     ])
     ->withRules([
         CodeQuality\BooleanNot\ReplaceMultipleBooleanNotRector::class,
@@ -40,23 +46,4 @@ return RectorConfig::configure()
         DeadCode\ClassMethod\RemoveUselessReturnTagRector::class,
         DeadCode\Property\RemoveUselessVarTagRector::class,
         TypeDeclaration\ClassMethod\ReturnNeverTypeRector::class,
-    ])
-    ->withPreparedSets(
-        false,
-        false,
-        false,
-        false,
-        true,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        true,
-        false,
-        false,
-        false,
-        false,
-        true,
-    );
+    ]);
