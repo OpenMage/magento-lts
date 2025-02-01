e5feff0384a48be5a6b1ae3da98a2867348b734b
Milan Davídek <midlan@users.noreply.github.com>
2025-02-01 07:54:19 +0100

    php 8.3: fix catch for empty sitemap filename (#4521)

--- app/code/core/Mage/Sitemap/Model/Observer.php
+++ app/code/core/Mage/Sitemap/Model/Observer.php
@@ -68,7 +68,7 @@ class Mage_Sitemap_Model_Observer
 
             try {
                 $sitemap->generateXml();
-            } catch (Exception $e) {
+            } catch (Throwable $e) {
                 $errors[] = $e->getMessage();
             }
         }
