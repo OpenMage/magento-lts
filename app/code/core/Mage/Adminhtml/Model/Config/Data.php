523c4849b2108e4301ba3810429965fd83ca90bc
Pascal Querner <p.querner@ottomatik.de>
2025-02-08 15:47:53 +0100

    feat?: backend: show data from env

--- app/code/core/Mage/Adminhtml/Model/Config/Data.php
+++ app/code/core/Mage/Adminhtml/Model/Config/Data.php
@@ -355,6 +355,12 @@ class Mage_Adminhtml_Model_Config_Data extends Varien_Object
             }
         }
 
+        if (!$full) {
+            /** @var Mage_Core_Helper_EnvironmentConfigLoader $environmentConfigLoaderHelper */
+            $environmentConfigLoaderHelper = Mage::helper('core/environmentConfigLoader');
+            $envConfig = $environmentConfigLoaderHelper->getAsArray($scope = $this->getScope());
+            $config = array_merge($config, $envConfig);
+        }
         return $config;
     }
 
