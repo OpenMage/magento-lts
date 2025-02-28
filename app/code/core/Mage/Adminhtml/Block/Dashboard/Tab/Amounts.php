6f594653fb65aa122f3a45fc239771511e936bd7
Sven Reichel <github-sr@hotmail.com>
2025-02-28 17:11:40 +0100

    Replaced google dashboard charts with Chart.js (via composer) (#4435)
    * Replaced google dashboard charts with Chart.js
    
    * Changed chart size
    
    * Typo [skip ci]
    
    * fixes
    
    * use random_int()
    
    * removed duplicated code
    
    * sonar-1
    
    * sonar-2
    
    * check for installed module
    
    * update
    
    * Update app/code/core/Mage/Adminhtml/Block/Dashboard.php
    
    Co-authored-by: Colin Mollenhour <colin@mollenhour.com>
    
    * copyright, ref https://github.com/MahoCommerce/maho/commit/b4e66fc0a57f735debf6efc5f88f3956c70190bb
    
    * moved constants to correct class
    
    * docs
    
    * Update app/code/core/Mage/Reports/Helper/Data.php
    
    ---------
    
    Co-authored-by: Colin Mollenhour <colin@mollenhour.com>

--- app/code/core/Mage/Adminhtml/Block/Dashboard/Tab/Amounts.php
+++ app/code/core/Mage/Adminhtml/Block/Dashboard/Tab/Amounts.php
@@ -22,6 +22,11 @@
  */
 class Mage_Adminhtml_Block_Dashboard_Tab_Amounts extends Mage_Adminhtml_Block_Dashboard_Graph
 {
+    protected $_axisMaps = [
+        'x' => 'range',
+        'y' => 'revenue',
+    ];
+
     /**
      * Initialize object
      */
@@ -38,17 +43,7 @@ class Mage_Adminhtml_Block_Dashboard_Tab_Amounts extends Mage_Adminhtml_Block_Da
      */
     protected function _prepareData()
     {
-        $this->setDataHelperName('adminhtml/dashboard_order');
-        $this->getDataHelper()->setParam('store', $this->getRequest()->getParam('store'));
-        $this->getDataHelper()->setParam('website', $this->getRequest()->getParam('website'));
-        $this->getDataHelper()->setParam('group', $this->getRequest()->getParam('group'));
-
         $this->setDataRows('revenue');
-        $this->_axisMaps = [
-            'x' => 'range',
-            'y' => 'revenue',
-        ];
-
         parent::_prepareData();
     }
 }
