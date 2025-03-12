fc5ba06d74cbebaa559b74b1347affc3b14fafb0
Sven Reichel <github-sr@hotmail.com>
2025-03-12 20:38:23 +0100

    PhpStan fixes

--- app/code/core/Mage/Catalog/Model/Product/Image.php
+++ app/code/core/Mage/Catalog/Model/Product/Image.php
@@ -369,7 +369,7 @@ class Mage_Catalog_Model_Product_Image extends Mage_Core_Model_Abstract
 
         $baseFile = $baseDir . $file;
 
-        if ((!$file) || (!file_exists($baseFile))) {
+        if (!file_exists($baseFile)) {
             throw new Exception(Mage::helper('catalog')->__('Image file was not found.'));
         }
 
