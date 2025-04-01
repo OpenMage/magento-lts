471fecf4893214a32e52cb8150980b872d666673
hirale <22028058+hirale@users.noreply.github.com>
2025-04-01 14:55:57 +0200

    fix: fix the issue pointed out by sonarqube

--- app/code/core/Mage/Reports/Model/Resource/Order/Collection.php
+++ app/code/core/Mage/Reports/Model/Resource/Order/Collection.php
@@ -341,22 +341,19 @@ class Mage_Reports_Model_Resource_Order_Collection extends Mage_Sales_Model_Reso
                 break;
 
             case Mage_Reports_Helper_Data::PERIOD_1_MONTH:
+            case Mage_Reports_Helper_Data::PERIOD_3_MONTHS:
+            case Mage_Reports_Helper_Data::PERIOD_6_MONTHS:
                 $dateStart->setDay(Mage::getStoreConfig('reports/dashboard/mtd_start'));
+                if ($range === Mage_Reports_Helper_Data::PERIOD_3_MONTHS) {
+                    $dateStart->subMonth(2);
+                } elseif ($range === Mage_Reports_Helper_Data::PERIOD_6_MONTHS) {
+                    $dateStart->subMonth(5);
+                }
                 break;
 
             case Mage_Reports_Helper_Data::PERIOD_CUSTOM:
                 $dateStart = $customStart ?: $dateEnd;
                 $dateEnd   = $customEnd ?: $dateEnd;
-                break;
-
-            case Mage_Reports_Helper_Data::PERIOD_3_MONTHS:
-                $dateStart->setDay(Mage::getStoreConfig('reports/dashboard/mtd_start'));
-                $dateStart->subMonth(2);
-                break;
-
-            case Mage_Reports_Helper_Data::PERIOD_6_MONTHS:
-                $dateStart->setDay(Mage::getStoreConfig('reports/dashboard/mtd_start'));
-                $dateStart->subMonth(5);
                 break;
 
             case Mage_Reports_Helper_Data::PERIOD_1_YEAR:
