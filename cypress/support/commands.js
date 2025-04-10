bdcba9d39cf8a299f0db47975ac1b9854774c961
Sven Reichel <github-sr@hotmail.com>
2025-04-10 10:39:17 +0200

    update

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,21 +31,26 @@ Cypress.Commands.add('adminLogInValidUser', () => {
 })
 
 Cypress.Commands.add('adminLogInInvalidUser', () => {
-    cy.get('#username').clear().type(credentials.NoAdmin.login).should('have.value', credentials.NoAdmin.login);
-    cy.get('#login').clear().type(credentials.NoAdmin.password).should('have.value', credentials.NoAdmin.password);
+    const username = credentials.NoAdmin.login;
+    const password = credentials.NoAdmin.password;
+
+    cy.log(`Logging in as ${username}`);
+    cy.get('#username').clear().type(username).should('have.value', username);
+    cy.get('#login').clear().type(password).should('have.value', password);
     cy.get('.form-button').click();
 })
 
 Cypress.Commands.add('adminGoToTestRoute', (route) => {
     cy.get('body').then($body => {
-        if ($body.find('#message-popup-window .message-popup-head a').length > 0) {
+        const popup = '#message-popup-window .message-popup-head a';
+        if ($body.find(popup).length > 0) {
             cy.log('Dismissing popup');
-            cy.get('#message-popup-window .message-popup-head a').click({force: true});
+            cy.get(popup).click({force: true});
         }
     });
 
     cy.log(`Clicking on "${route.h3}" menu`);
-    cy.get(route.id).click({force: true});
+    cy.get(route._id).click({force: true});
     cy.url().should('include', route.url);
 })
 
@@ -54,17 +59,17 @@ Cypress.Commands.add('adminTestRoute', (route) => {
     cy.get(route._h3).should('include.text', route.h3);
 
     cy.log('Checking for active parent class');
-    cy.get(route.parent).should('have.class', 'active');
+    cy.get(route._id_parent).should('have.class', 'active');
 
     cy.log('Checking for active class');
-    cy.get(route.id).should('have.class', 'active');
+    cy.get(route._id).should('have.class', 'active');
 })
 
 Cypress.Commands.add('adminGetConfiguration', (route) => {
     cy.get('#nav-admin-system-config').click({force: true});
     cy.url().should('include', 'system_config/index');
 
-    cy.get(route.id).click({force: true});
+    cy.get(route._id).click({force: true});
     cy.url().should('include', route.url);
     cy.get('.content-header h3').should('include.text', route.h3);
 })
