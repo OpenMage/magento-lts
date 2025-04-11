3dad8ff1449acc18ef466c75aabdfafa92dcc388
Sven Reichel <github-sr@hotmail.com>
2025-04-11 11:53:43 +0200

    update

--- cypress/e2e/paths/backend/system/config/catalog/configswatches.cy.js
+++ cypress/e2e/paths/backend/system/config/catalog/configswatches.cy.js
@@ -46,7 +46,7 @@ describe(`Checks admin system "${route.h3}" settings`, () => {
         cy.adminSaveConfiguration();
 
         cy.log('Checking for error messages');
-        const error = validation.digits.error;
+        const error = cy.openmage.validation.digits.error;
         cy.get('#advice-validate-digits-configswatches_product_detail_dimensions_height').should('include.text', error);
         cy.get('#advice-validate-digits-configswatches_product_detail_dimensions_width').should('include.text', error);
         cy.get('#advice-validate-digits-configswatches_product_listing_dimensions_height').should('include.text', error);
