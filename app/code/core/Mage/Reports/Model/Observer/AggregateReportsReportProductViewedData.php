394dfc0d37fc6b34a872e7da9641895ddb0458ed
Sven Reichel <github-sr@hotmail.com>
2025-04-13 12:25:23 +0200

    reports

---
+++ app/code/core/Mage/Reports/Model/Observer/AggregateReportsReportProductViewedData.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * OpenMage
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available at https://opensource.org/license/osl-3-0-php
+ *
+ * @category   Mage
+ * @package    Mage_Reports
+ * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
+ * @copyright  Copyright (c) 2025 The OpenMage Contributors (https://www.openmage.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Reports Observer
+ */
+class Mage_Reports_Model_Observer_AggregateReportsReportProductViewedData implements Mage_Core_Observer_Interface
+{
+    /**
+     * Refresh viewed report statistics for last day
+     *
+     * @throws Zend_Date_Exception
+     */
+    public function execute(Varien_Event_Observer $observer): self
+    {
+        Mage::app()->getLocale()->emulate(0);
+        $currentDate = Mage::app()->getLocale()->date();
+        $date = $currentDate->subHour(25);
+        Mage::getResourceModel('reports/report_product_viewed')->aggregate($date);
+        Mage::app()->getLocale()->revert();
+        return $this;
+    }
+}
