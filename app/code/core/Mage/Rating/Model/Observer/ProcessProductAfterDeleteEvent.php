fbbb1984e216afce5b2594f06c8ceff126fde09b
Sven Reichel <github-sr@hotmail.com>
2025-04-13 12:30:39 +0200

    rating

--- app/code/core/Mage/Rating/Model/Observer/ProcessProductAfterDeleteEvent.php
+++ app/code/core/Mage/Rating/Model/Observer/ProcessProductAfterDeleteEvent.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * OpenMage
  *
@@ -10,7 +12,7 @@
  * @category   Mage
  * @package    Mage_Rating
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2020-2024 The OpenMage Contributors (https://www.openmage.org)
+ * @copyright  Copyright (c) 2025 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -20,14 +22,12 @@
  * @category   Mage
  * @package    Mage_Rating
  */
-class Mage_Rating_Model_Observer
+class Mage_Rating_Model_Observer_ProcessProductAfterDeleteEvent implements Mage_Core_Observer_Interface
 {
     /**
      * Cleanup product ratings after product delete
-     *
-     * @return Mage_Rating_Model_Observer
      */
-    public function processProductAfterDeleteEvent(Varien_Event_Observer $observer)
+    public function execute(Varien_Event_Observer $observer): self
     {
         /** @var Mage_Catalog_Model_Product $eventProduct */
         $eventProduct = $observer->getEvent()->getProduct();
