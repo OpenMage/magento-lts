7ee8a1997f011a707401b4bf49bdb15f77e3ba96
Sven Reichel <github-sr@hotmail.com>
2025-04-13 12:03:21 +0200

    rss

--- app/code/core/Mage/Rss/Model/Observer/Abstract.php
+++ app/code/core/Mage/Rss/Model/Observer/Abstract.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * OpenMage
  *
@@ -10,7 +12,7 @@
  * @category   Mage
  * @package    Mage_Rss
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://www.openmage.org)
+ * @copyright  Copyright (c) 2025 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -20,7 +22,7 @@
  * @category   Mage
  * @package    Mage_Rss
  */
-class Mage_Rss_Model_Observer
+abstract class Mage_Rss_Model_Observer_Abstract
 {
     /**
      * Factory instance
@@ -40,30 +42,6 @@ class Mage_Rss_Model_Observer
     {
         $this->_factory = !empty($args['factory']) ? $args['factory'] : Mage::getSingleton('core/factory');
         $this->_app = !empty($args['app']) ? $args['app'] : Mage::app();
-    }
-
-    /**
-     * Clean cache for catalog review rss
-     */
-    public function reviewSaveAfter(Varien_Event_Observer $observer)
-    {
-        $this->_cleanCache(Mage_Rss_Block_Catalog_Review::CACHE_TAG);
-    }
-
-    /**
-     * Clean cache for notify stock rss
-     */
-    public function salesOrderItemSaveAfterNotifyStock(Varien_Event_Observer $observer)
-    {
-        $this->_cleanCache(Mage_Rss_Block_Catalog_NotifyStock::CACHE_TAG);
-    }
-
-    /**
-     * Clean cache for catalog new orders rss
-     */
-    public function salesOrderItemSaveAfterOrderNew(Varien_Event_Observer $observer)
-    {
-        $this->_cleanCache(Mage_Rss_Block_Order_New::CACHE_TAG);
     }
 
     /**
