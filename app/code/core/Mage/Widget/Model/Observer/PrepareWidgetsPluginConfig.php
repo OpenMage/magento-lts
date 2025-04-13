4f9e1776830ddb5f297010624044c4c23785c9f2
Sven Reichel <github-sr@hotmail.com>
2025-04-13 02:02:55 +0200

    widget

--- app/code/core/Mage/Widget/Model/Observer/PrepareWidgetsPluginConfig.php
+++ app/code/core/Mage/Widget/Model/Observer/PrepareWidgetsPluginConfig.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * OpenMage
  *
@@ -10,7 +12,7 @@
  * @category   Mage
  * @package    Mage_Widget
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2019-2024 The OpenMage Contributors (https://www.openmage.org)
+ * @copyright  Copyright (c) 2025 The OpenMage Contributors (https://www.openmage.org)
  * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -20,21 +22,18 @@
  * @category   Mage
  * @package    Mage_Widget
  */
-class Mage_Widget_Model_Observer
+class Mage_Widget_Model_Observer_PrepareWidgetsPluginConfig implements Mage_Core_Observer_Interface
 {
     /**
      * Add additional settings to wysiwyg config for Widgets Insertion Plugin
-     *
-     * @return $this
      */
-    public function prepareWidgetsPluginConfig(Varien_Event_Observer $observer)
+    public function execute(Varien_Event_Observer $observer): void
     {
-        $config = $observer->getEvent()->getConfig();
-
-        if ($config->getData('add_widgets')) {
+        /** @var Varien_Object $config */
+        $config = $observer->getEvent()->getDataByKey('config');
+        if ($config->getDataByKey('add_widgets')) {
             $settings = Mage::getModel('widget/widget_config')->getPluginSettings($config);
             $config->addData($settings);
         }
-        return $this;
     }
 }
