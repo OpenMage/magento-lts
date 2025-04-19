eb59e17ac86c3bab49c6a23b8c53a5e4f802621d
Hans Mackowiak <h.mackowiak@b3-it.de>
2025-04-19 10:06:46 +0200

    Update app/code/core/Mage/Csp/Block/Csp.php
    Co-authored-by: Fabrizio Balliano <fabrizio.balliano@gmail.com>

--- app/code/core/Mage/Csp/Block/Csp.php
+++ app/code/core/Mage/Csp/Block/Csp.php
@@ -3,14 +3,14 @@
 /**
  * OpenMage
  *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE_AFL.txt.
- * It is also available at https://opensource.org/license/afl-3-0-php
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available at https://opensource.org/license/osl-3-0-php
  *
  * @category   Mage
  * @package    Mage_Csp
  * @copyright  Copyright (c) 2025 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 class Mage_Csp_Block_Csp extends Mage_Core_Block_Abstract
