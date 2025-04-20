ad34f896bfb1ce2d3435d5c7f08957464e5ac7f6
Sven Reichel <github-sr@hotmail.com>
2025-04-20 07:01:07 +0200

    sonar 4

--- app/code/core/Mage/Adminhtml/Model/System/Config/Source/Date/Short.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Source/Date/Short.php
@@ -25,12 +25,14 @@ class Mage_Adminhtml_Model_System_Config_Source_Date_Short
 {
     public function toOptionArray()
     {
+        $format = 'm/d/y';
+
         $arr = [];
         $arr[] = ['label' => '', 'value' => ''];
-        $arr[] = ['label' => sprintf('MM/DD/YY (%s)', Carbon::now()->format('m/d/y')), 'value' => '%m/%d/%y'];
-        $arr[] = ['label' => sprintf('MM/DD/YYYY (%s)', Carbon::now()->format('m/d/y')), 'value' => '%m/%d/%Y'];
-        $arr[] = ['label' => sprintf('DD/MM/YY (%s)', Carbon::now()->format('m/d/y')), 'value' => '%d/%m/%y'];
-        $arr[] = ['label' => sprintf('DD/MM/YYYY (%s)', Carbon::now()->format('m/d/y')), 'value' => '%d/%m/%Y'];
+        $arr[] = ['label' => sprintf('MM/DD/YY (%s)', Carbon::now()->format($format)), 'value' => '%m/%d/%y'];
+        $arr[] = ['label' => sprintf('MM/DD/YYYY (%s)', Carbon::now()->format($format)), 'value' => '%m/%d/%Y'];
+        $arr[] = ['label' => sprintf('DD/MM/YY (%s)', Carbon::now()->format($format)), 'value' => '%d/%m/%y'];
+        $arr[] = ['label' => sprintf('DD/MM/YYYY (%s)', Carbon::now()->format($format)), 'value' => '%d/%m/%Y'];
         return $arr;
     }
 }
