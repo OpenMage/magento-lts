b4a8cf3bb8fa8311a45c36804007d7507b1042be
Sven Reichel <github-sr@hotmail.com>
2025-04-21 15:25:12 +0200

    copyright, ref #4767

--- tests/unit/Mage/Reports/Model/Resource/Report/CollectionTest.php
+++ tests/unit/Mage/Reports/Model/Resource/Report/CollectionTest.php
@@ -1,16 +1,9 @@
 <?php
 
 /**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   OpenMage
- * @package    OpenMage_Tests
- * @copyright  Copyright (c) 2025 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright  For copyright and license information, read the COPYING.txt file.
+ * @link       /COPYING.txt
+ * @license    Open Software License (OSL 3.0)
  */
 
 declare(strict_types=1);
