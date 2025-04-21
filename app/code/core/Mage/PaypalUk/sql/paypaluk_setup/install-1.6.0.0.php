e1b6c44984f69d7bc84f79d67a7df1d9368454ab
Sven Reichel <github-sr@hotmail.com>
2025-04-21 12:46:17 +0200

    removed old 1

--- app/code/core/Mage/PaypalUk/sql/paypaluk_setup/install-1.6.0.0.php
+++ app/code/core/Mage/PaypalUk/sql/paypaluk_setup/install-1.6.0.0.php
@@ -5,9 +5,6 @@
  * @link       /COPYING.txt
  * @license    Open Software License (OSL 3.0)
  * @package    Mage_PaypalUk
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
 /** @var Mage_Core_Model_Resource_Setup $installer */
--- app/code/core/Mage/Usa/Block/Adminhtml/Dhl/Unitofmeasure.php
+++ app/code/core/Mage/Usa/Block/Adminhtml/Dhl/Unitofmeasure.php
@@ -5,9 +5,6 @@
  * @link       /COPYING.txt
  * @license    Open Software License (OSL 3.0)
  * @package    Mage_Usa
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2021-2024 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
 /**
