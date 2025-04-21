07b17ffe20e0def9c7d3d8bc87b83ebe1e300864
Sven Reichel <github-sr@hotmail.com>
2025-04-21 12:33:06 +0200

    added new info

--- app/code/core/Mage/Weee/Model/Resource/Setup.php
+++ app/code/core/Mage/Weee/Model/Resource/Setup.php
@@ -1,12 +1,9 @@
 <?php
 
 /**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
+ * @copyright  For copyright and license information, read the COPYING.txt file.
+ * @link       /COPYING.txt
+ * @license    Open Software License (OSL 3.0)
  * @package    Mage_Weee
  * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
  * @copyright  Copyright (c) 2022-2024 The OpenMage Contributors (https://www.openmage.org)
