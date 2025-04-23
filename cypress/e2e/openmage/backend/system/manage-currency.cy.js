4ec360169308b5ca53f4722ef2bf82a1e886b702
Sven Reichel <github-sr@hotmail.com>
2025-04-23 07:13:12 +0200

    update

--- cypress/e2e/openmage/backend/system/manage-currency.cy.js
+++ cypress/e2e/openmage/backend/system/manage-currency.cy.js
@@ -10,17 +10,20 @@ describe(`Checks admin system "${route.h3}"`, () => {
         cy.adminTestRoute(route);
     });
 
+    const warning = 'Invalid input data for USD => EUR rate';
+    const success = 'All valid rates have been saved.';
+
     it(`tests empty currency`, () => {
         cy.get(route.__validation._input.from).clear();
         cy.openmage.validation.saveAction(route._buttonSave);
-        cy.get('.warning-msg').should('include.text', 'Invalid input data for USD => EUR rate');
-        cy.get('.success-msg').should('include.text', 'All valid rates have been saved.');
+        cy.get('.warning-msg').should('include.text', warning);
+        cy.get('.success-msg').should('include.text', success);
     });
 
     it(`tests string currency`, () => {
         cy.get(route.__validation._input.from).clear().type('abc');
         cy.openmage.validation.saveAction(route._buttonSave);
-        cy.get('.warning-msg').should('include.text', 'Invalid input data for USD => EUR rate');
-        cy.get('.success-msg').should('include.text', 'All valid rates have been saved.');
+        cy.get('.warning-msg').should('include.text', warning);
+        cy.get('.success-msg').should('include.text', success);
     });
 });
