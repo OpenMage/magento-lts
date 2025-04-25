07bb0c3ef04de707e4653ce6739eacb8403b685e
Sven Reichel <github-sr@hotmail.com>
2025-04-25 20:31:21 +0200

    Added Cypress E2E tests (#4759)
    * added sample test
    
    * added workflow
    
    * added workflow - DDEV config
    
    * added workflow - DDEV config fix
    
    * cypress config
    
    * cypress test
    
    * config
    
    * test
    
    * test
    
    * tests
    
    * tests
    
    * tests
    
    * tests
    
    * tests
    
    * tests
    
    * tests
    
    * update
    
    * update
    
    * update
    
    * update
    
    * minor update
    
    * minor update
    
    * minor update
    
    * minor update
    
    * minor update
    
    * minor update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * typo
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * .gitignore
    
    * .gitignore
    
    * test
    
    * test
    
    * revert test
    
    * workflow
    
    * added test
    
    * update
    
    * update
    
    * added pseudo test, ref #4741
    
    * test apache/nginx
    
    - maybe useful to test .htaccess and nginx-config (?)
    
    ---------
    
    Co-authored-by: Colin Mollenhour <colin@mollenhour.com>

---
+++ cypress/e2e/openmage/backend/login.cy.js
@@ -0,0 +1,33 @@
+describe('Check admin login', () => {
+    beforeEach('Log in the user', () => {
+        cy.visit('/admin');
+    });
+
+    const login = cy.openmage.login.admin
+
+    it('tests valid login', () => {
+        const username = login.username.value;
+        const password = login.password.value;
+
+        cy.log(`Logging in as ${username}`);
+        cy.get(login.username._id).clear().type(username).should('have.value', username);
+        cy.get(login.password._id).clear().type(password).should('have.value', password);
+        cy.get(login._submit.__selector).click();
+    })
+
+    it('tests invalid login', () => {
+        const username = 'abc';
+        const password = '123';
+
+        cy.log(`Logging in as ${username}`);
+        cy.get(login.username._id).clear().type(username).should('have.value', username);
+        cy.get(login.password._id).clear().type(password).should('have.value', password);
+        cy.get(login._submit.__selector).click();
+    })
+
+    it('tests empty login', () => {
+        cy.get(login.username._id).clear().should('have.value', '');
+        cy.get(login.password._id).clear().should('have.value', '');
+        cy.get(login._submit.__selector).click();
+    })
+})
