8ed54169044df47358a853d65ca6b4c0b5a62109
Addison <8360474+addison74@users.noreply.github.com>
2025-09-27 15:24:56 +0300

    Correct docblock for _allowCreateFolders property
    Fix typo in docblock for _allowCreateFolders

--- lib/Varien/Io/File.php
+++ lib/Varien/Io/File.php
@@ -310,7 +310,7 @@ class Varien_Io_File extends Varien_Io_Abstract
     }
 
     /**
-     * Used to set {@link _allowCreateFolders} value
+     * Used to set the _allowCreateFolders} value
      *
      * @param bool $flag
      * @access public
