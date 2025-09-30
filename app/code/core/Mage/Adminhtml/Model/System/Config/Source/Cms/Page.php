87bd2762a8b0313af1eff929e681ab6a48fce7e3
Addison <8360474+addison74@users.noreply.github.com>
2025-09-30 13:14:57 +0300

    Fix formatting in Page.php

--- app/code/core/Mage/Adminhtml/Model/System/Config/Source/Cms/Page.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Source/Cms/Page.php
@@ -13,7 +13,7 @@
 class Mage_Adminhtml_Model_System_Config_Source_Cms_Page
 {
     protected $_options;
-    
+
     public function toOptionArray()
     {
         $storeCode = Mage::app()->getRequest()->getParam('store');
