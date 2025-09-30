9437d7734e13399feb9fbb5913ed190284c8dabb
Addison <8360474+addison74@users.noreply.github.com>
2025-09-30 13:09:29 +0300

    Add protected property for options in Cms Page model
    Added a protected property to hold options.

--- app/code/core/Mage/Adminhtml/Model/System/Config/Source/Cms/Page.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Source/Cms/Page.php
@@ -12,6 +12,8 @@
  */
 class Mage_Adminhtml_Model_System_Config_Source_Cms_Page
 {
+    protected $_options;
+    
     public function toOptionArray()
     {
         $storeCode = Mage::app()->getRequest()->getParam('store');
