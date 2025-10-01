2553366e497e28c1ad20ee1534343cd789b6f39a
Addison <8360474+addison74@users.noreply.github.com>
2025-10-01 21:51:52 +0300

    Fix formatting of username assignment

--- app/code/core/Mage/Admin/Model/User.php
+++ app/code/core/Mage/Admin/Model/User.php
@@ -142,7 +142,7 @@ class Mage_Admin_Model_User extends Mage_Core_Model_Abstract
         }
 
         if ($this->getUsername()) {
-            $data['username'] = trim((string)$this->getUsername());
+            $data['username'] = trim((string) $this->getUsername());
         }
 
         if ($this->getNewPassword()) {
