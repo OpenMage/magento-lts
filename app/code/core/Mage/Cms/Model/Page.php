5a0ddd46fcb9fa961071e0613a694d5f2ab9325d
addison74 <8360474+ADDISON74@users.noreply.github.com>
2025-10-02 14:05:53 +0300

    Fix and improve the cms page warning message when disabling

--- app/code/core/Mage/Cms/Model/Page.php
+++ app/code/core/Mage/Cms/Model/Page.php
@@ -172,4 +172,174 @@ class Mage_Cms_Model_Page extends Mage_Core_Model_Abstract
     {
         return $this->_getResource()->isUsedInStoreConfig($this, $paths);
     }
+
+    /**
+     * Joins an array of strings into a grammatically correct English list using commas and 'and'.
+     *
+     * Examples:
+     * - ['A']         => 'A'
+     * - ['A', 'B']    => 'A and B'
+     * - ['A', 'B', 'C'] => 'A, B, and C'
+     *
+     * @param array $items Array of strings to join
+     * @return string
+     */
+    protected function _joinWithCommaAnd(array $items)
+    {
+        $count = count($items);
+        if ($count === 0) {
+            return '';
+        } elseif ($count === 1) {
+            return $items[0];
+        } elseif ($count === 2) {
+            return $items[0] . ' and ' . $items[1];
+        } else {
+            return implode(', ', array_slice($items, 0, -1)) . ', and ' . $items[$count - 1];
+        }
+    }
+
+    /**
+     * Checks if the CMS page is used as a default page (Home, No Route, No Cookies) for any store view or website,
+     * and prevents disabling it with a warning message.
+     *
+     * The warning message format is configurable via delimiter variables:
+     * - $delimiter: between page type and store views
+     * - $labelDelimiter: between website and store view
+     * The list of usages is formatted using _joinWithCommaAnd for proper English grammar.
+     *
+     * Throws a Mage_Core_Exception if the page is in use as a default page.
+     *
+     * @return Mage_Cms_Model_Page
+     * @throws Mage_Core_Exception
+     */
+    protected function _beforeSave()
+    {
+        parent::_beforeSave();
+
+        // Delimiters (easy to edit)
+        $delimiter        = ' ';    // Between page type and scope label, it can be used ' in '
+        $labelDelimiter   = ' > ';   // Between website and store view
+
+        if ($this->getIsActive() == self::STATUS_DISABLED) {
+            /** @var array<string, array<string, bool>> $usage */
+            $usage = [
+                'web/default/cms_home_page'    => [],
+                'web/default/cms_no_route'     => [],
+                'web/default/cms_no_cookies'   => [],
+            ];
+
+            $configPaths = [
+                'web/default/cms_home_page'    => Mage::helper('cms')->__('Home Page'),
+                'web/default/cms_no_route'     => Mage::helper('cms')->__('No Route Page'),
+                'web/default/cms_no_cookies'   => Mage::helper('cms')->__('No Cookies Page'),
+            ];
+
+            $collection = Mage::getModel('core/config_data')->getCollection()
+                ->addFieldToFilter('path', array('in' => array_keys($configPaths)))
+                ->addFieldToFilter('value', $this->getIdentifier());
+
+            // Build associative array by website for grouping
+            $scopesByType = [
+                'Default Config' => [],
+                // website name => [null => true, store view name => true]
+            ];
+
+            foreach ($collection as $item) {
+                $scope = $item->getScope();
+                $scopeId = $item->getScopeId();
+                $path = $item->getPath();
+
+                if ($scope === 'stores') {
+                    $store = Mage::app()->getStore($scopeId);
+                    $website = $store->getWebsite();
+                    $websiteName = $website->getName();
+                    $storeViewName = $store->getName();
+
+                    if (!isset($scopesByType[$websiteName])) {
+                        $scopesByType[$websiteName] = [];
+                    }
+                    $scopesByType[$websiteName][$storeViewName] = true;
+
+                    if (isset($usage[$path])) {
+                        $usage[$path][$websiteName . $labelDelimiter . $storeViewName] = true;
+                    }
+                } elseif ($scope === 'websites') {
+                    $website = Mage::app()->getWebsite($scopeId);
+                    $websiteName = $website->getName();
+
+                    if (!isset($scopesByType[$websiteName])) {
+                        $scopesByType[$websiteName] = [];
+                    }
+                    $scopesByType[$websiteName][null] = true;
+
+                    if (isset($usage[$path])) {
+                        $usage[$path][$websiteName] = true;
+                    }
+                } else {
+                    // Default Config
+                    $scopesByType['Default Config'][null] = true;
+
+                    if (isset($usage[$path])) {
+                        $usage[$path]['Default Config'] = true;
+                    }
+                }
+            }
+
+            $usedIn = [];
+            // Build ordered labels for each page type.
+            // Ordered as follows: "Default Config" first, then for each website, the website name, followed by each store view under that website.
+            foreach ($usage as $path => $scopeLabels) {
+                if ($scopeLabels) {
+                    $labels = [];
+                    // Default Config first
+                    if (array_key_exists('Default Config', $scopeLabels)) {
+                        $labels[] = 'Default Config';
+                    }
+
+                    // Then each website, first website name, then store views
+                    foreach ($scopesByType as $websiteName => $storeViews) {
+                        if ($websiteName === 'Default Config') {
+                            continue;
+                        }
+                        // Website only
+                        if (array_key_exists($websiteName, $scopeLabels)) {
+                            $labels[] = $websiteName;
+                        }
+                        // Store views
+                        foreach ($storeViews as $storeViewName => $dummy) {
+                            $fullLabel = $websiteName . $labelDelimiter . $storeViewName;
+                            if (array_key_exists($fullLabel, $scopeLabels)) {
+                                $labels[] = $fullLabel;
+                            }
+                        }
+                    }
+
+                    $usedIn[] = sprintf(
+                        '%s%s(%s)',
+                        $configPaths[$path],
+                        $delimiter,
+                        implode(', ', $labels)
+                    );
+                }
+            }
+            $configUrl = Mage::helper('adminhtml')->getUrl('adminhtml/system_config/edit/section/web');
+            $configLink = '<a href="' . $configUrl . '" target="_blank">' . Mage::helper('cms')->__('Default Pages') . '</a>';
+
+            if (count($usedIn)) {
+                $configUrl = Mage::helper('adminhtml')->getUrl('adminhtml/system_config/edit/section/web');
+                $configLink = '<a href="' . $configUrl . '" target="_blank">' . Mage::helper('cms')->__('Default Pages') . '</a>';
+                $message = Mage::helper('cms')->__(
+                    'This page is used as %s.',
+                    $this->_joinWithCommaAnd($usedIn)
+                );
+                $message .= ' ' . Mage::helper('cms')->__(
+                        'Please change the %s configuration per scope before disabling.',
+                        $configLink
+                    );
+                Mage::throwException($message);
+            }
+        }
+
+        return $this;
+    }
 }
