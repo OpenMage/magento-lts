547d8725d2c4756edf1ef8949b750187b441a6bd
Sven Reichel <github-sr@hotmail.com>
2025-10-04 06:42:54 +0200

    Added test

--- cypress/e2e/openmage/backend/cms/page.cy.js
+++ cypress/e2e/openmage/backend/cms/page.cy.js
@@ -25,4 +25,27 @@ describe(`Checks admin system "${route.h3}"`, () => {
         cy.get(validation._warningMessage).should('include.text', 'Cannot disable page, it is used in configuration');
         cy.get(validation._successMessage).should('include.text', 'The page has been saved.');
     });
+
+    it('tests to un-asign a CMS page that is used in config', () => {
+        cy.log('Select a CMS page');
+        cy.get(route._gridTable)
+            .contains('td', 'no-route')
+            .click();
+
+        cy.log('Asign another store to the CMS page');
+        cy.get('#page_store_id')
+            .select(4);
+
+        validation.saveAction(route._buttonSaveAndContinue);
+        cy.get(validation._successMessage).should('include.text', 'The page has been saved.');
+
+        cy.log('Restore the default store to the CMS page');
+        cy.get('#page_store_id')
+            .select([1, 2, 3]);
+
+        validation.saveAction(route._buttonSaveAndContinue);
+        // @todo: fix needed - this test passes because of a Magento bug
+        cy.get(validation._successMessage).should('include.text', 'The page has been saved.');
+
+    });
 });
