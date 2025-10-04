64295055475f20322b4523b7ac6748d452cac20f
Sven Reichel <github-sr@hotmail.com>
2025-10-04 05:45:41 +0200

    Added cypress test for disable configured CMS page

--- cypress/e2e/openmage/backend/cms/page.cy.js
+++ cypress/e2e/openmage/backend/cms/page.cy.js
@@ -1,4 +1,5 @@
 const route = cy.testRoutes.backend.cms.page;
+const validation = cy.openmage.validation;
 
 describe(`Checks admin system "${route.h3}"`, () => {
     beforeEach('Log in the user', () => {
@@ -8,5 +9,20 @@ describe(`Checks admin system "${route.h3}"`, () => {
 
     it(`tests classes and title`, () => {
         cy.adminTestRoute(route);
+    });
+
+    it('tests to disable a CMS page that is used in config', () => {
+        cy.log('Select a CMS page');
+        cy.get(route._gridTable)
+            .contains('td', 'no-route')
+            .click();
+
+        cy.log('Disable the CMS page');
+        cy.get('#page_is_active')
+            .select('Disabled');
+
+        validation.saveAction(route._buttonSaveAndContinue);
+        cy.get(validation._warningMessage).should('include.text', 'Cannot disable page, it is used in configuration');
+        cy.get(validation._successMessage).should('include.text', 'The page has been saved.');
     });
 });
