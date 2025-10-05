2f9f632ec498e6424008698618f8ad57b33dd229
Sven Reichel <github-sr@hotmail.com>
2025-10-05 05:06:52 +0200

    cypress screenshots

--- cypress/e2e/openmage/backend/cms/page.cy.js
+++ cypress/e2e/openmage/backend/cms/page.cy.js
@@ -24,7 +24,7 @@ describe(`Checks admin system "${route.h3}"`, () => {
         validation.saveAction(route._buttonSaveAndContinue);
         cy.get(validation._warningMessage).should('include.text', 'Cannot disable page, it is used in configuration');
         cy.get(validation._successMessage).should('include.text', 'The page has been saved.');
-        //cy.get('#messages').screenshot('error-disable-active-page', { overwrite: true});
+        cy.get('#messages').screenshot('error-disable-active-page', { overwrite: true});
     });
 
     it('tests to delete a CMS page that is used in config', () => {
@@ -35,6 +35,6 @@ describe(`Checks admin system "${route.h3}"`, () => {
 
         validation.saveAction(route._buttonDelete);
         cy.get(validation._errorMessage).should('include.text', 'Cannot delete page');
-        //cy.get('#messages').screenshot('error-delete-active-page', { overwrite: true});
+        cy.get('#messages').screenshot('error-delete-active-page', { overwrite: true});
     });
 });
