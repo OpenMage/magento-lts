c9d58fbcdf7b009898f0fa628d397f150f96b1c0
Sven Reichel <github-sr@hotmail.com>
2025-10-13 09:20:28 +0200

    added test

---
+++ cypress/e2e/openmage/issues/4923.cy.js
@@ -0,0 +1,16 @@
+describe('Check trailing slash', () => {
+    const canonical = 'head link[rel="canonical"]';
+
+    it('tests page has canocincal tag', () => {
+        cy.visit('/');
+        cy.get(canonical).should('exist').should('have.attr', 'href');
+
+        cy.visit('/men.html');
+        cy.get(canonical).should('exist').should('have.attr', 'href');
+    });
+
+    it('tests page has no canocincal tag', () => {
+        cy.visit('/no-route');
+        cy.get(canonical).should('not.exist');
+    });
+})
