63fe7417afe84f75aee2c0b160ec9cf4f70a7930
Sven Reichel <github-sr@hotmail.com>
2025-10-27 05:50:57 +0100

    rector: `FunctionFirstClassCallableRector` (#5037)
    * rector: `FunctionFirstClassCallableRector`
    
    - https://getrector.com/rule-detail/function-first-class-callable-rector
    
    * fix
    
    * updated PhpStan baseline
    
    * cs

--- app/code/core/Mage/Catalog/Model/Product/Type/Configurable.php
+++ app/code/core/Mage/Catalog/Model/Product/Type/Configurable.php
@@ -862,7 +862,7 @@ class Mage_Catalog_Model_Product_Type_Configurable extends Mage_Catalog_Model_Pr
     public function processBuyRequest($product, $buyRequest)
     {
         $superAttribute = $buyRequest->getSuperAttribute();
-        $superAttribute = (is_array($superAttribute)) ? array_filter($superAttribute, '\intval') : [];
+        $superAttribute = (is_array($superAttribute)) ? array_filter($superAttribute, \intval(...)) : [];
 
         return ['super_attribute' => $superAttribute];
     }
