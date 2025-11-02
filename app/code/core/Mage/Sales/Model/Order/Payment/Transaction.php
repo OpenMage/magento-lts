16504c615e98d69f58dfddfb262e8a5ab2411799
Sven Reichel <github-sr@hotmail.com>
2025-11-02 05:41:23 +0100

    typo

--- app/code/core/Mage/Sales/Model/Order/Payment/Transaction.php
+++ app/code/core/Mage/Sales/Model/Order/Payment/Transaction.php
@@ -16,7 +16,7 @@
  *
  * @method Mage_Sales_Model_Resource_Order_Payment_Transaction _getResource()
  * @method Mage_Sales_Model_Resource_Order_Payment_Transaction getResource()
- * @method Mage_Sales_Model_Resource_Order_Payment_Transaction_Collection getCollection()()
+ * @method Mage_Sales_Model_Resource_Order_Payment_Transaction_Collection getCollection()
  * @method Mage_Sales_Model_Resource_Order_Payment_Transaction_Collection getResourceCollection()
  *
  * @method string getCreatedAt()
