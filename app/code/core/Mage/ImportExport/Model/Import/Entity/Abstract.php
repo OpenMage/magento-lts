71216cd2b58b97b6c09410f41fae238a4e5c631b
Sven Reichel <github-sr@hotmail.com>
2025-12-05 04:48:59 +0100

    fix-7

--- app/code/core/Mage/ImportExport/Model/Import/Entity/Abstract.php
+++ app/code/core/Mage/ImportExport/Model/Import/Entity/Abstract.php
@@ -574,6 +574,7 @@ abstract class Mage_ImportExport_Model_Import_Entity_Abstract
                 } catch (InvalidFormatException) {
                     $valid = false;
                 }
+
                 $valid !== false || preg_match('/^\d{2}.\d{2}.\d{2,4}(?:\s+\d{1,2}.\d{1,2}(?:.\d{1,2})?)?$/', $val);
                 break;
             case 'text':
