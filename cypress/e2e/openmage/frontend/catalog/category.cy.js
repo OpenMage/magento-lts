42f773423dd153539c7e3c91b77c181d926ea922
Sven Reichel <github-sr@hotmail.com>
2025-12-07 03:32:04 +0100

    added tests - that will fail

---
+++ cypress/e2e/openmage/frontend/catalog/category.cy.js
@@ -0,0 +1,18 @@
+const test = cy.openmage.test.frontend.catalog.category.config;
+
+describe('Check cataog category page', () => {
+    beforeEach('Go to page', () => {
+        cy.visit(test.url);
+    });
+
+    it('tests swatch: color', () => {
+        const options = 'ul.configurable-swatch-color';
+        const swatch = 'li a.swatch-link';
+
+        cy.get(options).eq(0).find(swatch).should('have.length', 4);
+        cy.get(options).eq(0).find(swatch).eq(0).invoke('attr', 'title').should('eq', 'Red');
+        cy.get(options).eq(0).find(swatch).eq(1).invoke('attr', 'title').should('eq', 'Khaki');
+        cy.get(options).eq(0).find(swatch).eq(2).invoke('attr', 'title').should('eq', 'Charcoal');
+        cy.get(options).eq(0).find(swatch).eq(3).invoke('attr', 'title').should('eq', 'Royal Blue');
+    });
+})
---
+++ cypress/e2e/openmage/frontend/catalog/product.cy.js
@@ -0,0 +1,31 @@
+const test = cy.openmage.test.frontend.catalog.product.config;
+
+describe('Check cataog product page', () => {
+    beforeEach('Go to page', () => {
+        cy.visit(test.url);
+    });
+
+    it('tests swatch: color', () => {
+        const options = 'ul#configurable_swatch_color li a.swatch-link';
+        cy.get(options).should('have.length', 4);
+        cy.get(options).eq(0).invoke('attr', 'title').should('eq', 'Charcoal');
+        cy.get(options).eq(1).invoke('attr', 'title').should('eq', 'Khaki');
+        cy.get(options).eq(2).invoke('attr', 'title').should('eq', 'Red');
+        cy.get(options).eq(3).invoke('attr', 'title').should('eq', 'Royal Blue');
+    });
+
+    it('tests swatch: size', () => {
+        const options = 'ul#configurable_swatch_size li a.swatch-link';
+        cy.get(options).should('have.length', 5);
+        cy.get(options).eq(0).contains('XS').should('exist');
+        cy.get(options).eq(1).contains('S').should('exist');
+        cy.get(options).eq(2).contains('M').should('exist');
+        cy.get(options).eq(3).contains('L').should('exist');
+        cy.get(options).eq(4).contains('XL').should('exist');
+        cy.get(options).eq(0).invoke('attr', 'title').should('eq', 'XS')
+        cy.get(options).eq(1).invoke('attr', 'title').should('eq', 'S')
+        cy.get(options).eq(2).invoke('attr', 'title').should('eq', 'M')
+        cy.get(options).eq(3).invoke('attr', 'title').should('eq', 'L')
+        cy.get(options).eq(4).invoke('attr', 'title').should('eq', 'XL')
+    });
+})
