518828f5aa075dcef50f91673e66d85ea44b5812
Sven Reichel <github-sr@hotmail.com>
2025-12-12 01:13:14 +0100

    added unit tests

---
+++ tests/unit/Mage/Core/Helper/LogTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @copyright  For copyright and license information, read the COPYING.txt file.
+ * @link       /COPYING.txt
+ * @license    Open Software License (OSL 3.0)
+ * @package    OpenMage_Tests
+ */
+
+declare(strict_types=1);
+
+namespace OpenMage\Tests\Unit\Mage\Core\Helper;
+
+use Mage;
+use Mage_Core_Helper_Log as Subject;
+use Monolog\Level;
+use OpenMage\Tests\Unit\OpenMageTest;
+use OpenMage\Tests\Unit\Traits\DataProvider\Mage\Core\Helper\LogTrait;
+
+final class LogTest extends OpenMageTest
+{
+    use LogTrait;
+
+    public const TEST_URL = 'foo';
+
+    // @phpstan-ignore property.onlyWritten
+    private static Subject $subject;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        self::$subject = Mage::helper('core/log');
+    }
+
+    /**
+     * @dataProvider provideGetLogLevelData
+     * @covers Mage_Core_Helper_Log::getLogLevel()
+     * @group Helper
+     */
+    public function testGetLogLevel(int $expectedResult, null|int|Level|string $level): void
+    {
+        self::assertSame($expectedResult, Subject::getLogLevel($level));
+    }
+
+    /**
+     * @covers Mage_Core_Helper_Log::getAlowedFileExtensions()
+     * @group Helper
+     */
+    public function testGetAlowedFileExtensions(): void
+    {
+        self::assertIsArray(Subject::getAlowedFileExtensions());
+    }
+
+    /**
+     * @covers Mage_Core_Helper_Log::getLogFile()
+     * @group Helper
+     */
+    public function testGetLogFile(): void
+    {
+        self::assertIsString(Subject::getLogFile());
+    }
+
+    /**
+     * @covers Mage_Core_Helper_Log::getLogLevelMax()
+     * @group Helper
+     */
+    public function testGetLogLevelMax(): void
+    {
+        self::assertIsInt(Subject::getLogLevelMax());
+    }
+}
---
+++ tests/unit/Traits/DataProvider/Mage/Core/Helper/LogTrait.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @copyright  For copyright and license information, read the COPYING.txt file.
+ * @link       /COPYING.txt
+ * @license    Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace OpenMage\Tests\Unit\Traits\DataProvider\Mage\Core\Helper;
+
+use Generator;
+use Monolog\Level;
+
+trait LogTrait
+{
+    public function provideGetLogLevelData(): Generator
+    {
+        yield 'null' => [
+            100,
+            null,
+        ];
+        yield 'empty string' => [
+            100,
+            '',
+        ];
+        yield 'string' => [
+            100,
+            'abc',
+        ];
+        yield 'string psr3' => [
+            200,
+            'info',
+        ];
+        yield 'numeric string' => [
+            600,
+            '0',
+        ];
+        yield 'numeric invalid string' => [
+            100,
+            '999',
+        ];
+        yield 'int to 7' => [
+            600,
+            0
+        ];
+        yield 'int to 100' => [
+            100,
+            66,
+        ];
+        yield 'int invalid' => [
+            100,
+            999,
+        ];
+        yield 'monolog level' => [
+            100,
+            Level::Debug,
+        ];
+    }
+}
