c45207c5bde990d23ac78451a504d97d716783a7
Sven Reichel <github-sr@hotmail.com>
2026-01-01 07:00:59 +0100

    phpstan

--- app/code/core/Mage/Admin/Model/User.php
+++ app/code/core/Mage/Admin/Model/User.php
@@ -533,10 +533,8 @@ class Mage_Admin_Model_User extends Mage_Core_Model_Abstract
                     return (string) $child->action;
                 }
 
-                if ($child->children) {
-                    $action = $this->findFirstAvailableMenu($child->children, $path . $childName . '/', $level + 1);
-                    return $action ?: (string) $child->action;
-                }
+                $action = $this->findFirstAvailableMenu($child->children, $path . $childName . '/', $level + 1);
+                return $action ?: (string) $child->action;
             }
         }
 
