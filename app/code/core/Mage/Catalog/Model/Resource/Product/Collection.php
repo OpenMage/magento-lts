f44588ea57c84d9fb81d211d2c5ccf5f380c5447
Sven Reichel <github-sr@hotmail.com>
2026-03-13 13:01:53 +0100

    fix

--- app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
+++ app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
@@ -1524,7 +1524,7 @@ class Mage_Catalog_Model_Resource_Product_Collection extends Mage_Catalog_Model_
             $productIds[] = $product->getId();
         }
 
-        if (!empty($productIds)) {
+        if ($productIds !== []) {
             $storeId = $this->getStoreId();
             $options = Mage::getModel('catalog/product_option')
                 ->getCollection()
