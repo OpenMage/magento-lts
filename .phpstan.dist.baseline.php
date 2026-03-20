eed27f9f5400bfea7def84a7ed1c2247520727e0
Sven Reichel <github-sr@hotmail.com>
2026-03-20 21:06:36 +0100

    baseline

--- .phpstan.dist.baseline.php
+++ .phpstan.dist.baseline.php
@@ -254,12 +254,6 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/app/code/core/Mage/Adminhtml/Block/Customer/Edit/Tab/Addresses.php',
 ];
 $ignoreErrors[] = [
-	'rawMessage' => 'PHPDoc type string of property Mage_Adminhtml_Block_Customer_Edit_Tab_Wishlist::$_defaultSort is not the same as PHPDoc type mixed of overridden property Mage_Adminhtml_Block_Widget_Grid::$_defaultSort.',
-	'identifier' => 'property.phpDocType',
-	'count' => 1,
-	'path' => __DIR__ . '/app/code/core/Mage/Adminhtml/Block/Customer/Edit/Tab/Wishlist.php',
-];
-$ignoreErrors[] = [
 	'rawMessage' => 'Construct empty() is not allowed. Use more strict comparison.',
 	'identifier' => 'empty.notAllowed',
 	'count' => 1,
@@ -378,12 +372,6 @@ $ignoreErrors[] = [
 	'identifier' => 'arguments.count',
 	'count' => 1,
 	'path' => __DIR__ . '/app/code/core/Mage/Adminhtml/Block/Report/Product/Grid.php',
-];
-$ignoreErrors[] = [
-	'rawMessage' => 'PHPDoc type int of property Mage_Adminhtml_Block_Report_Product_Sold_Grid::$_subReportSize is not the same as PHPDoc type mixed of overridden property Mage_Adminhtml_Block_Report_Grid::$_subReportSize.',
-	'identifier' => 'property.phpDocType',
-	'count' => 1,
-	'path' => __DIR__ . '/app/code/core/Mage/Adminhtml/Block/Report/Product/Sold/Grid.php',
 ];
 $ignoreErrors[] = [
 	'rawMessage' => 'PHPDoc type string of property Mage_Adminhtml_Block_Report_Product_Viewed_Grid::$_resourceCollectionName is not the same as PHPDoc type mixed of overridden property Mage_Adminhtml_Block_Report_Grid_Abstract::$_resourceCollectionName.',
@@ -5640,12 +5628,6 @@ $ignoreErrors[] = [
 	'identifier' => 'argument.type',
 	'count' => 1,
 	'path' => __DIR__ . '/app/code/core/Mage/Payment/Model/Recurring/Profile.php',
-];
-$ignoreErrors[] = [
-	'rawMessage' => 'PHPDoc type bool of property Mage_Paypal_Block_Adminhtml_Settlement_Report_Grid::$_saveParametersInSession is not the same as PHPDoc type mixed of overridden property Mage_Adminhtml_Block_Widget_Grid::$_saveParametersInSession.',
-	'identifier' => 'property.phpDocType',
-	'count' => 1,
-	'path' => __DIR__ . '/app/code/core/Mage/Paypal/Block/Adminhtml/Settlement/Report/Grid.php',
 ];
 $ignoreErrors[] = [
 	'rawMessage' => 'Construct empty() is not allowed. Use more strict comparison.',
