e1e0866236e63519f2cb687d344dc6a010da8cc0
Sven Reichel <github-sr@hotmail.com>
2026-03-25 10:26:44 +0100

    Added Mage_Core_Observer_Interface
    M2 pattern ...
    
    - observers should have only one function/method (execute)
    - new observers should be final ... NO rewrite. To disable observer, turn them off from XML
    - ...

---
+++ app/code/core/Mage/Core/Observer/Interface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright  For copyright and license information, read the COPYING.txt file.
+ * @link       /COPYING.txt
+ * @license    Open Software License (OSL 3.0)
+ * @package    Mage_Core
+ */
+
+/**
+ * Observer interface
+ *
+ * @category   Mage
+ * @package    Mage_Core
+ */
+interface Mage_Core_Observer_Interface
+{
+    public function execute(Varien_Event_Observer $observer): self;
+}
