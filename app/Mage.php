b71aecc67ec599b0eb1c122620fe631b1ccd15f6
Sven Reichel <github-sr@hotmail.com>
2026-03-26 22:05:30 +0100

    Update Mage::dispatchEvent()

--- app/Mage.php
+++ app/Mage.php
@@ -527,8 +527,8 @@ final class Mage
      * Calls all observer callbacks registered for this event
      * and multiple observers matching event name pattern
      *
-     * @param  string                    $name
-     * @param  array<string, \Exception> $data
+     * @param  string               $name
+     * @param  array<string, mixed> $data
      * @return Mage_Core_Model_App
      */
     public static function dispatchEvent($name, array $data = [])
