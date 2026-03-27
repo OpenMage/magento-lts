c0be925fac38557c8f7388fe3fbb67fc008c89cf
Hans Mackowiak <h.mackowiak@b3-it.de>
2026-03-27 15:41:50 +0100

    ~ fix visability

--- app/code/core/Mage/Adminhtml/Block/Customer/Edit/Tab/Account.php
+++ app/code/core/Mage/Adminhtml/Block/Customer/Edit/Tab/Account.php
@@ -22,7 +22,7 @@ class Mage_Adminhtml_Block_Customer_Edit_Tab_Account extends Mage_Adminhtml_Bloc
      * @return $this
      * @throws Mage_Core_Exception
      */
-    public function _prepareForm()
+    protected function _prepareForm()
     {
         $form = new Varien_Data_Form();
         $form->setHtmlIdPrefix('_account');
