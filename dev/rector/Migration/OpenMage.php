4cdd9a293875deeb0d915e46b7f46a697d237640
Sven Reichel <github-sr@hotmail.com>
2026-03-28 18:28:41 +0100

    typo

--- dev/rector/Migration/OpenMage.php
+++ dev/rector/Migration/OpenMage.php
@@ -25,9 +25,9 @@ final class OpenMage
         $paths = [
             'general/locale/code'           => 'Mage_Core_Model_Locale::XML_PATH_DEFAULT_LOCALE',
             'general/locale/timezone'       => 'Mage_Core_Model_Locale::XML_PATH_DEFAULT_TIMEZONE',
-            'web/default/cms_home_page'     => 'Mage_Cms_Helper_Page::XML_PATH_HOME_PAGE)',
-            'web/default/cms_no_cookies'    => 'Mage_Cms_Helper_Page::XML_PATH_NO_COOKIES_PAGE)',
-            'web/default/cms_no_route'      => 'Mage_Cms_Helper_Page::XML_PATH_NO_ROUTE_PAGE)',
+            'web/default/cms_home_page'     => 'Mage_Cms_Helper_Page::XML_PATH_HOME_PAGE',
+            'web/default/cms_no_cookies'    => 'Mage_Cms_Helper_Page::XML_PATH_NO_COOKIES_PAGE',
+            'web/default/cms_no_route'      => 'Mage_Cms_Helper_Page::XML_PATH_NO_ROUTE_PAGE',
         ];
 
         foreach ($paths as $old => $new) {
