ed4ff6d118422f1122ffc1721790ea6442cfb554
Ibrahim Saleh <accounts@aqeltech.com>
2026-03-29 13:19:38 +0300

    Update app/code/core/Mage/Usa/Model/Observer/ValidateOrderAddress.php
    Co-authored-by: Sven Reichel <github-sr@hotmail.com>

--- app/code/core/Mage/Usa/Model/Observer/ValidateOrderAddress.php
+++ app/code/core/Mage/Usa/Model/Observer/ValidateOrderAddress.php
@@ -19,7 +19,7 @@ declare(strict_types=1);
  *
  * @package    Mage_Usa
  */
-class Mage_Usa_Model_Observer_ValidateOrderAddress implements Mage_Core_Observer_Interface
+final class Mage_Usa_Model_Observer_ValidateOrderAddress implements Mage_Core_Observer_Interface
 {
     protected ?Mage_Usa_Model_Shipping_Carrier_Usps_Address_Service $_addressService = null;
 
