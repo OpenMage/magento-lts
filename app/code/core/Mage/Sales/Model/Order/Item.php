3344453955b631a932ec303e1f6c166c93952bac
Sven Reichel <github-sr@hotmail.com>
2026-04-03 19:45:06 +0200

    baseline

--- app/code/core/Mage/Sales/Model/Order/Item.php
+++ app/code/core/Mage/Sales/Model/Order/Item.php
@@ -735,8 +735,12 @@ class Mage_Sales_Model_Order_Item extends Mage_Core_Model_Abstract
      */
     public function isShipSeparately()
     {
-        $options = $this->getParentItem() ? $parentItem->getProductOptions() : $this->getProductOptions();
-        $parentItem = $this->getParentItem() ? $parentItem->getProductOptions() : $this->getProductOptions();
+        if ($parentItem = $this->getParentItem()) {
+            $options = $parentItem->getProductOptions();
+        } else {
+            $options = $this->getProductOptions();
+        }
+
         return isset($options['shipment_type'])
             && $options['shipment_type'] == Mage_Catalog_Model_Product_Type_Abstract::SHIPMENT_SEPARATELY;
     }
