223be08182ebdc5aa06d4f2e67ae998eb8308448
Sven Reichel <github-sr@hotmail.com>
2026-04-07 05:42:49 +0200

    phpmd

--- app/code/core/Mage/Tax/Model/Sales/Total/Quote/Tax.php
+++ app/code/core/Mage/Tax/Model/Sales/Total/Quote/Tax.php
@@ -218,31 +218,28 @@ class Mage_Tax_Model_Sales_Total_Quote_Tax extends Mage_Sales_Model_Quote_Addres
             if (isset($taxInfoItem['item'])) {
                 // Item hidden taxes
                 $item = $taxInfoItem['item'];
-                $rateKey = $taxInfoItem['rate_key'];
                 $hiddenTax = $taxInfoItem['value'];
                 $baseHiddenTax = $taxInfoItem['base_value'];
-                $inclTax = $taxInfoItem['incl_tax'];
                 $qty = $taxInfoItem['qty'];
 
                 $hiddenTax = $this->_calculator->round($hiddenTax);
                 $baseHiddenTax = $this->_calculator->round($baseHiddenTax);
+
                 $item->setHiddenTaxAmount(max(0, $qty * $hiddenTax));
                 $item->setBaseHiddenTaxAmount(max(0, $qty * $baseHiddenTax));
+
                 $this->_getAddress()->addTotalAmount('hidden_tax', $item->getHiddenTaxAmount());
                 $this->_getAddress()->addBaseTotalAmount('hidden_tax', $item->getBaseHiddenTaxAmount());
             } else {
                 // Shipping hidden taxes
-                $rateKey = $taxInfoItem['rate_key'];
                 $hiddenTax = $taxInfoItem['value'];
                 $baseHiddenTax = $taxInfoItem['base_value'];
-                $inclTax = $taxInfoItem['incl_tax'];
 
                 $hiddenTax = $this->_calculator->round($hiddenTax);
                 $baseHiddenTax = $this->_calculator->round($baseHiddenTax);
 
                 $this->_getAddress()->addTotalAmount('shipping_hidden_tax', $hiddenTax);
                 $this->_getAddress()->addBaseTotalAmount('shipping_hidden_tax', $baseHiddenTax);
-
                 $this->_getAddress()->setShippingHiddenTaxAmount(max(0, $hiddenTax));
                 $this->_getAddress()->setBaseShippingHiddenTaxAmount(max(0, $baseHiddenTax));
             }
