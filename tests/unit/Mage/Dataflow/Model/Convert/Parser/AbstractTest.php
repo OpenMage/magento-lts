dc3946aded927a1561d3cc599afe104b16529425
Sven Reichel <github-sr@hotmail.com>
2026-04-09 07:31:21 +0200

    phpunit: added test

--- tests/unit/Mage/Dataflow/Model/Convert/Parser/AbstractTest.php
+++ tests/unit/Mage/Dataflow/Model/Convert/Parser/AbstractTest.php
@@ -11,8 +11,10 @@ declare(strict_types=1);
 
 namespace OpenMage\Tests\Unit\Mage\Dataflow\Model\Convert\Parser;
 
-// use Mage;
-// use Mage_Dataflow_Model_Convert_Parser_Abstract as Subject;
+use Mage_Dataflow_Model_Batch;
+use Mage_Dataflow_Model_Batch_Export;
+use Mage_Dataflow_Model_Batch_Import;
+use Mage_Dataflow_Model_Convert_Parser_Abstract as Subject;
 use OpenMage\Tests\Unit\OpenMageTest;
 use OpenMage\Tests\Unit\Traits\DataProvider\Mage\Dataflow\Model\Convert\Parser\AbstractTrait;
 
@@ -20,12 +22,43 @@ final class AbstractTest extends OpenMageTest
 {
     use AbstractTrait;
 
-    // private static Subject $subject;
+    private static Subject $subject;
 
-    public static function setUpBeforeClass(): void
+    protected function setUp(): void
     {
-        parent::setUpBeforeClass();
-        // self::$subject = Mage::getModel('dataflow/convert_parser_abstract');
-        self::markTestSkipped('');
+        parent::setUp();
+        self::$subject = $this->getMockForAbstractClass(Subject::class);
+    }
+
+    /**
+     * @group Model
+     */
+    public function testGetBatchModel(): void
+    {
+        self::assertInstanceOf(Mage_Dataflow_Model_Batch::class, self::$subject->getBatchModel());
+    }
+
+    /**
+     * @group Model
+     */
+    public function testGetBatchExortModel(): void
+    {
+        self::assertInstanceOf(Mage_Dataflow_Model_Batch_Export::class, self::$subject->getBatchExportModel());
+    }
+
+    /**
+     * @group Model
+     */
+    public function testGetBatchImportModel(): void
+    {
+        self::assertInstanceOf(Mage_Dataflow_Model_Batch_Import::class, self::$subject->getBatchImportModel());
+    }
+
+    /**
+     * @dataProvider provideGetCopyFile
+     */
+    public function testGetCopyFile(string $expectedResult, string $files): void
+    {
+        self::assertSame($expectedResult, self::$subject->getCopyFile($files));
     }
 }
