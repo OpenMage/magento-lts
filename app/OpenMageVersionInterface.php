c89eaf88c9f36e39120a4534345b9e871a13dc31
Hans Mackowiak <h.mackowiak@b3-it.de>
2026-04-27 17:49:09 +0200

    ~ add Deprecated message and since

---
+++ app/OpenMageVersionInterface.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright  For copyright and license information, read the COPYING.txt file.
+ * @link       /COPYING.txt
+ * @license    Open Software License (OSL 3.0)
+ */
+
+interface OpenMageVersionInterface
+{
+    # EE only?
+    public const VERSION_1_14_2_0           = '1.14.2.0';
+
+    public const VERSION_1_1_5_0            = '1.1.5.0';
+
+    public const VERSION_1_1_7_0            = '1.1.7.0';
+
+    public const VERSION_1_11_2_0           = '1.11.2.0';
+
+    public const VERSION_1_2_0_0            = '1.2.0.0';
+
+    public const VERSION_1_3_0_0            = '1.3.0.0';
+
+    public const VERSION_1_3_2_0            = '1.3.2.0';
+
+    public const VERSION_1_3_2_2            = '1.3.2.2';
+
+    public const VERSION_1_3_2_3            = '1.3.2.3';
+
+    public const VERSION_1_3_2_4            = '1.3.2.4';
+
+    public const VERSION_1_4_0_0            = '1.4.0.0';
+
+    public const VERSION_1_4_0_0_ALPHA_1    = '1.4.0.0-alpha';
+
+    public const VERSION_1_4_0_0_ALPHA_2    = '1.4.0.0-alpha2';
+
+    public const VERSION_1_4_0_0_ALPHA_3    = '1.4.0.0-alpha3';
+
+    public const VERSION_1_4_0_0_BETA_1     = '1.4.0.0-beta1';
+
+    public const VERSION_1_4_0_0_RC_1       = '1.4.0.0-rc1';
+
+    public const VERSION_1_4_0_1            = '1.4.0.1';
+
+    public const VERSION_1_4_1_0            = '1.4.1.0';
+
+    public const VERSION_1_4_2_0            = '1.4.2.0';
+
+    public const VERSION_1_4_2_0_BETA_1     = '1.4.2.0-beta1';
+
+    public const VERSION_1_4_2_0_RC_1       = '1.4.2.0-rc1';
+
+    public const VERSION_1_5_0_0            = '1.5.0.0';
+
+    public const VERSION_1_5_0_0_BETA_2     = '1.5.0.0-beta2';
+
+    public const VERSION_1_5_1_0            = '1.5.1.0';
+
+    public const VERSION_1_6_0_0            = '1.6.0.0';
+
+    public const VERSION_1_6_0_0_RC_2       = '1.6.0.0-rc2';
+
+    public const VERSION_1_6_1_0            = '1.6.1.0';
+
+    public const VERSION_1_6_1_0_ALPHA_1    = '1.6.1.0-alpha1';
+
+    public const VERSION_1_6_2_0            = '1.6.2.0';
+
+    public const VERSION_1_7_0_0            = '1.7.0.0';
+
+    public const VERSION_1_7_0_1            = '1.7.0.1';
+
+    public const VERSION_1_7_0_2            = '1.7.0.2';
+
+    public const VERSION_1_7_1_0            = '1.7.1.0';
+
+    public const VERSION_1_8_0_0            = '1.8.0.0';
+
+    public const VERSION_1_8_2_0            = '1.8.2.0';
+
+    public const VERSION_1_9_2_4            = '1.9.2.4';
+
+    public const VERSION_19_4_16_0          = '19.4.16.0';
+
+    public const VERSION_19_5_0_0           = '19.5.0.0';
+
+    public const VERSION_20_15_0_0          = '20.15.0.0';
+
+    public const VERSION_20_16_0_0          = '20.16.0.0';
+
+    public const VERSION_20_17_0_0          = '20.17.0.0';
+
+    public const VERSION_20_18_0_0          = '20.18.0.0';
+}
