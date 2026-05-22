669b14308f0f03281420991945ea80470ba22f1b
Sven Reichel <github-sr@hotmail.com>
2026-05-22 18:42:33 +0200

    update

--- .phpstan.dist.baselines/_loader.php
+++ .phpstan.dist.baselines/_loader.php
@@ -1,7 +1,6 @@
 <?php declare(strict_types = 1);
 
 return ['includes' => [
-    __DIR__ . '/argument.templateType.php',
     __DIR__ . '/argument.type.php',
     __DIR__ . '/arguments.count.php',
     __DIR__ . '/array.invalidKey.php',
