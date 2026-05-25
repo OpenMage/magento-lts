dd4ff5377c9af5d9a20f9a57d91172e085899450
Andrea Zambon <azambon@webgriffe.com>
2026-05-25 09:33:36 +0200

    Fix CS

--- app/code/core/Mage/Checkout/Model/Session.php
+++ app/code/core/Mage/Checkout/Model/Session.php
@@ -212,7 +212,7 @@ class Mage_Checkout_Model_Session extends Mage_Core_Model_Session_Abstract
                 }
             }
 
-            if ((((bool) $this->getQuoteId()) || !$quote->getCustomerId()) && ($customerSession->isLoggedIn() || $this->_customer)) {
+            if ((!is_null($this->getQuoteId()) || !$quote->getCustomerId()) && ($customerSession->isLoggedIn() || $this->_customer)) {
                 $customer = ($this->_customer) ? $this->_customer : $customerSession->getCustomer();
                 $quote->setCustomer($customer);
             }
