8bcea16b678b8eaffa7f18e6fc3c02bf1e5c395e
Frank Rochlitzer <theroch@users.noreply.github.com>
2026-07-15 21:10:18 +0200

    Remove breaking change

--- app/code/core/Mage/Adminhtml/Block/System/Store/Edit/Form.php
+++ app/code/core/Mage/Adminhtml/Block/System/Store/Edit/Form.php
@@ -329,6 +329,12 @@ class Mage_Adminhtml_Block_System_Store_Edit_Form extends Mage_Adminhtml_Block_W
         $form->setUseContainer(true);
         $this->setForm($form);
 
+        trigger_error(
+            'Event "adminhtml_store_edit_form_prepare_form" is deprecated and will be removed in a future version.',
+            E_USER_DEPRECATED
+        );
+        Mage::dispatchEvent('adminhtml_store_edit_form_prepare_form', ['block' => $this]);
+
         return parent::_prepareForm();
     }
 }
