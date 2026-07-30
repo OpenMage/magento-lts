8829329188d5b3692fe1d80f5772171db6ce7239
Hans Mackowiak <h.mackowiak@b3-it.de>
2026-07-30 15:27:00 +0200

    Update View.php

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Comments/View.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Comments/View.php
@@ -73,6 +73,7 @@ class Mage_Adminhtml_Block_Sales_Order_Comments_View extends Mage_Adminhtml_Bloc
      * @param  null|string[]                       $allowedTags
      * @return null|string|array<int, string|null>
      * @phpstan-return ($data is array ? array<?string> : ?string)
+     */
     #[Override]
     public function escapeHtml($data, $allowedTags = null)
     {
