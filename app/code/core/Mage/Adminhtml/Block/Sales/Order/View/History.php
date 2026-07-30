64b40909b12e73547a358e0503f3a16726f2b5a1
Hans Mackowiak <h.mackowiak@b3-it.de>
2026-07-30 15:27:20 +0200

    Update History.php

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/View/History.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/View/History.php
@@ -76,7 +76,8 @@ class Mage_Adminhtml_Block_Sales_Order_View_History extends Mage_Adminhtml_Block
      * @param  null|string|array<int, string|null> $data
      * @param  null|string[]                       $allowedTags
      * @return null|string|array<int, string|null>
-     * @phpstan-return ($data is array ? array<?string> : ?string)
+     * @phpstan-return ($data is array ? array<?string> : ?string)                 
+     */
     #[Override]
     public function escapeHtml($data, $allowedTags = null)
     {
