c1ac148a5b2ad050c76bfcdd465db939e93d170c
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2026-09-08 17:09:53 +0200

    Fix type safety
    $address->getIsDefaultBilling() can return null, so return explicit return type of bool
    
    Signed-off-by: Frank Rochlitzer <f.rochlitzer@b3-it.de>

--- app/code/core/Mage/Customer/Model/Observer.php
+++ app/code/core/Mage/Customer/Model/Observer.php
@@ -40,7 +40,7 @@ class Mage_Customer_Model_Observer
             return true;
         }
 
-        return $address->getIsDefaultBilling();
+        return $address->getIsDefaultBilling() ? true : false;
     }
 
     /**
@@ -59,7 +59,7 @@ class Mage_Customer_Model_Observer
             return true;
         }
 
-        return $address->getIsDefaultShipping();
+        return $address->getIsDefaultBilling() ? true : false;
     }
 
     /**
