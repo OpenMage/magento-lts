66aa959ce0d2badaa31580cf37bf2039cdc27585
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2026-09-08 18:00:06 +0200

    Fix type safety
    Signed-off-by: Frank Rochlitzer <f.rochlitzer@b3-it.de>

--- app/code/core/Mage/Customer/Model/Observer.php
+++ app/code/core/Mage/Customer/Model/Observer.php
@@ -40,7 +40,7 @@ class Mage_Customer_Model_Observer
             return true;
         }
 
-        return $address->getIsDefaultBilling() ? true : false;
+        return (bool)$address->getIsDefaultBilling();
     }
 
     /**
@@ -59,7 +59,7 @@ class Mage_Customer_Model_Observer
             return true;
         }
 
-        return $address->getIsDefaultBilling() ? true : false;
+        return (bool)$address->getIsDefaultBilling();
     }
 
     /**
