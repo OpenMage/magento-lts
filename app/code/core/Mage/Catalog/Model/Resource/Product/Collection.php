60c93d19e92429bbd20ecf587a2c3b5a90695932
Loek van Gool <loek@deheerhoreca.nl>
2026-09-12 16:29:54 +0200

    Ensure strict comparison for url_data_object flag

--- app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
+++ app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
@@ -580,7 +580,7 @@ class Mage_Catalog_Model_Resource_Product_Collection extends Mage_Catalog_Model_
             }
         }
 
-        if ($objects && $this->getFlag('url_data_object')) {
+        if ($objects && $this->getFlag('url_data_object') === true) {
             $objects = Mage::getResourceSingleton('catalog/url')
                 ->getRewriteByProductStore($objects);
             foreach ($this->_items as $item) {
