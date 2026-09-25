ea76c95f3da9a8c4e2de28d63a39b4b3b27bef45
Hans Mackowiak <h.mackowiak@b3-it.de>
2026-09-25 15:45:30 +0200

    Update missingType.generics.php

--- .phpstan.dist.baselines/missingType.generics.php
+++ .phpstan.dist.baselines/missingType.generics.php
@@ -1412,11 +1412,6 @@ $ignoreErrors[] = [
     'path' => __DIR__ . '/../tests/unit/Mage/Reports/Model/Resource/Report/CollectionTest.php',
 ];
 $ignoreErrors[] = [
-    'rawMessage' => 'Method OpenMage\\Tests\\Unit\\Mage\\Review\\Model\\ReviewTest::provideValidateReviewData() return type with generic class Generator does not specify its types: TKey, TValue, TSend, TReturn',
-    'count' => 1,
-    'path' => __DIR__ . '/../tests/unit/Mage/Review/Model/ReviewTest.php',
-];
-$ignoreErrors[] = [
     'rawMessage' => 'Method OpenMage\\Tests\\Unit\\Mage\\Rule\\Model\\AbstractTest::provideValidateData() return type with generic class Generator does not specify its types: TKey, TValue, TSend, TReturn',
     'count' => 1,
     'path' => __DIR__ . '/../tests/unit/Mage/Rule/Model/AbstractTest.php',
